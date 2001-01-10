4c9e1aa626ec4ed28bf4eb4327afa563dd06fa7a
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-01-10 15:49:53 +0000

    Added some items to the ToDo list.

--- todo.php
+++ todo.php
@@ -1,7 +1,7 @@
 <?
 $title = "ToDo";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/01/09 10:26:39 $';
+$cvs_date = '$Date: 2001/01/10 15:49:53 $';
 
 include "header.inc";
 ?>
@@ -9,7 +9,8 @@ include "header.inc";
 
 <h2>The ToDo List</h2>
 
-<p>Some quick thoughts of what needs improvement in Fink:</p>
+<p>Some quick thoughts of what needs improvement in Fink (or what
+could become future features):</p>
 
 <ul>
 <li>Package revisions</li>
@@ -21,6 +22,12 @@ catalogs should be installed</li>
 <li>Mirror selection</li>
 <li>Optimization: make sure all binaries are stripped and at least
 basic optimization flags are used (e.g. <tt>-O2</tt>)</li>
+<li>Package database on the web site</li>
+<li>Make it run on older Perl versions</li>
+<li>Separate the package manager (i.e. the code) from the package
+information and patches stuff (i.e. the data)</li>
+<li>Support other Unices</li>
+<li>Binary packages</li>
 </ul>
 
 
