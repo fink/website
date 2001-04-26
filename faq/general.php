5d54cc5e2f3112c55658cffdcb6b50644ba46a8a
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-04-26 12:57:01 +0000

    FAQ added (preliminary version).

---
+++ faq/general.php
@@ -0,0 +1,23 @@
+<?
+$title = "F.A.Q. - General";
+$cvs_author = '$Author: chrisp $';
+$cvs_date = '$Date: 2001/04/26 12:57:01 $';
+
+include "header.inc";
+?>
+
+
+<h1>Fink F.A.Q. - General Questions</h1>
+
+<p><a name="what"><b>What is Fink?</b></a></p>
+
+<p>...</p>
+
+<p><a name="naming"><b>What does the name Fink stand for?</b></a></p>
+
+<p>...</p>
+
+
+<?
+include "footer.inc";
+?>
---
+++ faq/index.php
@@ -0,0 +1,45 @@
+<?
+$title = "F.A.Q.";
+$cvs_author = '$Author: chrisp $';
+$cvs_date = '$Date: 2001/04/26 12:57:01 $';
+
+include "header.inc";
+?>
+
+
+<h1>The Fink F.A.Q.</h1>
+
+<p>This is the list of frequently asked questions about Fink. Like in
+most FAQs, some questions are taken from real life and some are made
+up. It's really more like a documentation written in an ad-hoc,
+question and answer style.</p>
+
+<p>The FAQ consists of several pages, one for each section. All
+questions are listed and linked in the table of contents below.</p>
+
+<p>This is currently incomplete - some questions are listed in the
+table of contents but there are no answers.</p>
+
+<h2>Contents</h2>
+
+<ul>
+<li><a href="general.php"><b>General questions</b></a></li>
+<ul>
+<li><a href="general.php#what">What is Fink?</a></li>
+<li><a href="general.php#naming">What does the name Fink stand for?</a></li>
+</ul>
+<li><b>Installation troubleshooting</b></li>
+<ul>
+</ul>
+<li><b>Usage troubleshooting</b></li>
+<ul>
+</ul>
+<li><b>Problems with certain packages</b></li>
+<ul>
+</ul>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
