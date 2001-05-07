7e3f1424d913831ee7ddd627fcad2785125ca6c9
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-05-07 19:52:28 +0000

    Updated to do list. Added links to the SourceForge request trackers.

--- fink/todo.php
+++ fink/todo.php
@@ -1,7 +1,7 @@
 <?
 $title = "ToDo";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/03/19 09:43:03 $';
+$cvs_date = '$Date: 2001/05/07 19:52:28 $';
 
 include "header.inc";
 ?>
@@ -15,27 +15,33 @@ could become future features):</p>
 <ul>
 <li>More distinct dependencies: build-time vs. run-time, specific
 versions</li>
-<li>Move from stow to dpkg for binary package handling; also allows
-for a binary distribution</li>
+<li>Port ncurses, dselect and apt, so that we can make a binary
+distribution</li>
 <li>Infrastructure for daemons and cron jobs</li>
+<li>Support virtual packages (Provides, Replaces)</li>
 <li>Pseudo-packages for software that is already installed on the host
 system</li>
 <li>Optimization: make sure all binaries are stripped and at least
 basic optimization flags are used (e.g. <tt>-O2</tt>)</li>
 <li>Package database on the web site</li>
 <li>Make it run on older Perl versions</li>
-<li>Separate the package manager (i.e. the code) from the package
-information and patches stuff (i.e. the data)</li>
 <li>Support other Unices</li>
 <li>Manage info and other documentation</li>
+<li>Support for dpkg preinst/postinst/prerm scripts</li>
+<li>Support for dpkg configuration file handling</li>
 </ul>
 
-<p>Packages that I want to port in the near future:</p>
+<p>To request other features, use the <a href="http://sourceforge.net/tracker/?atid=367203&group_id=17203&func=browse">Feature
+Request Tracker</a> at SourceForge.</p>
+
+<p>Packages that I want to port or package in the near future:</p>
 
 <ul>
-<li>sawfish (turned out to be a bit difficult)</li>
-<li>possibly KDE</li>
+<li>XFree86 with shared libraries</li>
 </ul>
+
+<p>To suggest other packages to be ported, use the <a href="http://sourceforge.net/tracker/?atid=371315&group_id=17203&func=browse">Package
+Request Tracker</a> at SourceForge.</p>
 
 
 <?
