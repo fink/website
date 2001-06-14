88bff455d29935be10f29012a4ac30685e8b38a7
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-06-14 18:31:21 +0000

    Updated to reflect recent development.

--- fink/todo.php
+++ fink/todo.php
@@ -1,7 +1,7 @@
 <?
 $title = "ToDo";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/05/19 16:21:42 $';
+$cvs_date = '$Date: 2001/06/14 18:31:21 $';
 
 include "header.inc";
 ?>
@@ -15,8 +15,7 @@ could become future features):</p>
 <ul>
 <li>More distinct dependencies: build-time vs. run-time, specific
 versions</li>
-<li>Port apt, so that we can make a binary distribution</li>
-<li>Infrastructure for daemons and cron jobs</li>
+<li>Better infrastructure for daemons and cron jobs</li>
 <li>Support virtual packages (Provides, Replaces)</li>
 <li>Pseudo-packages for software that is already installed on the host
 system</li>
@@ -27,17 +26,18 @@ basic optimization flags are used (e.g. <tt>-O2</tt>)</li>
 <li>Make it run on older Perl versions</li>
 <li>Support other Unices</li>
 <li>Manage info and other documentation</li>
-<li>Support for dpkg configuration file handling</li>
 </ul>
 
 <p>To request other features, use the <a href="http://sourceforge.net/tracker/?atid=367203&group_id=17203&func=browse">Feature
 Request Tracker</a> at SourceForge.</p>
 
+<!--
 <p>Packages that I want to port or package in the near future:</p>
 
 <ul>
 <li>XFree86 with shared libraries</li>
 </ul>
+-->
 
 <p>To suggest other packages to be ported, use the <a href="http://sourceforge.net/tracker/?atid=371315&group_id=17203&func=browse">Package
 Request Tracker</a> at SourceForge.</p>
