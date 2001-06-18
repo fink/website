3905e76823a596aaf588d0f99be35c311b9637f9
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-06-18 13:51:50 +0000

    Changed the stylesheet and postprocessing to use plain HTML as the intermediate format. Makefiles reworked.

---
+++ xml/postprocess.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+use strict;
+
+my ($text, $tag_author, $tag_date);
+
+while (<>) {
+  $text .= $_;
+}
+
+$tag_author = '$Author'.'$';
+$tag_date = '$Date'.'$';
+
+$text =~ s|<html>.*<head>.*<title>|<?\n\$title = "|s;
+$text =~ s|</title>|";\n\$cvs_author = '$tag_author';\n\$cvs_date = '$tag_date';\n\n\$metatags = '|s;
+$text =~ s|</head>.*<body>|';\n\ninclude "header.inc";\n?>\n\n|s;
+$text =~ s|</body>.*</html>|\n\n\n<?\ninclude "footer.inc";\n?>\n|s;
+$text =~ s|\$Id|&#36;Id|g;
+
+print $text;
+
+exit 0;
