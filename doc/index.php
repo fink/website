d607752d2bee9ef93094f73d78a7856ec67df0a7
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-07-08 18:10:38 +0000

    Updated to include the new documents.

--- doc/index.php
+++ doc/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "Documentation";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/06/30 11:11:49 $';
+$cvs_date = '$Date: 2001/07/08 18:10:38 $';
 
 include "header.inc";
 ?>
@@ -10,13 +10,19 @@ include "header.inc";
 <h1>Fink Documentation</h1>
 
 <p>This is a collection of various documents written for Fink. Some of
-the documents should also be useful to people that use Mac OS X or
+the documents may also be useful to people that use Mac OS X or
 Darwin without Fink and want to learn about porting Unix software. The
 intended audience is noted with each document in the list below.</p>
 
 <h2>Available Documents</h2>
 
 <ul>
+<li><a href="bundled/install.php">Installation</a> - how to install
+Fink or upgrade to a new version (for Fink users, also included in the
+distribution)</li>
+<li><a href="bundled/usage.php">Usage</a> - how to use Fink
+and the installed software (for Fink users, also included in the
+distribution)</li>
 <li><a href="cvsaccess/index.php">CVS Access</a> - how to access the Fink CVS
 repository to get the latest code and packages between releases (for
 Fink users)</li>
@@ -26,7 +32,6 @@ maintain Fink packages (for Fink developers)</li>
 
 <p>Documents not yet adapted to the new site structure:</p>
 <ul>
-<li><a href="../fink/usage.php">Usage</a></li>
 <li>A <a href="../fink/todo.php">ToDo list</a></li>
 <li><a href="../darwin/porting.php">Darwin porting tips</a></li>
 <li><a href="../darwin/libtool.php">libtool musings</a></li>
