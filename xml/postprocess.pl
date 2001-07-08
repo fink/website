d0240da63ab1be4675e8a7af00c323949d3349ce
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-07-08 17:57:43 +0000

    Pass the info from the CVS Id tag on to the web site scripts that generate the footer.

--- xml/postprocess.pl
+++ xml/postprocess.pl
@@ -10,6 +10,11 @@ while (<>) {
 $tag_author = '$Author'.'$';
 $tag_date = '$Date'.'$';
 
+if ($text =~ m|Id: \S+ \S+ (\d{2,4}/\d{1,2}/\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}) (\S+)|) {
+  $tag_author = "Author: $2";
+  $tag_date = "Date: $1";
+}
+
 $text =~ s|<html>.*<head>.*<title>|<?\n\$title = "|s;
 $text =~ s|</title>|";\n\$cvs_author = '$tag_author';\n\$cvs_date = '$tag_date';\n\n\$metatags = '|s;
 $text =~ s|</head>.*<body>|';\n\ninclude "header.inc";\n?>\n\n|s;
