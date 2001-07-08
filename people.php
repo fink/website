41cef56b312d8d615b1f504cb47c4a4b90742faf
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-07-08 19:17:31 +0000

    Added contributor credit page.

---
+++ people.php
@@ -0,0 +1,49 @@
+<?
+$title = "Contributors";
+$cvs_author = '$Author: chrisp $';
+$cvs_date = '$Date: 2001/07/08 19:17:31 $';
+$wantnav = "main";
+
+include "header.inc";
+?>
+
+
+<h1>Contributors</h1>
+
+<p>The Fink distribution is maintained by a group of volunteers. Each
+package has a Maintainer field that lists the person responsible for
+that package. Here's a list of the people that have contributed to
+Fink (in alphabetical order):</p>
+
+<h2>Regular Contributors</h2>
+
+<ul>
+<li>Finlay Dobbie</li>
+<li>Jeremy Higgs</li>
+<li>Max Horn</li>
+<li>Andrew Rohl</li>
+<li>Jeffrey S. Whitaker</li>
+<li>Ashley Yakeley</li>
+</ul>
+
+<h2>Occasional Contributors</h2>
+
+<ul>
+<li>Jake Goulding</li>
+<li>Dave Morrison</li>
+<li>Peter O'Gorman</li>
+<li>Chris Pepper</li>
+<li>David Ross</li>
+<li>Christian Swinehart</li>
+<li>Aaron Voisine</li>
+<li>"Zauc"</li>
+<li>Tom Zerucha</li>
+</ul>
+
+<p>If I forgot to list someone, please forgive me and let me know you
+should be listed.</p>
+
+
+<?
+include "footer.inc";
+?>
