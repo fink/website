55075e2b36d70dd7145e473c59bbacf0f2b1fae4
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-08-04 06:37:46 +0000

    Added clarification about copyright.

--- legal.php
+++ legal.php
@@ -1,7 +1,7 @@
 <?
 $title = "Legal Stuff";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/01/11 19:13:25 $';
+$cvs_date = '$Date: 2001/08/04 06:37:46 $';
 
 include "header.inc";
 ?>
@@ -9,12 +9,21 @@ include "header.inc";
 
 <h1>Legal Stuff</h1>
 
-<p>Some legal rumblings to keep layers happy:</p>
+<p>
+Some legal rumblings to keep layers happy:
+</p>
 
-<p>The material on this website is Copyright &copy; 2001
-Christoph Pfisterer, unless noted otherwise.</p>
-<p>This site is neither approved nor endorsed by Apple Computer,
-Inc. All trademarks are the property of their respective holders.</p>
+<p>
+The material on this website is Copyright &copy; 2001
+Christoph Pfisterer, unless noted otherwise.
+If you want to reuse material from this website,
+<a href="mailto:fink@chrisp.de">talk to me</a> first.
+</p>
+
+<p>
+This site is neither approved nor endorsed by Apple Computer, Inc.
+All trademarks are the property of their respective holders.
+</p>
 
 
 <?
