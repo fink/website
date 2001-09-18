9c15c5f210fe85c8626591bee7c807876aa1b2a7
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-09-18 16:52:28 +0000

    Added user's guide.

---
+++ doc/users-guide/index.php
@@ -0,0 +1,33 @@
+<?
+$title = "User's Guide";
+$cvs_author = 'Author: chrisp';
+$cvs_date = 'Date: 2001/09/18 16:52:28';
+
+$metatags = '<link rel="start" href="index.php" title="User's Guide Contents"><link rel="next" href="intro.php" title="Introduction">';
+
+include "header.inc";
+?>
+
+<h1>Fink User's Guide</h1>
+<p>
+This will one day become a comprehensive user's guide to Fink.
+Most sections are targeted at users with previous Unix command line
+experience.
+</p>
+<p>
+This guide covers first time installation and upgrade procedures for
+both the source and the binary distribution.
+Package installation and maintenance is covered as well.
+</p>
+<h2>Contents</h2><ul>
+<li><a href="intro.php"><b>Introduction</b></a></li>
+<ul>
+<li><a href="intro.php#what">What is Fink?</a></li>
+<li><a href="intro.php#req">Requirements</a></li>
+</ul>
+</ul><p>Generated from <i>$Fink: uguide.xml,v 1.1 2001/09/18 16:52:28 chrisp Exp $</i></p>
+
+
+<?
+include "footer.inc";
+?>
---
+++ doc/users-guide/intro.php
@@ -0,0 +1,63 @@
+<?
+$title = "User's Guide - Introduction";
+$cvs_author = 'Author: chrisp';
+$cvs_date = 'Date: 2001/09/18 16:52:28';
+
+$metatags = '<link rel="start" href="index.php" title="User's Guide Contents"><link rel="contents" href="index.php" title="User's Guide Contents"><link rel="prev" href="index.php" title="User's Guide Contents">';
+
+include "header.inc";
+?>
+
+<h1>User's Guide - Introduction</h1>
+
+
+
+<a name="what"><h2>What is Fink?</h2></a>
+<p>
+</p>
+
+
+<a name="req"><h2>Requirements</h2></a>
+<p>
+You need:
+</p>
+<ul>
+<li><p>
+An installed Mac OS X system, version 10.0 or later, or equivalent
+Darwin releases.
+Earlier versions of both will <b>not</b> work.
+See below for more information about supported systems.
+</p></li>
+<li><p>
+Development tools.
+On Mac OS X, install the Developer.pkg package from the Developer
+Tools CD.
+On Darwin, the tools should be present in the default install.
+Note that while the tools are usually not needed in a binary
+</p></li>
+<li><p>
+Many other things that come with Mac OS X and the Developer Tools.
+This includes perl 5.6 and either wget or curl.
+</p></li>
+<li><p>
+Internet access.
+All source code is downloaded from mirror sites.
+</p></li>
+<li><p>
+Patience.
+Compiling several big packages takes time.
+I'm talking hours or even days here.
+</p></li>
+</ul>
+
+
+
+
+
+
+
+
+
+<?
+include "footer.inc";
+?>
