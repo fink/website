4f589c369927c42ad8d74cabd085197b8891fc7b
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-09-22 21:09:28 +0000

    Added Archives links.

--- lists/index.php
+++ lists/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "Mailing Lists";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/07/30 17:30:10 $';
+$cvs_date = '$Date: 2001/09/22 21:09:28 $';
 
 include "header.inc";
 ?>
@@ -18,11 +18,14 @@ All lists are public and have an open subscription policy.
 Click on a mailing list name below to get more information.
 </p>
 <ul>
-<li><p><a href="fink-announce.php">fink-announce</a><br>
+<li><p><a href="fink-announce.php">fink-announce</a>
+(<a href="http://www.geocrawler.com/redir-sf.php3?list=fink-announce">Archives</a>)<br>
 Announcement list for new Fink releases. (low traffic)</p></li>
-<li><p><a href="fink-users.php">fink-users</a><br>
+<li><p><a href="fink-users.php">fink-users</a>
+(<a href="http://www.geocrawler.com/redir-sf.php3?list=fink-users">Archives</a>)<br>
 Discussion and support list for Fink users.</p></li>
-<li><p><a href="fink-devel.php">fink-devel</a><br>
+<li><p><a href="fink-devel.php">fink-devel</a>
+(<a href="http://www.geocrawler.com/redir-sf.php3?list=fink-devel">Archives</a>)<br>
 Discussion list for Fink package maintainers and developers.</p></li>
 <li><p><a href="fink-commits.php">fink-commits</a><br>
 Distribution list for automated CVS commit reports.</p></li>
