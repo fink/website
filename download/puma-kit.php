0b407addb6640ac77a4fdbc88b13bbcffa80eac3
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-10-04 16:13:23 +0000

    Separate instructions for recent and not-so-recent versions, other fixes.

--- download/puma-kit.php
+++ download/puma-kit.php
@@ -1,7 +1,7 @@
 <?
 $title = "Upgrade Kit for Mac OS X 10.1";
 $cvs_author = '$Author: chrisp $';
-$cvs_date = '$Date: 2001/09/30 14:57:24 $';
+$cvs_date = '$Date: 2001/10/04 16:13:23 $';
 
 include "header.inc";
 ?>
@@ -11,20 +11,25 @@ include "header.inc";
 
 <p>
 Since older versions of apt don't work on Mac OS X 10.1, here's the
-special procedure to upgrade an existing Fink installation using
-binary packages.
+special procedure to upgrade an existing Fink installation created by
+the binary installer.
 </p>
 <p>
-Note:
-This procedure will actually work on all systems and can even be used
-to update really old installs of Fink 0.2.x (including MacGIMP and the
-first release of OpenOSX's GIMP CD).
+A very similar procedure can be used to update really old installs of
+Fink 0.2.x (including MacGIMP and the first release of OpenOSX's GIMP
+CD).
 The only requirement is that Fink was installed in <tt>/sw</tt> and
 not in any other directory.
+See <a href="#oldversion">below</a>.
 </p>
 
+<h2>Fink 0.2.4 and newer</h2>
+
 <p>
-The procedure has four main steps:
+This procedure assumes you have installed Fink with the official
+binary installer.
+The first such installer was based on Fink 0.2.4.
+The procedure has three main steps:
 </p>
 <ol>
 
@@ -37,32 +42,87 @@ file and run this command:
 </p>
 <pre>sudo dpkg -i apt_0.5.3-7_darwin-powerpc.deb</pre>
 <p>
-Once apt is installed, let it download the latest package listings:
+Once apt is installed, use these commands to update the package
+listings:
 </p>
-<pre>sudo apt-get update</pre>
+<pre>rehash
+fink scanpackages
+sudo apt-get update</pre>
 </li>
 
 <li><p>Updating the base packages.
 It's important to have the latest package management tools installed,
-the ones you have on your system may be buggy or too old for the
-current packages.
+the ones you have on your system may be outdated.
 </p>
 <pre>sudo apt-get install base-files gettext dpkg fink</pre>
 </li>
 
+<li><p>Updating the rest of your system.
+You can do this either through <tt>dselect</tt> (recommended)
+<b>or</b> with this apt command:
+</p>
+<pre>sudo apt-get dist-upgrade</pre>
+</li>
+
+</ol>
+
+<h2><a name="oldversion">Fink 0.2.3 and older</a></h2>
+
+<p>
+This procedure assumes you have installed an old version of Fink
+(0.2.1 in most cases) as part of another package, like the MacGIMP
+installer or OpenOSX's GIMP installer.
+The procedure has four main steps:
+</p>
+<ol>
+
+<li><p>Getting decent apt and fink packages.
+Download the
+<a href="../bindist/dists/fink-0.3.0/main/binary-darwin-powerpc/base/apt_0.5.3-7_darwin-powerpc.deb">apt-0.5.3-7</a>
+and the
+<a href="../bindist/dists/fink-0.3.0/main/binary-darwin-powerpc/base/fink_0.9.1-1_darwin-powerpc.deb">fink-0.9.1-1</a>
+packages.
+(Yes, that version number is for real.
+The <tt>fink</tt> command in the fink package now has separate
+version numbering from the Fink distribution.)
+In a Terminal.app window, go to the folder where you downloaded the
+file and run these commands to install the packages:
+</p>
+<pre>sudo dpkg -i apt_0.5.3-7_darwin-powerpc.deb
+sudo dpkg -i fink_0.9.1-1_darwin-powerpc.deb</pre>
+<p>
+Once they are installed, use these commands to update the package
+listings:
+</p>
+<pre>rehash
+fink scanpackages
+sudo apt-get update</pre>
+</li>
+
+<li><p>Updating the other base packages.
+It's important to have the latest package management tools installed,
+the ones you have on your system may be buggy or too old for the
+current packages.
+</p>
+<pre>sudo apt-get install base-files gettext dpkg</pre>
+</li>
+
 <li><p>Getting X11 sorted out.
-If you're upgrading from Fink 0.2.1 (used in the MacGIMP and OpenOSX
-installers) or older, you'll have to sort the X11 dependencies out
-before proceeding.
-If you have XFree86 installed, you'll want to install the
-<nobr>system-xfree86</nobr> package in most cases:
+Before proceeding you'll have to sort the X11 dependencies out.
+With MacGIMP and OpenOSX's packages, you'll have a "manual" XFree86
+install (from Fink's point of view, that is), so you should install
+the <nobr>system-xfree86</nobr> package:
 </p>
 <pre>sudo apt-get install system-xfree86</pre>
+<p>
+If the package complains that your XFree86 install is too old, you'll
+have to update it first, then run the above command again.
+</p>
 </li>
 
 <li><p>Updating the rest of your system.
-You can do this either through dselect (recommended) or with this apt
-command:
+You can do this either through <tt>dselect</tt> (recommended)
+<b>or</b> with this apt command:
 </p>
 <pre>sudo apt-get dist-upgrade</pre>
 </li>
