66a802d921c449e0832751399064fa345a7d61fb
Max Horn <max@quendi.de>
2002-05-28 18:56:47 +0000

    clarified text a bit

--- news/kde.php
+++ news/kde.php
@@ -1,7 +1,7 @@
 <?
 $title = "KDE Support In Fink";
-$cvs_author = '$Author: rangerrick $';
-$cvs_date = '$Date: 2002/05/28 17:20:16 $';
+$cvs_author = '$Author: fingolfin $';
+$cvs_date = '$Date: 2002/05/28 18:56:47 $';
 
 include "header.inc";
 ?>
@@ -14,7 +14,7 @@ include "header.inc";
 </p>
 
 <p>
-screenshots:
+Screenshots:
 [<a href="kde3-thesin-gimp.html">#1</a>]
 [<a href="kde3-vasi-oroborosx_and_konq.html">#2</a>]
 [<a href="kde3-rangerrick-ie_and_konq.html">#3</a>]
@@ -32,7 +32,7 @@ screenshots:
  The work-in-progress KDE source tree is being graciously hosted by the
  folks at <a href="http://www.opendarwin.org/">OpenDarwin</a>. More info
  on our progress and things that need to be done are regularly updated
- in the <a href="http://www.opendarwin.org/cgi-bin/cvsweb.cgi/proj/KDE-Darwin/README.Darwin?rev=HEAD&content-type=text/x-cvsweb-markup">README.Darwin</a>
+ in the <a href="http://www.opendarwin.org/cgi-bin/cvsweb.cgi/proj/KDE-Darwin/README.Darwin?rev=HEAD&auml;content-type=text/x-cvsweb-markup">README.Darwin</a>
  file available at the top of the KDE-Darwin tree.
 </p>
 
@@ -61,82 +61,89 @@ screenshots:
 </p>
 
 <p>
- There are a couple of ways to install the KDE packages on
- Fink:
- <ol>
-  <li>
-   <p>
-    <strong>
-     Install from source packages in the unstable tree.
-    </strong>
-   </p>
-   <p>
-    First of all, make sure you have the unstable distribution enabled in your fink
-    configuration.  If you used the defaults when installing fink, just:
-   </p>
-   <p>
-    <ol>
-     <li> <p><strong> Add the unstable tree(s) to your /sw/etc/fink.conf. </strong></p>
-      <p> Add <b>"unstable/main"</b> (without the quotes) to the "<b>Trees:</b>"
-      line in fink.conf.  If US export laws allow it, you can add
-      <b>"unstable/crypto"</b> as well (required for HTTPS support in the Konqueror
-      browser). </p> </li>
-     <li> <p><strong> Update your package index. </strong></p>
-      <p>Run "<b><tt>fink selfupdate-cvs</tt></b>" in a terminal window to get the
-      latest package descriptions from the fink site.  If this is your first time
-      running it, it may ask you some questions.  If you don't know what to answer,
-      the defaults are perfectly safe. </p> </li>
-     <li> <p><strong> Install KDE. </strong></p>
-      <p> If you want to install the base KDE system, run "<b><tt>fink install
-      kdebase3</tt></b>" (or, if you have crypto support, "<b><tt>fink install
-      kdebase3-ssl</tt></b>").  If you want to build all of the official KDE
-      distribution that has been ported, there is a convenience package that
-      will build everything for you.  Just run "<b><tt>fink install
-      bundle-kde</tt></b>". </p> </li>
-     <li> <p><strong> Wait.  A long, long time. </strong></p>
-      <p> Seriously, building all of bundle-kde from scratch took over 24 hours
-      on a G4/800 with 640MB RAM.  This is not a small project to build -- if
-      you really want to build from source, be prepared to spend some time
-      doing something other than using your computer.  :) </p> </li>
-    </ol>
-   </p>
-  </li>
-  <li>
-   <p>
-    <strong>
-     Install from pre-build binary packages with apt or dselect.
-    </strong>
-   </p>
-   <p>
-    To install binaries using apt or dselect, it should be enough to add the
-    following line to your /sw/etc/apt/sources.list file:
-   </p>
-   <p>
-    "<b><tt>deb http://us.dl.sourceforge.net/fink/direct_download fink-kde main</tt></b>"
-   </p>
-   <p>
-    If export laws in the US allow exporting strong cryptography to your country,
-    you can also add "<b>crypto</b>" to the end of that line.
-   </p>
-   <p>
-    Next, update your package cache by running "<b><tt>apt-get update</tt></b>".
-    If all goes well, you should be able to install any of the KDE packages.  To
-    install everything up to the the base set of packages required to run KDE
-    apps, run "<b><tt>apt-get install kdebase3</tt></b>" (or
-    "<b><tt>apt-get install kdebase3-ssl</tt></b>" if you have crypto support).
-    If you want to install all of the official KDE packages that have been
-    ported, run "<b><tt>apt-get install bundle-kde</tt></b>".  After an
-    "apt-get update", you should also be able to access all of the new packages
-    from the dselect console.
-   </p>
-  </li>
- </ol>
+ There are two ways to install the KDE packages via Fink:
 </p>
+<ul>
+ <li>
+  <p>
+   <strong>
+	Install from source packages in the unstable tree.
+   </strong>
+  </p>
+  <p>
+   First of all, make sure you have the unstable distribution enabled in your fink
+   configuration.  If you used the defaults when installing fink, just:
+  </p>
+  <p>
+   <ol>
+	<li> <p><strong> Add the unstable tree(s) to your /sw/etc/fink.conf. </strong></p>
+	 <p> Add <b>"unstable/main"</b> (without the quotes) to the "<b>Trees:</b>"
+	 line in fink.conf.  If US export laws allow it, you can add
+	 <b>"unstable/crypto"</b> as well (required for HTTPS support in the Konqueror
+	 browser). </p> </li>
+	<li> <p><strong> Update your package index. </strong></p>
+	 <p>Run "<b><tt>fink selfupdate-cvs</tt></b>" in a terminal window to get the
+	 latest package descriptions from the fink site.  If this is your first time
+	 running it, it may ask you some questions.  If you don't know what to answer,
+	 the defaults are perfectly safe. </p> </li>
+	<li> <p><strong> Install KDE. </strong></p>
+	 <p> If you want to install the base KDE system, run "<b><tt>fink install
+	 kdebase3</tt></b>" (or, if you have crypto support, "<b><tt>fink install
+	 kdebase3-ssl</tt></b>").  If you want to build all of the official KDE
+	 distribution that has been ported, there is a convenience package that
+	 will build everything for you.  Just run "<b><tt>fink install
+	 bundle-kde</tt></b>". </p> </li>
+	<li> <p><strong> Wait.  A long, long time. </strong></p>
+	 <p> Seriously, building all of bundle-kde from scratch took over 24 hours
+	 on a G4/800 with 640MB RAM.  This is not a small project to build -- if
+	 you really want to build from source, be prepared to spend some time
+	 doing something other than using your computer.  :) </p> </li>
+   </ol>
+  </p>
+ </li>
+ <li>
+  <p>
+   <strong>
+	Install from pre-build binary packages with apt or dselect.
+   </strong>
+  </p>
+  <ol>
+  <li>
+   <p>First add the following line to your <i>/sw/etc/apt/sources.list</i> file:</p>
+   <p>
+	 <nobr><b><tt>deb http://us.dl.sourceforge.net/fink/direct_download fink-kde main</tt></b></nobr>
+   </p>
+   <p>If export laws in the US allow exporting strong cryptography to your country,
+   you can instead use this line which will allow you (besides other things) to access
+   secure web pages in konqueror:
+   </p>
+   <p>
+	 <nobr><b><tt>deb http://us.dl.sourceforge.net/fink/direct_download fink-kde main crypto</tt></b></nobr>
+   </p>
+  </li>
+  <li>
+   <p>Next, update your package cache by running <b><tt>apt-get update</tt></b>. This
+   will update the local list of all available binary packages.</p>
+  </li>
+  <li>
+   If all went well, you should now be able to install any of the KDE packages. To
+   install everything up to the the base set of packages required to run KDE
+   apps, run <b><tt>sudo apt-get install kdebase3</tt></b> (or
+   <b><tt>sudo apt-get install kdebase3-ssl</tt></b> if you enabled crypto support in step 1).
+   If you want to install all of the official KDE packages that have been
+   ported so far, run <b><tt>sudo apt-get install bundle-kde</tt></b>.  Alternatively
+   you can also select and install individual packages via the <b><tt>dselect</tt></b>
+   command in an interactive fashion.
+  </li>
+  </ol>
+ </li>
+</ul>
+
 <h1>If You Have Problems...</h1>
 <p>
  ...please check the
  <a href="http://fink.sourceforge.net/lists/index.php">list archives</a> and
- <a href="http://sourceforge.net/tracker/?atid=117203&group_id=17203">bug tracker</a>
+ <a href="http://sourceforge.net/tracker/?atid=117203&auml;group_id=17203">bug tracker</a>
  before posting questions.  You can also try the
  <b><a href="http://www.aquaflux.org/~fink/">#fink</a></b> channel on
  The OpenProjects IRC network (<b>irc.openprojects.net</b>).  A number of the
