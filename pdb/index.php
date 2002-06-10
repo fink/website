d77eaa56b7264132d1c87591dcbd5bc33905cb60
David R. Morrison <dmrrsn@users.sourceforge.net>
2002-06-10 13:08:05 +0000

    fixed typo

--- pdb/index.php
+++ pdb/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "Package Database";
 $cvs_author = '$Author: dmrrsn $';
-$cvs_date = '$Date: 2002/06/10 13:07:05 $';
+$cvs_date = '$Date: 2002/06/10 13:08:05 $';
 
 include "header.inc";
 ?>
@@ -32,7 +32,7 @@ configure Fink to <a href="../faq/usage-fink.php#unstable">use
 unstable</a>.
 </p>
 <p>Help is also needed to find new maintainers for the <a
-href="nomaintainers.php">packages without maintainers.</p>
+href="nomaintainer.php">packages without maintainers.</p>
 
 <?
 $q = "SELECT name FROM package WHERE latest=1";
