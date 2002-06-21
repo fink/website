aeaa8abb9c11a1953606f211dc38a9f0a5b609c4
Ben Hines <benh57@users.sourceforge.net>
2002-06-21 11:01:16 +0000

    Adding every maintainer that isn't on this list yet. Based on http://homepage.mac.com/bhines/FinkMaintainers.txt

--- people.php
+++ people.php
@@ -1,7 +1,7 @@
 <?
 $title = "Contributors";
-$cvs_author = '$Author: dmrrsn $';
-$cvs_date = '$Date: 2002/05/26 13:17:39 $';
+$cvs_author = '$Author: benh57 $';
+$cvs_date = '$Date: 2002/06/21 11:01:16 $';
 
 include "header.inc";
 ?>
@@ -39,21 +39,64 @@ Fink (in alphabetical order):</p>
 <h2>Occasional Contributors</h2>
 
 <ul>
-<li>Bill Bumgarner</li>
-<li>Jake Goulding</li>
-<li>Thomas Kotzian</li>
-<li>Kyle Moffett</li>
-<li>Chris Pepper</li>
-<li>Andrew Rohl</li>
-<li>David Ross</li>
-<li>Christian Schaffner</li>
-<li>Damian Steer</li>
-<li>Paul Swenson</li>
-<li>Aaron Voisine</li>
-<li>Ashley Yakeley</li>
-<li>Tom Zerucha</li>
-<li>Chris Zubrzycki</li>
-<li>Sasha Zucker</li>
+<li>Jorge Acereda</li> 
+<li>Mike Akers</li> 
+<li>David Bacher</li> 
+<li>Michael Baudis</li> 
+<li>Chris Black</li> 
+<li>Bill Bumgarner</li> 
+<li>Avram Cherry</li> 
+<li>Jon Dugan</li> 
+<li>Jeffrey Ellis</li> 
+<li>Jeremy Erwin</li> 
+<li>Benjamin Esham</li> 
+<li>Matthew Flanagan</li> 
+<li>Chris Frank</li> 
+<li>Paul Gaborit</li> 
+<li>Stanton Gallegos</li> 
+<li>Jake Goulding</li> 
+<li>Matthew Harris</li> 
+<li>Sam Johnston</li> 
+<li>Richard Kiss</li> 
+<li>Eric Knauel</li> 
+<li>Killian Koepsell</li> 
+<li>Thomas Kotzian</li> 
+<li>Bernd Kuemmerlen</li> 
+<li>Christopher League</li> 
+<li>Adam Lebsack</li> 
+<li>William McCallum</li> 
+<li>Kyle Moffett</li> 
+<li>David H. Munro</li> 
+<li>Joseph K Myers</li> 
+<li>Kaben Nanlohy</li> 
+<li>Magenta Hari Nezumi</li> 
+<li>Eric Norum</li> 
+<li>Daniel Parks</li> 
+<li>Chris Pepper</li> 
+<li>Sebastien Pierre</li> 
+<li>Damien Pollet</li> 
+<li>Brian Redman</li> 
+<li>Rudy Richter</li> 
+<li>Stefano Rodriguez</li> 
+<li>Andrew Rohl</li> 
+<li>David Ross</li> 
+<li>Christian Schaffner</li> 
+<li>Michel Schinz</li> 
+<li>Chris Shabsin</li> 
+<li>Leigh Smith</li> 
+<li>Daniel Sohl</li> 
+<li>Damian Steer</li> 
+<li>Mark Stosberg</li> 
+<li>Paul Swenson</li> 
+<li>Patrick Tescher</li> 
+<li>Andrei Verovski</li> 
+<li>Aaron Voisine</li> 
+<li>Dominik Westner</li> 
+<li>Ken Williams</li> 
+<li>Ashley Yakeley</li> 
+<li>Tom Zerucha</li> 
+<li>Chris Zubrzycki</li> 
+<li>Sasha Zucker</li> 
 </ul>
 
 <p><b>Former project lead</b><br>
