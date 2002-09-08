d5b41e3bce2945c60266d324d9754e2ba0c5ea06
David R. Morrison <dmrrsn@users.sourceforge.net>
2002-09-08 19:37:12 +0000

    new upgrade guide

---
+++ news/jaguar.php
@@ -0,0 +1,96 @@
+<?
+$title = "Upgrading Fink for Mac OS X 10.2";
+$cvs_author = '$Author: dmrrsn $';
+$cvs_date = '$Date: 2002/09/08 19:37:12 $';
+
+include "header.inc";
+?>
+
+
+<h1>Upgrading Fink for Mac OS X 10.2</h1>
+
+<p>
+Apple introduced major changes in Mac OS X 10.2, and Fink installations
+need to be upgraded with care in order to cope with these changes.
+If you have upgraded to Mac OS X 10.2 on a machine with a pre-existing
+Fink installation, then this document will provide step-by-step instructions
+for upgrading Fink.  The instructions are current as of 8 September 2002,
+and can be expected to simplify at a later date as further progress in
+updating Fink is made.  Roughly 800 out of 1150 Fink packages are available
+in the new 10.2 version of Fink, with more coming every day.
+</p>
+<p>If you want to install a fresh copy of Fink on Mac OS X 10.2, then you
+should consult the <a href="jag-bootstrap.php">Jaguar bootstrap guide</a>
+instead of this document.
+</p>
+<p>
+Here are the steps to follow for an upgrade.
+</p>
+<ul>
+<li> <b>Step 1: Upgrade Mac OS X and install the OS X 10.2 Developer Tools.</b>
+Even if you have been using Fink's binary distribution, you will need to
+install the Developer Tools, because the 10.2 Fink packages are not yet
+available in binary form.
+<li> <b>Step 2: Obtain the files for the Fink upgrade.</b>
+To do this, create a directory <b>finkcvs</b> in a location of your
+choice.  Now issue the commands
+<pre>
+  cd finkcvs
+  cvs -d:pserver:anonymous@cvs.fink.sourceforge.net:/cvsroot/fink login
+  cvs -d:pserver:anonymous@cvs.fink.sourceforge.net:/cvsroot/fink co fink
+  cd fink
+</pre>
+All subsequent commands will assume that you are in the <b>finkcvs/fink</b>
+directory.
+<li><b>Step 3: Verify the integrity of your Fink installation, and update
+any external software which is related to Fink.</b>  If you have installed
+XFree86 using the distribution from the XonX project, you need to get
+a 10.2 upgrade from that project.  Any other software which Fink learns
+about via a system-foo package should also be checked for updates.
+<p> There have been reports that if you choose "Archive and Install"
+when upgrading to OS X 10.2, some parts of your Fink installation may
+be damaged.  To help diagnose this, run the command
+<pre>
+  ./dpkg-checkall.sh
+</pre>
+from within the finkcvs/fink directory.  You will get a report of any
+missing files from your Fink installation.  In some cases these files
+are harmless, but in case of doubt about the package foo, you should
+either say <b>fink reinstall foo</b/> or <b>fink remove foo</b>.
+You can run dpkg-checkall.sh multiple times if needed.
+<li><b>Step 4: Edit your fink.conf file.</b>
+You will find this file at /sw/etc/fink.conf (or another location if you
+installed Fink in a non-standard place).  You may need to change to file
+permissions or use sudo to edit the file.  You want to add the "unstable"
+trees to the Trees line in this file, so that the line reads
+<pre>
+Trees: local/main stable/main stable/crypto local/bootstrap unstable/main unstable/crypto
+</pre>
+Even if you have not used the unstable Trees in the past, at the present
+time virtually all of the 10.2 Fink packages are still being tested in
+the unstable tree and have not yet been moved to the stable tree, so
+this step is highly recommended.
+<li><b>Step 5: Run the update script.</b>
+From within the finkcvs/fink directory, issue the command
+<pre>
+sudo ./update-fink.sh
+</pre>
+You will need an internet connection, and a bit of patience, as the update
+takes a while to complete.
+<li><b>Step 6: Update your XFree86 packages.</b>
+This is done with the command <b>fink update xfree86-base xfree86-rootless</b>.
+If you are not running XFree86, or if you installed it externally from the
+XonX project, you may skip this step.
+<li><b>Step 7: Update your othe fink packages.</b>
+The easy way to do this is with the command <b>fink update-all</b>, but
+if you prefer to customize things you may issue individual <b>fink update</b>
+commands.
+<li> Finally, when all of this has been completed, you may remove the
+finkcvs directory and its contents if you wish.
+</ul>
+
+
+
+<?
+include "footer.inc";
+?>
