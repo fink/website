5daf14f22bce7d87acd4d73063b20871578ef3eb
Max Horn <max@quendi.de>
2002-10-24 17:36:21 +0000

    changed contributor page a bit

--- people.php
+++ people.php
@@ -1,18 +1,38 @@
 <?
 $title = "Contributors";
-$cvs_author = '$Author: benh57 $';
-$cvs_date = '$Date: 2002/07/14 09:48:00 $';
+$cvs_author = '$Author: fingolfin $';
+$cvs_date = '$Date: 2002/10/24 17:36:21 $';
 
 include "header.inc";
 ?>
 
-
 <h1>Contributors</h1>
 
-<p>The Fink distribution is maintained by a group of volunteers. Each
-package has a Maintainer field that lists the person responsible for
-that package. Here's a list of the people that have contributed to
-Fink (in alphabetical order):</p>
+<p>The Fink project is maintained by a group of volunteers. Each
+Fink package has a Maintainer who works hard to ensure the package works as
+desired. Futhermore there are people that work on the fink program itself.
+The web site has to be kept current. Users need support, provided via our mailing lists
+and our IRC channel. These and many more tasks require a lot of work, thus we would
+like to say "Thank you" to all these people who sacrifice their spare time to help
+the community. As such, this page contains a list of people that have to contributed
+to Fink in some way.
+</p>
+
+<p>So many people have helped Fink over the course of time that it's hard
+for us to list all of you. If you think you should be on this list but aren't,
+drop us a mail and we will consider including you in the next revision of
+this list.</p>
+
+<h2>Former project lead</h2>
+<ul>
+<li>Christoph Pfisterer (<i>chrisp</i>)</li>
+</ul>
+
+<h2>Current project lead</h2>
+<ul>
+<li>Max Horn (<i>Fingolfin</i>)</li>
+<li>David R. Morrison (<i>drm</i>)</li>
+</ul>
 
 <h2>Regular Contributors</h2>
 
@@ -21,6 +41,7 @@ Fink (in alphabetical order):</p>
 <li>Sylvain Cuaz</li>
 <li>Finlay Dobbie (<i>AngusD</i>)</li>
 <li>Justin F. Hallett (<i>TheSin</i>)</li>
+<li>Alexander Hansen</li> <!-- support work on mailing lists! -->
 <li>Jeremy Higgs</li>
 <li>Ben Hines (<i>Clef</i>)</li>
 <li>Max Horn (<i>Fingolfin</i>)</li>
@@ -100,14 +121,6 @@ Fink (in alphabetical order):</p>
 <li>Chris Zubrzycki</li> 
 <li>Sasha Zucker</li> 
 </ul>
-
-<p><b>Former project lead</b><br>
-<blockquote>Christoph Pfisterer</blockquote></p>
-<p><b>Current fink project lead</b><br>
-<blockquote>Max Horn</blockquote></p>
-
-<p>If I forgot to list someone, please forgive me and let me know you
-should be listed.</p>
 
 
 <?
