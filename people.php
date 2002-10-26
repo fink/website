b593cf156ff1f4d0c8a46563559a0e88cdeccd2d
David R. Morrison <dmrrsn@users.sourceforge.net>
2002-10-26 21:49:24 +0000

    adding more people

--- people.php
+++ people.php
@@ -1,7 +1,7 @@
 <?
 $title = "Contributors";
-$cvs_author = '$Author: fingolfin $';
-$cvs_date = '$Date: 2002/10/24 17:36:21 $';
+$cvs_author = '$Author: dmrrsn $';
+$cvs_date = '$Date: 2002/10/26 21:49:24 $';
 
 include "header.inc";
 ?>
@@ -28,7 +28,7 @@ this list.</p>
 <li>Christoph Pfisterer (<i>chrisp</i>)</li>
 </ul>
 
-<h2>Current project lead</h2>
+<h2>Current project leads</h2>
 <ul>
 <li>Max Horn (<i>Fingolfin</i>)</li>
 <li>David R. Morrison (<i>drm</i>)</li>
@@ -49,12 +49,14 @@ this list.</p>
 <li>Matthias Neeracher (<i>Murr</i>)</li>
 <li>Peter O'Gorman (<i>pogma</i>)</li>
 <li>Benjamin Reed (<i>RangerRick</i>)</li>
+<li>Christian Schaffner</li>
 <li>Masanori Sekino (<i>msek</i>)</li>
 <li>Matt Stephenson</li>
 <li>Alexander Strange (<i>Feanor</i>)</li>
 <li>Christian Swinehart</li>
 <li>Dave Vasilevsky (<i>vasi</i>)</li>
 <li>Jeffrey S. Whitaker</li>
+<li>Chris Zubrzycki (<i>cirdan</i>)</li>
 </ul>
 
 <h2>Occasional Contributors</h2>
@@ -66,6 +68,7 @@ this list.</p>
 <li>Michael Baudis</li> 
 <li>Chris Black</li> 
 <li>Bill Bumgarner</li> 
+<li>Yves de Champlain</li>
 <li>Avram Cherry</li> 
 <li>Jon Dugan</li> 
 <li>Jeffrey Ellis</li> 
@@ -77,19 +80,24 @@ this list.</p>
 <li>Stanton Gallegos (<i>stantron</i>)</li> 
 <li>Joe Gorse (<i>inca34</i>)</li>
 <li>Jake Goulding</li> 
+<li>Richard Graul</li>
 <li>Matthew Harris</li> 
 <li>Sam Johnston</li> 
 <li>Richard Kiss</li> 
+<li>Carsten Klapp</li>
 <li>Eric Knauel</li> 
 <li>Killian Koepsell</li> 
 <li>Thomas Kotzian</li> 
+<li>Eric Knauel</li>
 <li>Bernd Kuemmerlen</li> 
 <li>Christopher League</li> 
 <li>Adam Lebsack</li> 
 <li>William McCallum</li> 
+<li>Michael McKibben</li>
+<li>Mathias Meyer</li>
 <li>Kyle Moffett</li> 
 <li>David H. Munro</li> 
-<li>Joseph K Myers</li> 
+<li>Joseph K. Myers</li> 
 <li>Kaben Nanlohy</li> 
 <li>Magenta Hari Nezumi</li> 
 <li>Eric Norum</li> 
@@ -102,7 +110,6 @@ this list.</p>
 <li>Stefano Rodriguez</li> 
 <li>Andrew Rohl</li> 
 <li>David Ross</li> 
-<li>Christian Schaffner</li> 
 <li>Michel Schinz</li> 
 <li>Chris Shabsin</li> 
 <li>Leigh Smith</li> 
@@ -110,15 +117,16 @@ this list.</p>
 <li>Lindsey Spratt</li>
 <li>Damian Steer</li> 
 <li>Mark Stosberg</li> 
-<li>Paul Swenson</li> 
+<li>Tim Seufert</li>
+<Li>Paul Swenson</li> 
 <li>Patrick Tescher</li> 
 <li>Andrei Verovski</li> 
 <li>Aaron Voisine</li> 
 <li>Dominik Westner</li> 
 <li>Ken Williams</li> 
+<li>William Dell Wisner</li>
 <li>Ashley Yakeley</li> 
 <li>Tom Zerucha</li> 
-<li>Chris Zubrzycki</li> 
 <li>Sasha Zucker</li> 
 </ul>
 
