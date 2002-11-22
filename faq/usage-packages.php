602f3d62330b78c8163f8caee179eaf521e4c187
Alexander Hansen <alexkhansen@users.sourceforge.net>
2002-11-22 15:34:02 +0000

    Modified section 1 of answer to Q5.2 due to new xaw3d package from Jeff W.

--- faq/usage-packages.php
+++ faq/usage-packages.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Usage (2)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2002/11/21 15:01:35';
+$cvs_date = 'Date: 2002/11/22 15:34:02';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="prev" href="usage-general.php" title="Package Usage Problems - General">';
 
