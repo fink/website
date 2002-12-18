b2465b2a98d875bbfc1e346eb012b86c830630f5
Alexander Hansen <alexkhansen@users.sourceforge.net>
2002-12-18 15:46:24 +0000

    Addendum to CVS failure:  look for lines starting with "C"

--- faq/comp-general.php
+++ faq/comp-general.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Compiling (1)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2002/12/18 15:13:11';
+$cvs_date = 'Date: 2002/12/18 15:46:24';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="next" href="comp-packages.php" title="Compile Problems - Specific Packages"><link rel="prev" href="usage-fink.php" title="Installing, Using and Maintaining Fink">';
 
--- faq/usage-packages.php
+++ faq/usage-packages.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Usage (2)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2002/12/18 15:13:11';
+$cvs_date = 'Date: 2002/12/18 15:46:24';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="prev" href="usage-general.php" title="Package Usage Problems - General">';
 
