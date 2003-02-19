c42419bad5fe0a5ee1d5aa4d44d2c49e74687d64
Alexander Hansen <alexkhansen@users.sourceforge.net>
2003-02-19 16:40:34 +0000

    Q3.21:  must be root to do operation-- use sudo -s before running

--- faq/relations.php
+++ faq/relations.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Relations";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2003/02/19 15:31:11';
+$cvs_date = 'Date: 2003/02/19 16:40:34';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="next" href="usage-fink.php" title="Installing, Using and Maintaining Fink"><link rel="prev" href="general.php" title="General Questions">';
 
--- faq/usage-general.php
+++ faq/usage-general.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Usage (1)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2003/02/19 15:31:11';
+$cvs_date = 'Date: 2003/02/19 16:40:34';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="next" href="usage-packages.php" title="Package Usage Problems - Specific Packages"><link rel="prev" href="comp-packages.php" title="Compile Problems - Specific Packages">';
 
