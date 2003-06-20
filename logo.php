da25e4ad26db3051154d240a097a51fcc2db9a9f
Darian Lanx <dmalloc@users.sourceforge.net>
2003-06-20 18:13:22 +0000

    logo rules. Please keep in mind that this is a translation of a german article. I know it might sound awkawrd in english, yet if you do nto have the time to compare it to the german original, please only edit it for style and typoes not necessairly semantics. Thanks

---
+++ logo.php
@@ -0,0 +1,56 @@
+<?
+$title = "logo Contest";
+$cvs_author = '$Author: dmalloc $';
+$cvs_date = '$Date: 2003/06/20 18:13:22 $';
+$is_home = 0;
+
+$metatags = '<meta name="description" content="Fink, a distribution of Unix software for Mac OS X and Darwin">
+<meta name="keywords" content="Mac OS X, Darwin, GNU, Unix, GNOME, KDE, software, distribution, Fink">
+';
+
+include "header.inc";
+?>
+
+<b>Note:</b> Below you will find a rough translation of the German article recently posted on Macwelt.de. The Fink project is happy to have started a logo contest. Everyone is invited to join this joint effort in finding a graphical representation of what Fink stands for. More information can be found in the translation below. <p>
+
+<h3>A logo for the Fink Project.<h3>
+<p>
+<a href="http://www.macwelt.de">Macwelt</a>, Europe's best selling magazine for MacIntosh and mac-publishing, asks its readers to create a logo for the open source project Fink. 
+<p>
+<b>Munich/Macwelt</b> - Fink needs a face. The open source project lead by developer Max Horn, Benjamin Reed  and Dave Morrison is looking for a logo. 
+
+macwelt is actively supporting this open source project, asking all of its readers and those interested
+ in finding a logo for the Fink Community. Similar to the red devil of the Darwin Project oder the little
+ Gimp man the Fink logo should meet the demands of modern day print media. Therefore it is necessary for
+ this logo to be created in RGB, CMYK and B/W. It should scale to large formats such as DIN A0 as well as tiny ones to sit in the corner of a business card. 
+<p>
+The bird "Darwin Finke" (Darwin Finch) does not have to be present. Rather the Fink projects asks the artists to let their fantasy play freely.
+Quoting Fink's public Relations Manager David H. <tt>"Everything is allowed. From graffiti to comic. Even photographs are welcome"</tt>.
+<br>
+He also created a little check list for the Fink Jury, those willing to submit their artwork, should make sure that it meets most requirements.
+
+<ul>
+<li>Is it possible to display this logo three dimensional?
+<li>Is the logo still readable at extreme diminution?
+<li>Does the logo display optically well at extreme magnification?
+<li>Is it possible to draw the logo in black and white?
+<li>Are only necessary colours used in the Logo?
+<li>Will the logo be notice in impulse? Is there an eye-catch?
+<li>Doe the used type face fit the branch?
+<li>Is the logo incontestably under the Competition protection act
+<li>Is the logo discernible from a distance?
+<li>Is the logo discernible when up close?
+<li>Is this logo memorable?
+</ul>
+<p>
+The contest will be running the next six weeks on the pages of Macwelt. Sent in logos will be shown in the Macwel gallery area as well as on the Fink Website. Macwelt is hoping to provide the winner with a prize. 
+Accepted Formats for the sent in logo are TIFF as well as PDF. The pieces of artwork should be sent to redaktionATmacwelt.de with a subject of <b>Fink logo</b>. The Macwelt team will take it from there.
+<p>
+
+The Fink Project would like to thank Macwelt for their support, we are looking forward to <b>your</b> submission and hope that you will be amongst the winners.
+
+<?
+
+include "footer.inc";
+?>
+
