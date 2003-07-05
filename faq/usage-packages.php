0d248929694cf1134baa0b832e997689bc39f32e
Alexander Hansen <alexkhansen@users.sourceforge.net>
2003-07-05 21:25:43 +0000

    Maybe I only need a single commit now to do both the XML and the php?
    Anyway:
    
    updates to FAQ
    
    3.9:  added note that packages in unstable may well have dependencies in
    unstable.
    
    5.6.  NEW--explain cctools version error.

--- faq/usage-packages.php
+++ faq/usage-packages.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Usage (2)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2003/06/24 13:48:00';
+$cvs_date = 'Date: 2003/07/05 21:22:45';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="prev" href="usage-general.php" title="Package Usage Problems - General">';
 
