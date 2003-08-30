43bda848aa35bbe050e80323071a6e5186e48d11
Alexander Hansen <alexkhansen@users.sourceforge.net>
2003-08-30 20:55:38 +0000

    updated FAQ 5.6:  new upgrade path

--- faq/comp-general.php
+++ faq/comp-general.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Compiling (1)";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2003/08/24 22:35:00';
+$cvs_date = 'Date: 2003/08/30 20:55:38';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="next" href="comp-packages.php" title="Compile Problems - Specific Packages"><link rel="prev" href="usage-fink.php" title="Installing, Using and Maintaining Fink">';
 
@@ -13,7 +13,7 @@ include "header.inc";
 
 <a name="compiler">
 <div class="question"><p><b>Q4.1: A configure script complains
-that it can't find an &quot;acceptable cc&quot;. What's that?</b></p></div>
+that it can't find an "acceptable cc". What's that?</b></p></div>
 <div class="answer"><p><b>A:</b> 
 Read the docs next time.
 To compile packages from source, you must install the Developer Tools,
@@ -21,7 +21,7 @@ which among other stuff contains the C compiler, <code>cc</code>.
 </p></div>
 </a>
 <a name="cvs">
-<div class="question"><p><b>Q4.2: When I try a &quot;fink selfupdate-cvs&quot; I get this message: &quot;cvs: Command not found.&quot; </b></p></div>
+<div class="question"><p><b>Q4.2: When I try a "fink selfupdate-cvs" I get this message: "cvs: Command not found." </b></p></div>
 <div class="answer"><p><b>A:</b> You need to install the Developer Tools.</p></div>
 </a>
 <a name="make">
@@ -62,7 +62,7 @@ installation or install libwww after you installed Fink.
 <div class="answer"><p><b>A:</b> This occasionally happens with splitoff packages (i.e. the ones with -dev, -shlibs, etc.) when a file gets moved from one part of the splitoff to another (e.g. from <code>foo</code> to <code>foo-shlibs</code>.  What you can do is overwrite the file with that from the package you are trying to install (since they are nominally the same):</p><pre>sudo dpkg -i --force-overwite packagename</pre><p>where <b>packagename</b> is the package that you are trying to install.</p></div>
 </a>
 <a name="weak_lib">
-<div class="question"><p><b>Q4.6: After I installed the December 2002 Development Tools I get messages about &quot;weak libraries&quot;.</b></p></div>
+<div class="question"><p><b>Q4.6: After I installed the December 2002 Development Tools I get messages about "weak libraries".</b></p></div>
 <div class="answer"><p><b>A:</b> This is new with the December 2002 Tools.  You may occasionally see messages like (choosing libgdk-pixbuf as an example):</p><p>
 <code>ld: warning dynamic shared library: /sw/lib/libgdk-pixbuf.dylib not made a weak library in output with MACOSX_DEPLOYMENT_TARGET environment variable set to: 10.1</code>
 </p><p>You may regard these as harmless.  If you are curious, read through the release notes in the developer documentation directory, 
@@ -71,7 +71,7 @@ do with whether missing symbols at runtime is considered a fatal error on
 startup or not, for applications that use weak references.</p></div>
 </a>
 <a name="mv-failed">
-<div class="question"><p><b>Q4.7: What does &quot;execution of mv failed, exit code 1&quot; mean when I try to build a package?</b></p></div>
+<div class="question"><p><b>Q4.7: What does "execution of mv failed, exit code 1" mean when I try to build a package?</b></p></div>
 <div class="answer"><p><b>A:</b> It typically means that another error happened earlier in the build, and so one or more files weren't created, but the build process didn't stop.  To track down the offending file(s), search in the output of the build for the nonexistent file, e.g. if you have something like:</p><pre>mv /sw/src/root-foo-0.1.2-3/sw/lib/libbar*.dylib \
  /sw/src/root-foo-shlibs-0.1.2-3/sw/lib/
  mv: cannot stat `/sw/src/root-foo-0.1.2-3/sw/lib/libbar*.dylib': 
@@ -80,7 +80,7 @@ startup or not, for applications that use weak references.</p></div>
 Failed: installing foo-0.1.2-3 failed</pre><p>then you should look for <code>libbar</code> somewhere further back in the output of your build attempt.</p></div>
 </a>
 <a name="node-exists">
-<div class="question"><p><b>Q4.8: I can't install a package | update because I get a message that a &quot;node&quot; already exists.</b></p></div>
+<div class="question"><p><b>Q4.8: I can't install a package | update because I get a message that a "node" already exists.</b></p></div>
 <div class="answer"><p><b>A:</b> These errors look something like this:</p><pre>Failed: Internal error: node for system-xfree86 already exists</pre><p>This problem is that the dependency engine is confused, due to changes in some of the package info files.  To fix it:</p><ul>
 <li>
 <p>Remove the offending package by force, e. g.</p>
@@ -88,7 +88,7 @@ Failed: installing foo-0.1.2-3 failed</pre><p>then you should look for <code>lib
 <p>for the example given above.</p>
 </li>
 <li>
-<p>Try again to install | upgrade.  At some point a &quot;virtual dependency&quot; prompt will come up that includes the package you just removed.  Select it, and it will be reinstalled during your build.</p>
+<p>Try again to install | upgrade.  At some point a "virtual dependency" prompt will come up that includes the package you just removed.  Select it, and it will be reinstalled during your build.</p>
 </li>
 </ul></div>
 </a>
@@ -97,7 +97,7 @@ Failed: installing foo-0.1.2-3 failed</pre><p>then you should look for <code>lib
 <div class="answer"><p><b>A:</b> This is a frequent source of problems, because the package configuration script finds libraries under <code>/usr/local/lib</code> before searching in the Fink path.  If you are having problems with a build that aren't covered by another FAQ entry, you should check whether you have libraries in <code>/usr/local/lib</code>.  If so, then try renaming <code>/usr/local</code> to something else, e.g.:</p><pre>sudo mv /usr/local /usr/local.moved</pre><p>do your build, and then put <code>/usr/local</code> back:</p><pre>sudo mv /usr/local.moved /usr/local</pre></div>
 </a>
 <a name="toc-out-of-date">
-<div class="question"><p><b>Q4.10: When I try to build a package, I get a message that a &quot;table of contents&quot; is out of date.  What do I need to do?</b></p></div>
+<div class="question"><p><b>Q4.10: When I try to build a package, I get a message that a "table of contents" is out of date.  What do I need to do?</b></p></div>
 <div class="answer"><p><b>A:</b> The output hints at what to do.  The message is usually something like:</p><pre>ld: table of contents for archive: /sw/lib/libintl.a is out of date; rerun ranlib(1) (can't load from it)</pre><p>What you need to do is run ranlib (as root) on whatever library is causing the problem.  As an example, for the case above, you would run:</p><pre>sudo ranlib /sw/lib/libintl.a</pre></div>
 </a>
 <a name="fc-atlaas">
--- faq/index.php
+++ faq/index.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q.";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2003/08/24 22:35:00';
+$cvs_date = 'Date: 2003/08/30 20:55:38';
 
 $metatags = '<link rel="contents" href="index.php" title="F.A.Q. Contents"><link rel="next" href="general.php" title="General Questions">';
 
@@ -65,39 +65,39 @@ package found'. How can I install it?</a></li>
 <li><a href="usage-fink.php#sudo">3.10 I'm tired of typing my password into sudo again
 and again. Is there a way around this?</a></li>
 <li><a href="usage-fink.php#exec-init-csh">3.11 When I try to run
-init.csh, I get a &quot;Permission denied&quot; error. What am I doing
+init.csh, I get a "Permission denied" error. What am I doing
 wrong?</a></li>
 <li><a href="usage-fink.php#dselect-access">3.12 Help! I used the
-&quot;[A]ccess&quot; menu entry in dselect and now I can't download packages any
+"[A]ccess" menu entry in dselect and now I can't download packages any
 more!</a></li>
-<li><a href="usage-fink.php#cvs-busy">3.13 When I try to run <q>fink selfupdate</q> or &quot;fink selfupdate-cvs&quot;, I get the error &quot;<code>Updating using CVS failed. Check the error messages above.</code>&quot;</a></li>
+<li><a href="usage-fink.php#cvs-busy">3.13 When I try to run <q>fink selfupdate</q> or "fink selfupdate-cvs", I get the error "<code>Updating using CVS failed. Check the error messages above.</code>"</a></li>
 <li><a href="usage-fink.php#kernel-panics">3.14 When I use fink, my whole machine 
 freezes up/kernel panics/dies. Help!</a></li>
 <li><a href="usage-fink.php#not-found">3.15 I'm trying to install a package, but fink can't download it.  The download site shows a later version number of the package than what fink has.  What do I do?</a></li>
-<li><a href="usage-fink.php#fink-not-found">3.16 I've edited my .cshrc and started a new terminal, but I still get &quot;command not found&quot; errors when I run fink or anything that I installed with fink.</a></li>
+<li><a href="usage-fink.php#fink-not-found">3.16 I've edited my .cshrc and started a new terminal, but I still get "command not found" errors when I run fink or anything that I installed with fink.</a></li>
 <li><a href="usage-fink.php#invisible-sw">3.17 I want to hide /sw in the Finder to keep users from damaging the fink setup.</a></li>
-<li><a href="usage-fink.php#install-info-bad">3.18 I can't install anything, because I get the following error: &quot;install-info: unrecognized option `--infodir=/sw/share/info'&quot;</a></li>
-<li><a href="usage-fink.php#bad-list-file">3.19 I can't install or remove anything, because of a problem with a &quot;files list file&quot;.</a></li>
-<li><a href="usage-fink.php#error-nineteen">3.20 When I use the Fink binary installer package, I get a big &quot;19&quot; in the window and can't install anything.</a></li>
+<li><a href="usage-fink.php#install-info-bad">3.18 I can't install anything, because I get the following error: "install-info: unrecognized option `--infodir=/sw/share/info'"</a></li>
+<li><a href="usage-fink.php#bad-list-file">3.19 I can't install or remove anything, because of a problem with a "files list file".</a></li>
+<li><a href="usage-fink.php#error-nineteen">3.20 When I use the Fink binary installer package, I get a big "19" in the window and can't install anything.</a></li>
 <li><a href="usage-fink.php#dselect-garbage">3.21 I get a bunch of garbage when I select packages in <code>dselect</code>.  How can I use it?</a></li>
-<li><a href="usage-fink.php#perl-undefined-symbol">3.22 Why do I get a bunch of &quot;dyld: perl undefined symbols&quot; errors when I run Fink commands?</a></li>
+<li><a href="usage-fink.php#perl-undefined-symbol">3.22 Why do I get a bunch of "dyld: perl undefined symbols" errors when I run Fink commands?</a></li>
 <li><a href="usage-fink.php#cant-upgrade">3.23 I can't seem to update Fink's version.</a></li>
 <li><a href="usage-fink.php#spaces-in-directory">3.24 Can I put Fink in a volume or directory with a space in its name?</a></li>
 </ul>
 <li><a href="comp-general.php"><b>4 Compile Problems - General</b></a></li>
 <ul>
 <li><a href="comp-general.php#compiler">4.1 A configure script complains
-that it can't find an &quot;acceptable cc&quot;. What's that?</a></li>
-<li><a href="comp-general.php#cvs">4.2 When I try a &quot;fink selfupdate-cvs&quot; I get this message: &quot;cvs: Command not found.&quot; </a></li>
+that it can't find an "acceptable cc". What's that?</a></li>
+<li><a href="comp-general.php#cvs">4.2 When I try a "fink selfupdate-cvs" I get this message: "cvs: Command not found." </a></li>
 <li><a href="comp-general.php#make">4.3 make: illegal option -- C</a></li>
 <li><a href="comp-general.php#head">4.4 I'm getting a strange usage message
 from the head command. What's broken?</a></li>
 <li><a href="comp-general.php#also_in">4.5 When I try to install a package I get an error message about trying to overwrite a file that is in another package.</a></li>
-<li><a href="comp-general.php#weak_lib">4.6 After I installed the December 2002 Development Tools I get messages about &quot;weak libraries&quot;.</a></li>
-<li><a href="comp-general.php#mv-failed">4.7 What does &quot;execution of mv failed, exit code 1&quot; mean when I try to build a package?</a></li>
-<li><a href="comp-general.php#node-exists">4.8 I can't install a package | update because I get a message that a &quot;node&quot; already exists.</a></li>
+<li><a href="comp-general.php#weak_lib">4.6 After I installed the December 2002 Development Tools I get messages about "weak libraries".</a></li>
+<li><a href="comp-general.php#mv-failed">4.7 What does "execution of mv failed, exit code 1" mean when I try to build a package?</a></li>
+<li><a href="comp-general.php#node-exists">4.8 I can't install a package | update because I get a message that a "node" already exists.</a></li>
 <li><a href="comp-general.php#usr-local-libs">4.9 I've heard that libraries installed in /usr/local/lib sometimes cause build problems for Fink.  Is this true?</a></li>
-<li><a href="comp-general.php#toc-out-of-date">4.10 When I try to build a package, I get a message that a &quot;table of contents&quot; is out of date.  What do I need to do?</a></li>
+<li><a href="comp-general.php#toc-out-of-date">4.10 When I try to build a package, I get a message that a "table of contents" is out of date.  What do I need to do?</a></li>
 <li><a href="comp-general.php#fc-atlaas">4.11 Fink Commander hangs when I try to install atlas.</a></li>
 <li><a href="comp-general.php#basic-headers">4.12 I get messages saying that I'm missing stddef.h.  Where do I find it?</a></li>
 </ul>
@@ -107,15 +107,15 @@ from the head command. What's broken?</a></li>
 <li><a href="comp-packages.php#cant-install-xfree">5.2 I want to switch to fink's xfree86 packages, but I can't install xfree86-base, because it conflicts with system-xfree86.</a></li>
 <li><a href="comp-packages.php#change-thread-nothread">5.3 How do I change from the non-threaded version of fink's XFree86 packages to the threaded version (or vice-versa)?</a></li>
 <li><a href="comp-packages.php#apple-x11">5.4 I've installed the Apple X11 package, but system-xfree86 won't install.</a></li>
-<li><a href="comp-packages.php#cctools">5.5 &quot;When I try to install KDE, I get the following message:  'Can't resolve dependency &quot;cctools (&gt;= 446-1)&quot;'</a></li>
+<li><a href="comp-packages.php#cctools">5.5 "When I try to install KDE, I get the following message:  'Can't resolve dependency "cctools (&gt;= 446-1)"'</a></li>
 <li><a href="comp-packages.php#system-xfree86-upgrade">5.6 I can't update system-xfree86, because of a conflict with x11.</a></li>
 </ul>
 <li><a href="usage-general.php"><b>6 Package Usage Problems - General</b></a></li>
 <ul>
 <li><a href="usage-general.php#xlocale">6.1 I'm getting lots of messages
-like &quot;locale not supported by C library&quot;. Is that bad?</a></li>
+like "locale not supported by C library". Is that bad?</a></li>
 <li><a href="usage-general.php#passwd">6.2 There are suddenly a number of 
-strange users on my system, with names like &quot;mysql&quot;, &quot;pgsql&quot;, and &quot;games&quot;.  
+strange users on my system, with names like "mysql", "pgsql", and "games".  
 Where did they come from?</a></li>
 <li><a href="usage-general.php#compile-myself">6.3 How do I compile something
 myself using fink-installed software?</a></li>
@@ -129,17 +129,17 @@ XMMS</a></li>
 <li><a href="usage-packages.php#nedit-window-locks">7.2 If I am editing a file in nedit, when I open another file its window pops up but is unresponsive.</a></li>
 <li><a href="usage-packages.php#xdarwin-start">7.3 Help! When I start
 XDarwin, it immediately quits!</a></li>
-<li><a href="usage-packages.php#no-server">7.4 When I try to start XDarwin I get the message &quot;xinit:  No such file or directory (errno 2):  no server &quot;/usr/X11R6/bin/X&quot; in PATH&quot;.</a></li>
+<li><a href="usage-packages.php#no-server">7.4 When I try to start XDarwin I get the message "xinit:  No such file or directory (errno 2):  no server "/usr/X11R6/bin/X" in PATH".</a></li>
 <li><a href="usage-packages.php#xfree-keymapping">7.5 I just upgraded to Mac
 OS X 10.1 and now XFree86 always quits immediately. In the messages it
-says &quot;assert failed on line 454 of darwinKeyboard.c!&quot;. What's
+says "assert failed on line 454 of darwinKeyboard.c!". What's
 wrong?</a></li>
-<li><a href="usage-packages.php#xterm-error">7.6 xterm fails with &quot;dyld: xterm Undefined symbols:  xterm undefined reference to _tgetent expected to be defined in /usr/lib/libSystem.B.dylib&quot;.</a></li>
-<li><a href="usage-packages.php#libXmuu">7.7 When I try to start XFree86 I get one of the following errors:  &quot;dyld: xinit can't open library: /usr/X11R6/lib/libXmuu.1.dylib&quot; or &quot;dyld: xinit can't open library:  /usr/X11R6/lib/libXext.6.dylib&quot;</a></li>
+<li><a href="usage-packages.php#xterm-error">7.6 xterm fails with "dyld: xterm Undefined symbols:  xterm undefined reference to _tgetent expected to be defined in /usr/lib/libSystem.B.dylib".</a></li>
+<li><a href="usage-packages.php#libXmuu">7.7 When I try to start XFree86 I get one of the following errors:  "dyld: xinit can't open library: /usr/X11R6/lib/libXmuu.1.dylib" or "dyld: xinit can't open library:  /usr/X11R6/lib/libXext.6.dylib"</a></li>
 <li><a href="usage-packages.php#apple-x-bugs">7.8 I had Fink's XFree86 installed, and I've replaced it with Apple's X11, and now everything's crashing!</a></li>
 <li><a href="usage-packages.php#apple-x-delete">7.9 I want the delete key in Apple's X11.app to behave like that in XDarwin.</a></li>
 </ul>
-</ul><p>Generated from <i>$Fink: faq.xml,v 1.125 2003/08/24 22:35:00 alexkhansen Exp $</i></p>
+</ul><p>Generated from <i>$Fink: faq.xml,v 1.126 2003/08/30 20:55:38 alexkhansen Exp $</i></p>
 
 
 <?
