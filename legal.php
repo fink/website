42c8fde54e2e618d60c707f864360388281412b8
David R. Morrison <dmrrsn@users.sourceforge.net>
2004-01-01 18:57:59 +0000

    update Copyright

--- legal.php
+++ legal.php
@@ -1,7 +1,7 @@
 <?
 $title = "Legal Stuff";
 $cvs_author = '$Author: dmrrsn $';
-$cvs_date = '$Date: 2003/01/18 21:54:18 $';
+$cvs_date = '$Date: 2004/01/01 18:57:59 $';
 
 include "header.inc";
 ?>
@@ -15,7 +15,7 @@ Some legal rumblings to keep lawyers happy:
 
 <p>
 The material on this website is Copyright &copy; 2001 Christoph Pfisterer,
-Copyright &copy; 2001-2003 The Fink Project, unless noted otherwise.
+Copyright &copy; 2001-2004 The Fink Project, unless noted otherwise.
 If you want to reuse material from this website,
 <a href="mailto:fink-develATlistsDOTsourceforgeDOTnet">talk to us</a> first.
 </p>
