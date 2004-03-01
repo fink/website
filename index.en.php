5941e75ecea2ad6ce6a20b357e5af2e62a39d51f
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2004-03-01 23:29:41 +0000

    cp index.php index.en.php; now you may replaces index.php to include phpLang

---
+++ index.en.php
@@ -0,0 +1,101 @@
+<?
+$title = "Home";
+$cvs_author = '$Author: babayoshihiko $';
+$cvs_date = '$Date: 2004/03/01 23:29:41 $';
+$is_home = 1;
+
+$metatags = '<meta name="description" content="Fink, a distribution of Unix software for Mac OS X and Darwin">
+<meta name="keywords" content="Mac OS X, Darwin, GNU, Unix, GNOME, KDE, software, distribution, Fink">
+';
+
+include "header.inc";
+?>
+
+
+<p>
+The Fink project wants to bring the full world of Unix
+<a href="http://www.opensource.org/">Open Source</a> software to
+<a href="http://www.opensource.apple.com/">Darwin</a> and
+<a href="http://www.apple.com/macosx/">Mac OS X</a>.
+We modify Unix software so that it compiles and runs on Mac OS X
+("port" it) and make it available for download as a coherent
+distribution.
+Fink uses <a href="http://www.debian.org/">Debian</a> tools like dpkg
+and apt-get to provide powerful binary package management.
+You can choose whether you want to download precompiled binary
+packages or build everything from source.
+<a href="about.php">Read more...</a>
+</p>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr valign="top"><td width="50%">
+
+<h1>News</h1>
+
+<?
+// Include news items
+include $fsroot."news/news.inc";
+?>
+<div align="right"><a href="<? print $root; ?>news/index.php">Older News...</a></div>
+
+
+</td><td>&nbsp;&nbsp;&nbsp;</td><td width="50%">
+
+<h1>Status</h1>
+<? 
+include "fink_version.inc";
+?>
+
+<p>
+Fink <? print $fink_version ?> was released on <? print $release_date ?>.  
+The release includes source and binary packages as well as a binary installer.
+This release was built on OS X 10.2 with the gcc 3.3 compiler, and should
+run on OS X 10.3 as well.
+</p>
+
+<h1>Resources</h1>
+
+<p>
+If you're looking for support, check out the <a
+href="help/index.php">help page</a>.
+That page also lists various options to help the project and submit
+feedback.
+</p>
+
+<p>
+The Fink project is hosted by
+<a href="http://sourceforge.net/">SourceForge</a>.
+In addition to hosting this site and the downloads, SourceForge
+provides the following resources for the project:
+</p>
+<ul>
+<li><a href="http://sourceforge.net/projects/fink/">SourceForge project summary page</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=117203&amp;group_id=17203">Report or view bugs</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=371315&amp;group_id=17203">Request a package that's not in Fink</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=367203&amp;group_id=17203">Request a feature that's not in fink (the program)</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=414256&amp;group_id=17203">Submit a new Fink package (non-core developers)</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=317203&amp;group_id=17203">Submit a patch for fink (the program)</a></li>
+<li><a href="lists/index.php">Mailing lists</a></li>
+<li>CVS (<a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/fink">browse
+online</a>, <a href="doc/cvsaccess/index.php">access instructions</a>)</li>
+</ul>
+<p>
+Please note that to use some of these resources (ie, to report a bug or request a new Fink package), you
+will need to be logged in to your SourceForge account.  If you do not have one, you can sign up for one
+for free on the <a href="http://sourceforge.net/">SourceForge web site</a>.
+</p>
+
+</td></tr></table>
+
+<script type="text/javascript" language="JavaScript" src="http://db3.net-filter.com/script/13500.js"></script>
+<noscript><img src="http://db3.net-filter.com/db.php?id=13500&amp;page=unknown" alt=""></noscript>
+
+<?
+include "footer.inc";
+?>
