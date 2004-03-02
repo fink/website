deac26625816e453895a5b03b02b29341ed65b60
Jeff Ye <jeff_yecn@users.sourceforge.net>
2004-03-02 03:15:42 +0000

    Finished the Chinese version of Homepage, Documentation index page and partially the FAQs xml file

---
+++ doc/index.zh.php
@@ -0,0 +1,42 @@
+<?
+$title = "";
+$cvs_author = 'Author: fingolfin';
+$cvs_date = 'Date: 2004/02/29 22:31:41';
+include_once 'nav.inc';
+$fsroot = $root = '../';
+include_once '../header.inc'; 
+?><h1>Fink － 文档</h1><p>Generated from <i>$Id: index.zh.php,v 1.1 2004/03/02 03:15:43 jeff_yecn Exp $</i></p>
+<p>
+这里收集了为 Fink 编写的各种文档。
+有些文档可能会对使用 Mac OS X 的用户有用，或那些没有使用 Fink 但又向学习如何移植 Unix 软件的 Darwin 用户有用。
+</p>
+<h2><a name="userdoc">用户文档</a></h2>
+
+<p>
+当前 Fink 的用户文档：
+</p>
+<ul>
+<li><a href="users-guide/index.php">Fink 用户指南</a> － 它涉及如何安装 Fink 自身，安装软件包，和升级到新的 Fink 版本。它包括对使用源代码版本和二进制版本的使用说明。
+<b>正在编写中！</b></li>
+<li><a href="x11/index.php">在 Darwin 和 Mac OS X 上运行 X11</a> － 涉及有关概念，安装和启动（同时面向 Darwin 和 Mac OS X 用户）</li>
+</ul>
+
+<p>
+很多更完整的文档，但稍微有点过期而且不再被维护：
+</p>
+<ul>
+<li><a href="bundled/install.php">安装和升级</a> －　如何安装 Fink 或升级到新的版本</li>
+<li><a href="bundled/usage.php">Usage</a> － 如何使用 Fink 及其安装的软件</li>
+<li><a href="bundled/readme.php">Fink 自述</a> － 源代码发布版本的自述文件</li>
+<li><a href="cvsaccess/index.php">CVS 访问</a> － 如何访问 Fink CVS 库来在新版本发布前获取最新源代码包。</li>
+</ul>
+
+<h2><a name="developerdoc">开发者文档</a></h2>
+
+<ul>
+<li><a href="http://fink.sourceforge.net/doc/UsingFink.pdf">使用 Fink：开发者使用指南</a> (2MB pdf 文件，英文版) － 一组用于 <a href="http://conferences.oreillynet.com/macosx2002/">O'Reilly Mac OS X 大会</a>上演示的幻灯片（同时也有 <a href="http://conferences.oreillynet.com/presentations/macosx02/morrison_david.ppt">PowerPoint 文件</a>版本）</li>
+<li><a href="porting/index.php">移植技巧提示</a> － 关于如何移植 Unix 软件到 Darwin 上的资料</li>
+<li><a href="packaging/index.php">软件打包手册</a> － 如何创建和维护 Fink 软件包</li>
+</ul>
+
+<? include_once "../footer.inc"; ?>
---
+++ index.zh.php
@@ -0,0 +1,91 @@
+<?
+$title = "Home";
+$cvs_author = '$Author: jeff_yecn $';
+$cvs_date = '$Date: 2004/03/02 03:15:42 $';
+$is_home = 1;
+
+$metatags = '<meta name="description" content="Fink, 一个 Mac OS X 和 Darwin 上的 Unix 软件发布系统">
+<meta name="keywords" content="Mac OS X, Darwin, GNU, Unix, GNOME, KDE, 软件, 发布, Fink">
+';
+
+include "header.inc";
+?>
+
+
+<p>
+Fink 项目希望把 Unix 上各种<a href="http://www.opensource.org/">开放源码</a>软件带到
+<a href="http://www.opensource.apple.com/">Darwin</a> 和
+<a href="http://www.apple.com/macosx/">Mac OS X</a> 平台上。
+我们通过修改 Unix 软件使得它可以在 Mac OS X 上编译和运行（“移植”）,并提供一个方便的分发系统使得每个人都可以下载和使用它。
+Fink 使用 <a href="http://www.debian.org/">Debian</a> 中的象 dpkg
+和 apt-get 等工具来提供强大的二进制软件包管理。
+你可以随意选择是下载预编译好的二进制安装包或从源代码自己构建一切。
+<a href="about.php">阅读更多信息…</a>
+</p>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr valign="top"><td width="50%">
+
+<h1>新闻</h1>
+
+<?
+// Include news items
+include $fsroot."news/news.inc";
+?>
+<div align="right"><a href="<? print $root; ?>news/index.php">以前的消息…</a></div>
+
+
+</td><td>&nbsp;&nbsp;&nbsp;</td><td width="50%">
+
+<h1>当前状况</h1>
+<? 
+include "fink_version.inc";
+?>
+
+<p>
+Fink <? print $fink_version ?> 已经于 <? print $release_date ?> 发布。 
+它包括源代码和二进制文件两种发行方式，同时也包括二进制形式可执行的安装程序。
+本版本在 OS X 10.2 使用 gcc 3.3 编译器构建，但应该也可以在 OS X 10.3 上运行。
+
+<h1>资源</h1>
+
+<p>
+如果你在寻求技术支持的话，请访问 <a
+href="help/index.php">帮助页面</a>。
+帮助页面还列有支持本项目的几种方法以及如何提交反馈。
+</p>
+
+<p>
+Fink 项目建立于
+<a href="http://sourceforge.net/">SourceForge</a>中。
+除了提供本网站的主机服务以及下载支持外，SourceForge
+还为本项目提供下面的资源：
+</p>
+<ul>
+<li><a href="http://sourceforge.net/projects/fink/">SourceForge 项目概述页面</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=117203&amp;group_id=17203">汇报和检视软件缺陷</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=371315&amp;group_id=17203">请求一个还不在 Fink 中的软件包</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=367203&amp;group_id=17203">请求一个 fink（程序）中还没有的功能</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=414256&amp;group_id=17203">提交一个新的 Fink 软件包（非核心开发者）</a></li>
+<li><a
+href="http://sourceforge.net/tracker/?atid=317203&amp;group_id=17203">提交 fink（程序）的一个补丁。</a></li>
+<li><a href="lists/index.php">邮件列表</a></li>
+<li>CVS (<a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/fink">在线浏览</a>, <a href="doc/cvsaccess/index.php">如何访问</a>)</li>
+</ul>
+<p>
+请注意：要使用其中的一些资源（比如，汇报一个软件缺陷或请求一个新的 Fink 软件包），你需要先登录进你的 SourceForge 帐号。如果你现在还没有帐号，你可以在 <a href="http://sourceforge.net/">SourceForge 网站</a>免费注册一个。
+</p>
+
+</td></tr></table>
+
+<script type="text/javascript" language="JavaScript" src="http://db3.net-filter.com/script/13500.js"></script>
+<noscript><img src="http://db3.net-filter.com/db.php?id=13500&amp;page=unknown" alt=""></noscript>
+
+<?
+include "footer.inc";
+?>
