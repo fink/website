9d2705f4a47361a3dbf7b149a516fc9db701c671
Darian Lanx <dmalloc@users.sourceforge.net>
2004-03-02 17:22:48 +0000

    Testing German

---
+++ people.de.php
@@ -0,0 +1,150 @@
+<?
+$title = "Contributors";
+$cvs_author = '$Author: dmalloc $';
+$cvs_date = '$Date: 2004/03/02 17:22:48 $';
+
+include "header.inc";
+?>
+
+<h1>Mitwirkende</h1>
+
+<p>Fink wird von vielen freiwilligen Mitarbeitern betreut. Jedes Paket in Fink wir von einem sogenannten 'Maintainer' verwaltet. Diese Person kümmert sich darum daß dieses Paket so arbeitet wie man es erwarten würde, außerdem kümmert sich diese Person auch um Fehler die berichtigt werden müssen. Es gibt Menschen die sich um das Programm 'fink' kümmern, dieser Web-Auftritt muß betreut werden und viele Fink Nutzer brauchen Hilfe im täglichen Umgang mit Fink. Ein Großteil dieser Hilfe wird von unseren Mailing-Listen getragen sowie dem Chat im IRC. 
+Die oben genannten Aufgaben, aber auch viele andere benötigen eine Menge Zeit und Aufmerksamkeit. Diese Seite dient dazu sich genau bei diesen Personen zu bedanken, denn ohne unsere fleißigen freiwilligen Helfer wäre es nicht möglich dieses Projekt am Leben zu erhalten.
+</p>
+
+<p>
+Im laufe der Zeit hat Fink soviel Hilfe erfahren, daß es schwierig ist alle Personen hier aufzulisten. Wenn Sie der Meinung sind, daß Sie einem substantiellen Beitrag zu Fink geleistet haben, dann teilen Sie uns dies bitte per Mail mit und in der nächsten Revision dieses Dokumentes wird auch Ihr Name nicht fehlen.
+</p>
+<p>Wörter die durch Kursivschrift hervorgehoben sind stehen für den Nicknamen im IRC.</p>
+
+<h2>Ehemaliger Projektleiter</h2>
+<ul>
+<li>Christoph Pfisterer (<i>chrisp</i>)</li>
+</ul>
+
+<h2>Augenblickliche Projektleiter</h2>
+<ul>
+<li>Max Horn (<i>Fingolfin</i>)</li>
+<li>David R. Morrison (<i>drm</i>)</li>
+<li>Benjamin Reed (<i>RangerRick</i>)</li>
+<li>Peter O'Gorman (<i>pogma</i>)</li>
+</ul>
+
+<h2>Administrativa und Öffentlichkeitsarbeit</h2>
+<ul>
+<li>David Höhn aka. Darian Lanx (<i>dmalloc</i>)</li>
+</ul>
+
+<h2>Ständige freiwillige Mitarbeiter</h2>
+
+<ul>
+<li>Rob Braun (<i>bbraun</i>)</li>
+<li>Keith Conger (<i>kconger</i>)</li>
+<li>Martin Costabel</li>
+<li>Sylvain Cuaz</li>
+<li>Finlay Dobbie (<i>AngusD</i>)</li>
+<li>Justin F. Hallett (<i>TheSin</i>)</li>
+<li>Jeremy Higgs</li>
+<li>Ben Hines (<i>Clef</i>)</li>
+<li>Carsten Klapp</li>
+<li>Thomas Kotzian</li> 
+<li>Daniel Macks (<i>dmacks</i>)</li>
+<li>Matthias Neeracher (<i>Murr</i>)</li>
+<li>Christian Schaffner (<i>chris01</i>)</li>
+<li>Masanori Sekino (<i>msek</i>)</li>
+<li>Matt Stephenson</li>
+<li>Michael G. Schwern</li>
+<li>Alexander Strange (<i>Feanor</i>)</li>
+<li>Christian Swinehart</li>
+<li>Dave Vasilevsky (<i>vasi</i>)</li>
+<li>Jeffrey S. Whitaker</li>
+<li>Chris Zubrzycki (<i>cirdan</i>)</li>
+
+
+</ul>
+
+<h2>Support, Übersetzung und Dokumentation Team</h2>
+<ul>
+<li>Baba Yoshihiko (<i>baba</i>)</li> <!-- i18n and l10n -->
+<li>Hisashi Todd Fujinaka (<i>htodd</i>)</li> <!-- doc writer -->
+<li>Alexander Hansen</li> <!-- support work on mailing lists! -->
+</ul>
+
+<h2>Gelegentlich Mitwirkende</h2>
+
+<ul>
+<li>Jorge Acereda</li> 
+<li>Mike Akers</li> 
+<li>David Bacher</li> 
+<li>Michael Baudis</li> 
+<li>Chris Black</li> 
+<li>Bill Bumgarner</li> 
+<li>Yves de Champlain</li>
+<li>Avram Cherry</li> 
+<li>Jon Dugan</li> 
+<li>Jeffrey Ellis</li> 
+<li>Jeremy Erwin</li> 
+<li>Benjamin Esham</li> 
+<li>Matthew Flanagan</li> 
+<li>Chris Frank</li> 
+<li>Landon Fuller (<i>landonf</i>)</li>
+<li>Paul Gaborit</li> 
+<li>Stanton Gallegos (<i>stantron</i>)</li> 
+<li>James Gibbs</li>
+<li>Joe Gorse (<i>inca34</i>)</li>
+<li>Jake Goulding</li> 
+<li>Richard Graul</li>
+<li>Matthew Harris</li> 
+<li>Shawn Hsiao (<i>shawnh</i>)</li>
+<li>Sam Johnston</li> 
+<li>Richard Kiss</li> 
+<li>Eric Knauel</li> 
+<li>Killian Koepsell</li> 
+<li>Eric Knauel</li>
+<li>Bernd Kuemmerlen</li> 
+<li>Christopher League</li> 
+<li>Adam Lebsack</li> 
+<li>William McCallum</li> 
+<li>Michael McKibben</li>
+<li>Mathias Meyer</li>
+<li>Kyle Moffett</li> 
+<li>David H. Munro</li> 
+<li>Joseph K. Myers</li> 
+<li>Kaben Nanlohy</li> 
+<li>Magenta Hari Nezumi</li> 
+<li>Eric Norum</li> 
+<li>Daniel Parks</li> 
+<li>Chris Pepper</li> 
+<li>Sebastien Pierre</li> 
+<li>Damien Pollet</li> 
+<li>Brian Redman</li> 
+<li>Rudy Richter (<i>rudy</i>)</li> 
+<li>Stefano Rodriguez</li> 
+<li>Andrew Rohl</li> 
+<li>David Ross</li> 
+<li>Michel Salim (<i>Hayashi-M</i>)</li>
+<li>Michel Schinz</li> 
+<li>Chris Shabsin</li> 
+<li>Dustin Sias (<i>dsias</i>)</li>
+<li>Leigh Smith</li> 
+<li>Daniel Sohl</li> 
+<li>Lindsey Spratt</li>
+<li>Damian Steer</li> 
+<li>Mark Stosberg</li> 
+<li>Tim Seufert</li>
+<Li>Paul Swenson</li> 
+<li>Patrick Tescher</li> 
+<li>Andrei Verovski</li> 
+<li>Aaron Voisine</li> 
+<li>Dominik Westner</li> 
+<li>Ken Williams</li> 
+<li>William Dell Wisner</li>
+<li>Ashley Yakeley</li> 
+<li>Tom Zerucha</li> 
+<li>Sasha Zucker</li> 
+</ul>
+
+
+<?
+include "footer.inc";
+?>
