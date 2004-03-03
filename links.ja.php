bfa4a5ea02e7a9f052ede7db8cd868ec338bf8ed
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2004-03-03 13:28:12 +0000

    Adding some Japanese versions.  But how do we add contributors?  Will we modify every time we add a person, or separate the list?

---
+++ links.ja.php
@@ -0,0 +1,85 @@
+<?
+$title = "リンク集";
+$cvs_author = '$Author: babayoshihiko $';
+$cvs_date = '$Date: 2004/03/03 13:28:12 $';
+
+include "header.inc";
+?>
+
+
+<h1>リンク集</h1>
+
+<p>
+タイトルの通り、リンク集です。
+リンクは別ウィンドウで開きます。
+断りがない限り、全て英語のサイトです。
+</p>
+
+<p>Fink 関連</p>
+<ul>
+<li><a href="http://finkmirrors.net/" target="_blank">Fink Mirrors</a> - 
+Fink ミラーの登録方法と現在のミラーのステータスの情報があります。</li>
+<li><a href="http://finkcommander.sourceforge.net/" target="_blank">FinkCommander</a> -
+Fink の GUI です。</li>
+<li><a href="http://fink.aquaflux.org/" target="_blank">FinkBot</a> -
+#fink IRC チャンネルの過去ログと統計。</li>
+<li><a href="http://www.funwithfink.com/" target="_blank">FunWithFink</a> -
+非公式の Fink CD とFink tips の本を出版している。</li>
+<li><a href="http://www.antcrc.utas.edu.au/models/macosx/Fink_on_NFS/" target="_blank">Fink on NFS</a> -
+Fink パッケージをネットワーククライアントに NFS で配布するスクリプト</li>
+</ul>
+
+<p>オープンソースソフトウェア</p>
+<ul>
+<li><a href="http://www.gnu.org/" target="_blank">GNU Software</a> -
+FSF と GNU ライセンスの情報もあります。</li>
+<li><a href="http://www.opensource.org/" target="_blank">Open Source Initiative</a> - 
+オープンソースに関する一般的な情報 (特にライセンス)</li>
+<li><a href="http://freshmeat.net/" target="_blank">Freshmeat</a> -
+最大のオープンソースソフトウェアディレクトリ</li>
+<li><a href="http://developer.apple.com/darwin/" target="_blank">Darwin</a>
+Apple のオープンソースプロジェクト</li>
+<li><a href="http://opendarwin.org/" target="_blank">OpenDarwin.org</a> -
+ダーウィンの開発
+</li>
+<li><a href="http://oroborosx.sourceforge.net/" target="_blank">OroborOSX</a> -
+X11 と Mac OS X を統合しようとしている X11 ウィンドウマネージャ。
+</ul>
+
+<p>似ているプロジェクト</p>
+<ul>
+<li><a href="http://gnu-darwin.sourceforge.net/" target="_blank">The GNU-Darwin Distribution</a></li>
+<li><a href="http://elisa.utopianet.net/~rlucia/devel/darwin_ports/" target="_blank">Darwin Ports</a> - 
+FreeBSD の ports collection をポーとしている。</li>
+<li><a href="http://openpackages.org/" target="_blank">OpenPackages</a> - 
+*BSD の port collections を統合、改善している。</li>
+</ul>
+
+<p>ポートされたパッケージ集</p>
+<ul>
+<li><a href="http://www.caos.aamu.edu/pub/MacOS_X/" target="_blank">http://www.caos.aamu.edu/pub/MacOS_X/</a> - 
+Marius Schamschula のサイト</li>
+<li><a href="http://www.osxgnu.org/" target="_blank">The GNU Mac OS X Public Archive</a></li>
+<li><a href="http://www.geeklair.net/downloads/" target="_blank">http://www.geeklair.net/downloads/</a> - 
+<a href="http://www.geeklair.net/~dluke/" target="_blank">Daniel Luke</a> のサイト</li>
+<li><a href="http://macosx.forked.net/" target="_blank">http://macosx.forked.net/</a></li>
+<li><a href="http://www-jlc.kek.jp/~fujiik/macosx/10.1.X/" target="_blank">HEP on X</a> - 
+RPM パッケージのコレクション</li>
+</ul>
+
+<p>関連情報</p>
+<ul>
+<li><a href="http://www.darwinfo.org/" target="_blank">Darwinfo</a> -
+ポートに関する情報やニュース。ソフトウェアアーカイブもある。</li>
+<li><a href="http://www.mrcla.com/XonX/" target="_blank">X on X</a> -
+XFree86 を Darwin や Mac OS X で使う。</li>
+<li><a href="http://www.stepwise.com/" target="_blank">StepWise</a> -
+ニュース、記事、ソフトウェアアーカイブ。</li>
+<li><a href="http://www.macgimp.org/" target="_blank">MacGimp</a> -
+Mac 用の GIMP、GTK、X のサイト</li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
---
+++ people.ja.php
@@ -0,0 +1,161 @@
+<?
+$title = "貢献している人々";
+$cvs_author = '$Author: babayoshihiko $';
+$cvs_date = '$Date: 2004/03/03 13:28:12 $';
+
+include "header.inc";
+?>
+
+<h1>貢献している人々</h1>
+
+<p>
+Fink プロジェクトはボランティアによって成り立っています。
+それぞれの Fink パッケージは、メンテナが力を尽くして動くようにしているのです。
+また、 Fink のプログラム自体を作っている人たちもいます。
+ウェブサイトもこうして最新に維持されています。
+サポートは、メーリングリストや IRC チャンネルで行っています。
+こういった作業はどれも大変な作業で、多くの時間を割いてくれていますので、「ありがとう」を言いたいと思います。
+以下は Fink に貢献してくれた人々のほんの一部です。
+</p>
+
+<p>
+非常に多くの人々が Fink を助けてくれたため、全てをあげるのは困難です。
+もし自分がここに載るに値するのであれば、メールを送って下さい。
+次までに掲載の検討をいたします。
+</p>
+
+<p>
+斜体字は IRC チャンネルでのニックネームです。
+</p>
+
+<h2>前プロジェクトリーダー</h2>
+<ul>
+<li>Christoph Pfisterer (<i>chrisp</i>)</li>
+</ul>
+
+<h2>プロジェクトリーダー</h2>
+<ul>
+<li>Max Horn (<i>Fingolfin</i>)</li>
+<li>David R. Morrison (<i>drm</i>)</li>
+<li>Benjamin Reed (<i>RangerRick</i>)</li>
+<li>Peter O'Gorman (<i>pogma</i>)</li>
+</ul>
+
+<h2>Administrativa and PR.</h2>
+<ul>
+<li>David H&#246;hn aka. Darian Lanx (<i>dmalloc</i>)</li>
+</ul>
+
+<h2>貢献者</h2>
+
+<ul>
+<li>Rob Braun (<i>bbraun</i>)</li>
+<li>Keith Conger (<i>kconger</i>)</li>
+<li>Martin Costabel</li>
+<li>Sylvain Cuaz</li>
+<li>Finlay Dobbie (<i>AngusD</i>)</li>
+<li>Justin F. Hallett (<i>TheSin</i>)</li>
+<li>Jeremy Higgs</li>
+<li>Ben Hines (<i>Clef</i>)</li>
+<li>Carsten Klapp</li>
+<li>Thomas Kotzian</li> 
+<li>Daniel Macks (<i>dmacks</i>)</li>
+<li>Matthias Neeracher (<i>Murr</i>)</li>
+<li>Christian Schaffner (<i>chris01</i>)</li>
+<li>Masanori Sekino (<i>msek</i>)</li>
+<li>Matt Stephenson</li>
+<li>Michael G. Schwern</li>
+<li>Alexander Strange (<i>Feanor</i>)</li>
+<li>Christian Swinehart</li>
+<li>Dave Vasilevsky (<i>vasi</i>)</li>
+<li>Jeffrey S. Whitaker</li>
+<li>Chris Zubrzycki (<i>cirdan</i>)</li>
+
+
+</ul>
+
+<h2>サポート、翻訳、ドキュメンテーションチーム</h2>
+<ul>
+<li>Baba Yoshihiko (<i>baba</i>)</li> <!-- i18n and l10n -->
+<li>Hisashi Todd Fujinaka (<i>htodd</i>)</li> <!-- doc writer -->
+<li>Alexander Hansen</li> <!-- support work on mailing lists! -->
+</ul>
+
+<h2>Occasional Contributors</h2>
+
+<ul>
+<li>Jorge Acereda</li> 
+<li>Mike Akers</li> 
+<li>David Bacher</li> 
+<li>Michael Baudis</li> 
+<li>Chris Black</li> 
+<li>Bill Bumgarner</li> 
+<li>Yves de Champlain</li>
+<li>Avram Cherry</li> 
+<li>Jon Dugan</li> 
+<li>Jeffrey Ellis</li> 
+<li>Jeremy Erwin</li> 
+<li>Benjamin Esham</li> 
+<li>Matthew Flanagan</li> 
+<li>Chris Frank</li> 
+<li>Landon Fuller (<i>landonf</i>)</li>
+<li>Paul Gaborit</li> 
+<li>Stanton Gallegos (<i>stantron</i>)</li> 
+<li>James Gibbs</li>
+<li>Joe Gorse (<i>inca34</i>)</li>
+<li>Jake Goulding</li> 
+<li>Richard Graul</li>
+<li>Matthew Harris</li> 
+<li>Shawn Hsiao (<i>shawnh</i>)</li>
+<li>Sam Johnston</li> 
+<li>Richard Kiss</li> 
+<li>Eric Knauel</li> 
+<li>Killian Koepsell</li> 
+<li>Eric Knauel</li>
+<li>Bernd Kuemmerlen</li> 
+<li>Christopher League</li> 
+<li>Adam Lebsack</li> 
+<li>William McCallum</li> 
+<li>Michael McKibben</li>
+<li>Mathias Meyer</li>
+<li>Kyle Moffett</li> 
+<li>David H. Munro</li> 
+<li>Joseph K. Myers</li> 
+<li>Kaben Nanlohy</li> 
+<li>Magenta Hari Nezumi</li> 
+<li>Eric Norum</li> 
+<li>Daniel Parks</li> 
+<li>Chris Pepper</li> 
+<li>Sebastien Pierre</li> 
+<li>Damien Pollet</li> 
+<li>Brian Redman</li> 
+<li>Rudy Richter (<i>rudy</i>)</li> 
+<li>Stefano Rodriguez</li> 
+<li>Andrew Rohl</li> 
+<li>David Ross</li> 
+<li>Michel Salim (<i>Hayashi-M</i>)</li>
+<li>Michel Schinz</li> 
+<li>Chris Shabsin</li> 
+<li>Dustin Sias (<i>dsias</i>)</li>
+<li>Leigh Smith</li> 
+<li>Daniel Sohl</li> 
+<li>Lindsey Spratt</li>
+<li>Damian Steer</li> 
+<li>Mark Stosberg</li> 
+<li>Tim Seufert</li>
+<Li>Paul Swenson</li> 
+<li>Patrick Tescher</li> 
+<li>Andrei Verovski</li> 
+<li>Aaron Voisine</li> 
+<li>Dominik Westner</li> 
+<li>Ken Williams</li> 
+<li>William Dell Wisner</li>
+<li>Ashley Yakeley</li> 
+<li>Tom Zerucha</li> 
+<li>Sasha Zucker</li> 
+</ul>
+
+
+<?
+include "footer.inc";
+?>
