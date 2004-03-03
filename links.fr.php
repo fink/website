dbbb3346d021d83ff204c8ba69efed63fa00dade
Michele Garoche <michga@users.sourceforge.net>
2004-03-03 17:03:21 +0000

    adding links.fr.php

---
+++ links.fr.php
@@ -0,0 +1,82 @@
+<?
+$title = "Liens";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/03/03 17:03:21 $';
+
+include "header.inc";
+?>
+
+
+<h1>Liens</h1>
+
+<p>Voici l'inévitable collection de liens... Tous les liens s'ouvrent dans une nouvelle fenêtre de votre navigateur.</p>
+
+<p>Liens associés à Fink</p>
+<ul>
+<li><a href="http://finkmirrors.net/" target="_blank">Miroirs de Fink</a> - comment installer un miroir de Fink et statut des miroirs existants.</li>
+<li><a href="http://finkcommander.sourceforge.net/" target="_blank">FinkCommander</a> -
+interface graphique pour Fink</li>
+<li><a href="http://fink.aquaflux.org/" target="_blank">FinkBot</a> -
+logs & stats du canal IRC #fink</li>
+<li><a href="http://www.funwithfink.com/" target="_blank">FunWithFink</a> -
+distribution non-officielle de Fink sur CD et livre de trucs et astuces sur Fink</li>
+<li><a href="http://www.antcrc.utas.edu.au/models/macosx/Fink_on_NFS/" target="_blank">Fink on NFS</a> -
+collection de scripts facilitant la distribution des paquets Fink sur des réseaux clients via NFS</li>
+</ul>
+
+<p>Logiciels Open Source</p>
+<ul>
+<li><a href="http://www.gnu.org/" target="_blank">Logiciels GNU</a> -
+contient aussi des informations sur les licences FSF et GNU</li>
+<li><a href="http://www.opensource.org/" target="_blank">Open Source
+Initiative</a> - information générale sur Open Source (en particulier sur les licences)</li>
+<li><a href="http://freshmeat.net/" target="_blank">Freshmeat</a> -
+le plus grand répertoire de logiciels Open Source sur la toile</li>
+<li><a href="http://developer.apple.com/darwin/" target="_blank">Darwin</a>
+et autres projets Open Source d'Apple</li>
+<li><a href="http://opendarwin.org/" target="_blank">OpenDarwin.org</a> -
+développement Darwin</li>
+<li><a href="http://oroborosx.sourceforge.net/" target="_blank">OroborOSX</a> -
+élégant gestionnaire de fenêtres X11 qui tente d'intégrer X11 à Mac OS X.</li>
+</ul>
+
+<p>Projets similaires</p>
+<ul>
+<li><a href="http://gnu-darwin.sourceforge.net/" target="_blank">The GNU-Darwin Distribution</a></li>
+<li><a href="http://elisa.utopianet.net/~rlucia/devel/darwin_ports/" target="_blank">Darwin
+Ports</a> - portage de la collection des ports FreeBSD</li>
+<li><a href="http://openpackages.org/" target="_blank">OpenPackages</a> - unification et amélioration des collections de port *BSD</li>
+</ul>
+
+<p>Collections diverses de paquets portés sur Mac OS X</p>
+<ul>
+<li><a href="http://www.caos.aamu.edu/pub/MacOS_X/"
+target="_blank">http://www.caos.aamu.edu/pub/MacOS_X/</a> - géré par Marius Schamschula</li>
+<li><a href="http://www.osxgnu.org/" target="_blank">The GNU Mac OS X
+Public Archive</a></li>
+<li><a href="http://www.geeklair.net/downloads/"
+target="_blank">http://www.geeklair.net/downloads/</a> - géré par
+<a href="http://www.geeklair.net/~dluke/"
+target="_blank">Daniel Luke</a></li>
+<li><a href="http://macosx.forked.net/"
+target="_blank">http://macosx.forked.net/</a></li>
+<li><a href="http://www-jlc.kek.jp/~fujiik/macosx/10.1.X/"
+target="_blank">HEP on X</a> - collection de paquets RPM</li>
+</ul>
+
+<p>Sites contenant des informations connexes</p>
+<ul>
+<li><a href="http://www.darwinfo.org/" target="_blank">Darwinfo</a> -
+information et nouvelles sur le portage, plus une archive de logiciels</li>
+<li><a href="http://www.mrcla.com/XonX/" target="_blank">X on X</a> -
+ou comment faire tourner XFree86 sur Darwin et Mac OS X</li>
+<li><a href="http://www.stepwise.com/" target="_blank">StepWise</a> -
+nouvelles, articles et archive de logiciels</li>
+<li><a href="http://www.macgimp.org/" target="_blank">MacGimp</a> -
+aide sur le  fonctionnement de The GIMP, GTK et X sur un Mac</li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
