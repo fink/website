b1ddfc63b950134b2932e8cb79b444cfdcd2e778
Michele Garoche <michga@users.sourceforge.net>
2004-03-05 16:34:41 +0000

    adding index.fr.php

---
+++ bindist/index.fr.php
@@ -0,0 +1,16 @@
+<?
+$title = "Changement de place de la distribution binaire";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/03/05 16:34:41 $';
+
+include "header.inc";
+?>
+
+<h1>Changement de place de la distribution binaire</h1>
+<p>
+   La distribution binaire de Fink a changé de place. Tous les utilisateurs Fink qui souhaitent utiliser la distribution binaire doit s'assurer qu'il s'agit bien de la nouvelle version (nombre d'eux vous l'utilisent déjà sans même sans douter). Si vous voulez savoir comment passer à la nouvelle version et pourquoi nous avons changé son emplacement, <a href="<?php print $root; ?>news/bindist_move.php">lisez ceci.</a>.
+</p>
+
+<?
+include "footer.inc";
+?>
