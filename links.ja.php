e5d1a2b266bdf505e15eb5dddde1a7149abf88f4
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2004-03-06 02:23:41 +0000

    the writing style checked

--- links.ja.php
+++ links.ja.php
@@ -1,7 +1,7 @@
 <?
 $title = "リンク集";
 $cvs_author = '$Author: babayoshihiko $';
-$cvs_date = '$Date: 2004/03/06 01:47:40 $';
+$cvs_date = '$Date: 2004/03/06 02:23:41 $';
 
 include "header.inc";
 ?>
@@ -18,29 +18,29 @@ include "header.inc";
 <p>Fink 関連</p>
 <ul>
 <li><a href="http://finkmirrors.net/" target="_blank">Fink Mirrors</a> - 
-Fink ミラーの登録方法と現在のミラーのステータスの情報があります。</li>
+Fink ミラーの登録方法と現在のミラーのステータスの情報がある。</li>
 <li><a href="http://finkcommander.sourceforge.net/" target="_blank">FinkCommander</a> -
-Fink の GUI です。</li>
+Fink の GUI 。</li>
 <li><a href="http://fink.aquaflux.org/" target="_blank">FinkBot</a> -
 #fink IRC チャンネルの過去ログと統計。</li>
 <li><a href="http://www.funwithfink.com/" target="_blank">FunWithFink</a> -
 非公式の Fink CD とFink tips の本を出版している。</li>
 <li><a href="http://www.antcrc.utas.edu.au/models/macosx/Fink_on_NFS/" target="_blank">Fink on NFS</a> -
-Fink パッケージをネットワーククライアントに NFS で配布するスクリプト</li>
+Fink パッケージをネットワーククライアントに NFS で配布するスクリプト。</li>
 </ul>
 
 <p>オープンソースソフトウェア</p>
 <ul>
 <li><a href="http://www.gnu.org/home.ja.html" target="_blank">GNU Software</a> -
-FSF と GNU ライセンスの情報もあります。日本語ページへのリンクです。</li>
+FSF と GNU ライセンスの情報もあります。日本語ページへのリンク。</li>
 <li><a href="http://www.opensource.org/" target="_blank">Open Source Initiative</a> - 
-オープンソースに関する一般的な情報 (特にライセンス)</li>
+オープンソースに関する一般的な情報 (特にライセンス)。</li>
 <li><a href="http://freshmeat.net/" target="_blank">Freshmeat</a> -
-最大のオープンソースソフトウェアディレクトリ</li>
+最大のオープンソースソフトウェアディレクトリ。</li>
 <li><a href="http://developer.apple.com/darwin/" target="_blank">Darwin</a>
-Apple のオープンソースプロジェクト</li>
+Apple のオープンソースプロジェクト。</li>
 <li><a href="http://opendarwin.org/" target="_blank">OpenDarwin.org</a> -
-ダーウィンの開発
+ダーウィンの開発。
 </li>
 <li><a href="http://oroborosx.sourceforge.net/" target="_blank">OroborOSX</a> -
 X11 と Mac OS X を統合しようとしている X11 ウィンドウマネージャ。
@@ -50,7 +50,7 @@ X11 と Mac OS X を統合しようとしている X11 ウィンドウマネー�
 <ul>
 <li><a href="http://gnu-darwin.sourceforge.net/" target="_blank">The GNU-Darwin Distribution</a></li>
 <li><a href="http://elisa.utopianet.net/~rlucia/devel/darwin_ports/" target="_blank">Darwin Ports</a> - 
-FreeBSD の ports collection をポーとしている。</li>
+FreeBSD の ports collection をポートしている。</li>
 <li><a href="http://openpackages.org/" target="_blank">OpenPackages</a> - 
 *BSD の port collections を統合、改善している。</li>
 </ul>
@@ -58,13 +58,13 @@ FreeBSD の ports collection をポーとしている。</li>
 <p>ポートされたパッケージ集</p>
 <ul>
 <li><a href="http://www.caos.aamu.edu/pub/MacOS_X/" target="_blank">http://www.caos.aamu.edu/pub/MacOS_X/</a> - 
-Marius Schamschula のサイト</li>
+Marius Schamschula のサイト。</li>
 <li><a href="http://www.osxgnu.org/" target="_blank">The GNU Mac OS X Public Archive</a></li>
 <li><a href="http://www.geeklair.net/downloads/" target="_blank">http://www.geeklair.net/downloads/</a> - 
-<a href="http://www.geeklair.net/~dluke/" target="_blank">Daniel Luke</a> のサイト</li>
+<a href="http://www.geeklair.net/~dluke/" target="_blank">Daniel Luke</a> のサイト。</li>
 <li><a href="http://macosx.forked.net/" target="_blank">http://macosx.forked.net/</a></li>
 <li><a href="http://www-jlc.kek.jp/~fujiik/macosx/10.1.X/" target="_blank">HEP on X</a> - 
-RPM パッケージのコレクション</li>
+RPM パッケージのコレクション。</li>
 </ul>
 
 <p>関連情報</p>
@@ -76,7 +76,7 @@ XFree86 を Darwin や Mac OS X で使う。</li>
 <li><a href="http://www.stepwise.com/" target="_blank">StepWise</a> -
 ニュース、記事、ソフトウェアアーカイブ。</li>
 <li><a href="http://www.macgimp.org/" target="_blank">MacGimp</a> -
-Mac 用の GIMP、GTK、X のサイト</li>
+Mac 用の GIMP、GTK、X のサイト。</li>
 </ul>
 
 
