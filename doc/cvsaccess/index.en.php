5d4ef91bdb84860480a602840e589e2cb6be935b
Michele Garoche <michga@users.sourceforge.net>
2004-03-07 10:24:15 +0000

    adding cvs.fr.html cvs.en.html index.fr.php index.en.php

---
+++ doc/cvsaccess/index.en.php
@@ -0,0 +1,67 @@
+<?
+$title = "Fink CVS Access";
+$cvs_author = 'Author: michga';
+$cvs_date = 'Date: 2004/03/07 09:54:37';
+include_once 'nav.inc';
+$fsroot = $root = '../';
+include_once '../header.inc'; 
+?><h1>Setting up Fink CVS Access</h1><p>Generated from <i>$Id: index.en.php,v 1.1 2004/03/07 10:24:15 michga Exp $</i></p>
+<p>
+Fink is developed via CVS.
+This means that you can stay up to date between releases and always
+get the newest stuff.
+This page tells you how to set up an existing Fink installation for
+updating via CVS.
+The information on this page applies to Fink 0.3.x and later.
+</p>
+<h2><a name="">Fink CVS Structure</a></h2>
+<p>Fink has several CVS modules. The module <code>dists</code> (<a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/fink/dists/">ViewCVS</a>)
+contains the package descriptions and patches for OS X 10.2 and above. There are other modules
+used by Fink developers, which anyone can view, but these are
+not interesting for most users.</p>
+<h2><a name="">Updating the Package Descriptions</a></h2>
+<p>In the past this was a bit tedious procedure; but in the current Fink
+versions, this is a very simple procedure. Just issue this command:
+</p>
+<pre>fink selfupdate-cvs</pre>
+<p>Fink will perform all necessary steps automatically for you. This
+includes retrieving the latest set of Package Descriptions, and updating
+a few essential core packages (among them the Fink package manager).
+</p>
+<p>If you are behind a firewall consult <a href="http://fink.sourceforge.net/faq/usage-fink.php#proxy">FAQ 3.2</a>.
+</p>
+<p>After you have updated your package descriptions this way, you may
+want to update your packages to the latest available versions. You can
+do so by with the following command:
+</p>
+<pre>fink update-all</pre>
+<h2><a name="">Updating the Package Manager</a></h2>
+<p>
+<b>Note:</b> As of September 20, 2001 it is no longer necessary to update
+the package manager separately; it is treated like any other package.
+It's still possible to update it directly from CVS, though this is
+usually only interesting for people creating packages, not the average
+user.
+</p>
+
+<p>The package manager must be updated through a separate directory
+and the <code>inject.pl</code> script. That script puts package
+descriptions and tarballs for the fink and base-files packages in your
+Fink tree and builds them.</p>
+<p>For the first time procedure, you need a temporary directory
+(called <code>tempdir</code> in the example) which is empty (or at least
+doesn't contain a subdirectory named 'fink'). The procedure goes like
+this:</p>
+<pre>cd tempdir
+cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/fink login
+cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/fink co fink
+cd fink
+./inject.pl</pre>
+<p>The login command will ask for a password - just press return. You
+could delete the temporary directory after the procedure, but if you
+leave it around, updating is easier the next time. The procedure is
+then:</p>
+<pre>cd tempdir/fink
+cvs -z3 update -d
+./inject.pl</pre>
+<? include_once "../footer.inc"; ?>
---
+++ doc/cvsaccess/index.fr.php
@@ -0,0 +1,47 @@
+<?
+$title = "Accès CVS à Fink";
+$cvs_author = 'Author: michga';
+$cvs_date = 'Date: 2004/03/07 09:54:37';
+include_once 'nav.inc';
+$fsroot = $root = '../';
+include_once '../header.inc'; 
+?><h1>Configuration de l'accès à Fink via CVS</h1><p>Generated from <i>$Id: index.fr.php,v 1.1 2004/03/07 10:24:15 michga Exp $</i></p>
+<p>
+Fink est développé via CVS.
+Cela vous permet de rester à jour entre deux versions et d'avoir toujours les paquets les plus récents.
+Cette page vous explique comment configurer une installation Fink existante pour qu'elle puisse être mise à jour via CVS.
+Les informations ci-dessous sont applicables à Fink 0.3.x et ultérieure.
+</p>
+<h2><a name="">Structure CVS de Fink</a></h2>
+<p>Fink comporte plusieurs modules CVS. Le module <code>dists</code> (<a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/fink/dists/">ViewCVS</a>)
+contient les descriptions de paquets et les rustines pour OS X 10.2 et supérieure. Il existe d'autres modules pour les développeurs de Fin. Vous pouvez les afficher, mais ils n'ont aucun intérêt pour la plupart des utilisateurs.</p>
+<h2><a name="">Mise à jour des descriptions de paquets</a></h2>
+<p>Auparavant, la procédure était compliquée ; mais, avec la version actuelle de Fink, elle s'est grandement simplifiée. Il faut suffit de lancer cette commande :
+</p>
+<pre>fink selfupdate-cvs</pre>
+<p>Fink exécute alors automatiquement toutes les étapes nécessaires. Il récupère les descriptions de paquets les plus récentes et met à jour un certain nombre de paquets fondamentaux (dont le gestionnaire de paquet Fink).
+</p>
+<p>Si vous êtes derrière un mur pare-feu, consultez <a href="http://fink.sourceforge.net/faq/usage-fink.php#proxy">QFP 3.2</a>.
+</p>
+<p>Après avoir ainsi mis à jour les descriptions de paquets, vous pouvez mettre à jour les paquets. Pour ce faire, lancez la commande suivante :
+</p>
+<pre>fink update-all</pre>
+<h2><a name="">Mise à jour du gestionnaire de paquets</a></h2>
+<p>
+<b>Note:</b> Depuis le 20 septembre 2001, il n'est plus utile d'effectuer une mise à jour spéciale pour le gestionnaire de paquets ; il est traité comme n'importe quel autre paquet.
+Il est, cependant, toujours possible de le mettre à jour directement via CVS, bien que cela ne soit vraiment utile qu'aux personnes qui créent des paquets.
+</p>
+
+<p>Le gestionnaire de paquet doit être mis à jour dans un répertoire distinct à l'aide du script <code>inject.pl</code>. Ce script met dans votre arborescence Fink les descriptions de paquets et les archives tar de fink et des paquets fondamentaux, puis les compile.</p>
+<p>La première, vous devez utiliser un répertoire temporaire
+(nommé <code>tempdir</code> par exemple) vide (ou qui ne contient pas lui-même un sous-répertoire 'fink'). Voici la procédure à employer :</p>
+<pre>cd tempdir
+cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/fink login
+cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/fink co fink
+cd fink
+./inject.pl</pre>
+<p>La commande login vous demandera un mot de passe - contentez d'appuyer sur la touche retour chariot. Vous pouvez supprimer le répertoire temporaire à la fin de la procédure, mais si vous le conservez, les mises à jour suivantes en seront facilitées. La procédure sera alors :</p>
+<pre>cd tempdir/fink
+cvs -z3 update -d
+./inject.pl</pre>
+<? include_once "../footer.inc"; ?>
