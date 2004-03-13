7e35af53911fdd67ede90a6a91890896fc987651
Alexander Hansen <alexkhansen@users.sourceforge.net>
2004-03-13 02:04:54 +0000

    Actual web stuff for i18n HOWTO, including a link in the Documents index page.

--- doc/index.en.php
+++ doc/index.en.php
@@ -1,62 +1,67 @@
 <?
 $title = "Documentation";
-$cvs_author = 'Author: fingolfin';
-$cvs_date = 'Date: 2004/03/07 20:16:29';
+$cvs_author = 'Author: alexkhansen';
+$cvs_date = 'Date: 2004/03/13 01:55:54';
 $metatags = '';
 
 include_once "header.inc";
 ?>
 
-<h1>Fink - Documentation</h1><!--Generated from $Fink: doc.en.xml,v 1.3 2004/03/07 20:16:29 fingolfin Exp $-->
+<h1>Fink - Documentation</h1><!--Generated from $Fink: doc.en.xml,v 1.5 2004/03/13 01:55:54 alexkhansen Exp $-->
 <p>
 This is a collection of various documents written for Fink.
 Some of the documents may also be useful to people who use Mac OS X
 or Darwin without Fink and want to learn about porting Unix software.
 </p>
-<h2><a name="userdoc">User Documentation</a></h2>
-
-<p>
+  <h2><a name="userdoc">User Documentation</a></h2>
+    
+    <p>
 The current user documentation for Fink:
 </p>
-<ul>
-<li><a href="users-guide/index.php">Fink User's Guide</a> -
+    <ul>
+      <li><a href="users-guide/index.php">Fink User's Guide</a> -
 this covers installing Fink itself, installing packages, and upgrading
 to a new Fink release. It contains instructions for both the source
 and the binary release.
 <b>Work in Progress!</b></li>
-<li><a href="x11/index.php">Running X11 on Darwin and Mac OS X</a> -
+      <li><a href="x11/index.php">Running X11 on Darwin and Mac OS X</a> -
 covers concepts, installation and launching (also intended for Darwin
 and Mac OS X users in general)</li>
-</ul>
-
-<p>
+    </ul>
+    <p>
 A bunch of documents that are more complete, but slightly outdated and
 no longer maintained:
 </p>
-<ul>
-<li><a href="bundled/install.php">Installation and Upgrading</a> - how
+    <ul>
+      <li><a href="bundled/install.php">Installation and Upgrading</a> - how
 to install Fink or upgrade to a new version</li>
-<li><a href="bundled/usage.php">Usage</a> - how to use Fink
+      <li><a href="bundled/usage.php">Usage</a> - how to use Fink
 and the installed software</li>
-<li><a href="bundled/readme.php">Fink ReadMe</a> - the ReadMe for the
+      <li><a href="bundled/readme.php">Fink ReadMe</a> - the ReadMe for the
 source distribution</li>
-<li><a href="cvsaccess/index.php">CVS Access</a> - how to access the
+      <li><a href="cvsaccess/index.php">CVS Access</a> - how to access the
 Fink CVS repository to get the latest source packages between
 releases</li>
-</ul>
-
-<h2><a name="developerdoc">Developer Documentation</a></h2>
-
-<ul>
-<li><a href="http://fink.sourceforge.net/doc/UsingFink.pdf">Using Fink: A Developer's How To</a> (2MB pdf
+    </ul>
+  <h2><a name="developerdoc">Developer Documentation</a></h2>
+    
+    <ul>
+      <li><a href="http://fink.sourceforge.net/doc/UsingFink.pdf">Using Fink: A Developer's How To</a> (2MB pdf
 file) - slides from a presentation at the <a href="http://conferences.oreillynet.com/macosx2002/">O'Reilly Mac OS X Conference</a> (also available as a
 <a href="http://conferences.oreillynet.com/presentations/macosx02/morrison_david.ppt">PowerPoint file</a>) </li>
-<li><a href="porting/index.php">Porting Tips</a> - notes for porting
+      <li><a href="porting/index.php">Porting Tips</a> - notes for porting
 Unix applications to Darwin</li>
-<li><a href="packaging/index.php">Packaging Manual</a> - how to create and
+      <li><a href="packaging/index.php">Packaging Manual</a> - how to create and
 maintain Fink packages</li>
-</ul>
-
-
+    </ul>
+  <h2><a name="otherdoc">Other Documents</a></h2>
+    
+    <ul>
+      <li><a href="multilingual/index.php">Internationalization Guide</a>- material concerning
+the ongoing website
+internationalization 
+effort</li>
+    </ul>
+  
 
 <? include_once "footer.inc"; ?>
---
+++ doc/multilingual/intro.en.php
@@ -0,0 +1,85 @@
+<?
+$title = "i18n - Intro";
+$cvs_author = 'Author: alexkhansen';
+$cvs_date = 'Date: 2004/03/10 02:23:16';
+$metatags = '<link rel="contents" href="index.php?phpLang=en" title="i18n Contents"><link rel="next" href="files.php?phpLang=en" title="The Documentation Files"><link rel="prev" href="index.php?phpLang=en" title="i18n Contents">';
+
+include_once "header.inc";
+?>
+
+<h1>i18n - 1 Introduction</h1>
+    
+
+    
+
+    <h2><a name="what">1.1 What is going on with internationalization of the Fink
+      website?</a></h2>
+      
+
+      <p>The Fink project has undertaken an effort to add full
+      internationalization support to its website, in order to make the site
+      more accessible worldwide. Thanks to Baba Yoshihiko, there is now a
+      framework in which pages in multiple languages can be employed</p>
+    
+
+    <h2><a name="languages">1.2 Languages</a></h2>
+      
+
+      <p>We currently have volunteers working on the following languages:</p>
+
+      <ul>
+        <li>Danish</li>
+
+        <li>Dutch</li>
+
+        <li>English (the baseline documentation)</li>
+
+        <li>French</li>
+
+        <li>German</li>
+
+        <li>Italian</li>
+
+        <li>Japanese</li>
+
+        <li>Spanish</li>
+
+        <li>Simplified Chinese</li>
+      </ul>
+    
+
+    <h2><a name="organization">1.3 Organization</a></h2>
+      
+
+      <p>A chart of the organization of the i18n team is available <a href="http://micmacfr.homeunix.org/web/organisation/">here</a>.</p>
+    
+
+    <h2><a name="helping">1.4 Helping out.</a></h2>
+      
+
+      <p>If you wish to help out with the internationalization effort, there
+      are several things that you can do:</p>
+
+      <ul>
+        <li>Join discussions on the <a href="http://fink.sourceforge.net/lists/fink-i18n.php">fink-i18n
+        mailing list</a>. This is where we will discuss the mechanics of
+        internationalization, announcements of updates to documents will be
+        made, etc.</li>
+
+        <li>Look over the translation for your own language. You may have a
+        translation that flows better than does the official one, you may find
+        a translation error, and other possibilities.</li>
+
+        <li>Become a translator yourself. If your language isn't currently
+        represented (or even if it is), and you think you can provide an
+        effective translation, then you may want to volunteer to be a
+        translator for your language. <p>Be advised, however, if you are the
+        first person to work on your language: there are quite a few documents
+        that you will have to translate initially. Once this is done, there
+        typically will be small changes made.</p></li>
+      </ul>
+    
+  <p align="right">
+Next: <a href="files.php?phpLang=en">2 The Documentation Files</a></p>
+
+<? include_once "footer.inc"; ?>
