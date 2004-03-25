16296ba65819a21c2e4d69c8cc579a2dbe4918f6
Michele Garoche <michga@users.sourceforge.net>
2004-03-25 05:48:40 +0000

    Adding French translation for  mailing lists

---
+++ lists/index.fr.php
@@ -0,0 +1,55 @@
+<?
+$title = "Listes de diffusion";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/03/25 05:48:40 $';
+
+include "header.inc";
+?>
+
+
+<h1>Listes de diffusion</h1>
+
+<p>
+Le projet a plusieurs listes de diffusion.
+Elles sont toutes hébergées par SourceForge et archivées par Geocrawler.
+</p>
+<p>
+Toutes les listes sont publiques et tout le monde peut s'abonner librement.
+Cliquez sur le nom d'une des listes de diffusion ci-dessous pour de plus amples informations.
+</p>
+<p>
+<em>Ne faites pas de messages croisés !</em> Choisissez une des listes pour vos messages. Si vous ne savez pas quelle est la liste la plus appropriée pour votre question, n'hésitez pas à demander sur la liste si c'est le cas.
+</p>
+<ul>
+<li><p><a href="fink-announce.php">fink-announce</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.announce">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.announce">NNTP</a>)<br>
+Liste d'annonces des nouvelles versions de Fink. (faible trafic)</p></li>
+<li><p><a href="fink-i18n.php">fink-i18n</a> (<a href="http://news.gmane.org/gmane.os.apple.fink.i18n">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.i18n">NNTP</a>)<br>
+Liste de discussion et support sur les problèmes de traduction et d'architecture du site web de Fink.</p></li>
+<li><p><a href="fink-beginners.php">fink-beginners</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.beginners">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.beginners">NNTP</a>)<br>
+Liste de discussion et support pour les utilisateurs débutants de Fink</p></li>
+<li><p><a href="fink-users.php">fink-users</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.general">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.general">NNTP</a>)<br>
+Listes de discussion et support pour les utilisateurs chevronnés de Fink.</p></li>
+<li><p><a href="fink-devel.php">fink-devel</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.devel">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.devel">NNTP</a>)<br>
+Listes de discussion pour les mainteneurs et développeurs de paquets Fink.</p></li>
+<li><p><a href="fink-core.php">fink-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.core">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.core">NNTP</a>)<br>
+Liste de discussion pour les paquets fondamentaux de Fink.</p></li>
+<li><p><a href="fink-gnome-core.php">fink-gnome-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.gnome">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.gnome">NNTP</a>)<br>
+Liste de discussion pour les paquets GNOME de Fink.</p></li>
+<li><p><a href="fink-commits.php">fink-commits</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.cvs">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.cvs">NNTP</a>)<br>
+Liste de distribution des rapports de commit CVS.</p></li>
+<li><p><a href="fink-tracker.php">fink-tracker</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.tracker">Archives</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.tracker">NNTP</a>)<br>
+Liste de distribution pour les rapports de bogues et la soumission de paquets.</p></li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
