cd2b0047d6bf1d288b9bfde6d5dfe2937ecf812c
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2004-03-27 05:03:15 +0000

    Adding Japanese

---
+++ lists/index.ja.php
@@ -0,0 +1,60 @@
+<?
+$title = "メーリングリスト";
+$cvs_author = '$Author: babayoshihiko $';
+$cvs_date = '$Date: 2004/03/27 05:03:15 $';
+
+include "header.inc";
+?>
+
+
+<h1>メーリングリスト</h1>
+
+<p>
+プロジェクトに関連したメーリングリストを紹介しています。
+いずれも SourceForge でホストされ、 Geocrawler で保管されています。
+</p>
+<p>
+全てのリストは公開されていて、自由に購読することができます。
+それぞれの詳細については、下記のメーリングリスト名をクリックして下さい。
+</p>
+<p>
+<em>マルチポストはしないで下さい。</em>
+どれかひとつ選択し、そのリストにだけ投稿して下さい。
+どのリストが適当かわからない場合、リスト上で聞いてみて下さい。
+</p>
+<p>
+訳注: 全て英語で議論されています。
+</p>
+<ul>
+<li><p><a href="fink-announce.php">fink-announce</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.announce">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.announce">NNTP</a>)<br>
+新しい Fink のリリースを広報する (低交通量)</p></li>
+<li><p><a href="fink-i18n.php">fink-i18n</a> (<a href="http://news.gmane.org/gmane.os.apple.fink.i18n">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.i18n">NNTP</a>)<br>
+翻訳および Fink ウェブサイトに関する議論と支援</p></li>
+<li><p><a href="fink-beginners.php">fink-beginners</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.beginners">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.beginners">NNTP</a>)<br>
+新規利用者のための議論と支援リスト</p></li>
+<li><p><a href="fink-users.php">fink-users</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.general">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.general">NNTP</a>)<br>
+上級利用者のための議論と支援リスト</p></li>
+<li><p><a href="fink-devel.php">fink-devel</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.devel">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.devel">NNTP</a>)<br>
+Fink パッケージメンテナと開発者のための議論と支援リスト</p></li>
+<li><p><a href="fink-core.php">fink-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.core">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.core">NNTP</a>)<br>
+Fink ベースパッケージのための議論リスト</p></li>
+<li><p><a href="fink-gnome-core.php">fink-gnome-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.gnome">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.gnome">NNTP</a>)<br>
+Fink GNOME パッケージのための議論リスト</p></li>
+<li><p><a href="fink-commits.php">fink-commits</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.cvs">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.cvs">NNTP</a>)<br>
+自動的に CVS コミットを報告する配布専用リスト</p></li>
+<li><p><a href="fink-tracker.php">fink-tracker</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.tracker">アーカイブ</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.tracker">NNTP</a>)<br>
+バグ報告とパッケージ提案の報告をする配布専用リスト</p></li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
