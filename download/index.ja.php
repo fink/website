e6c9dd1b998f6bb439bf573a407315ba7d46ce9d
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2004-04-03 16:14:35 +0000

    index.en.php 1.2

--- download/index.ja.php
+++ download/index.ja.php
@@ -1,7 +1,7 @@
 <?
 $title = "Download Quick Start";
 $cvs_author = '$Author: babayoshihiko $';
-$cvs_date = '$Date: 2004/04/03 16:12:49 $';
+$cvs_date = '$Date: 2004/04/03 16:14:35 $';
 
 include "header.inc";
 ?>
@@ -30,7 +30,7 @@ include "../fink_version.inc";
 <a href="http://prdownloads.sourceforge.net/fink/Fink-<? print $fink_version; ?>-Installer.dmg?download">Fink
 <? print $fink_version; ?> バイナリインストーラ</a> - <? print $dmg_size; ?><br>
 (10.2 の場合は <a href="http://prdownloads.sourceforge.net/fink/Fink-0.6.3-installer.dmg?download">Fink
-0.4.1</a> をダウンロードして下さい)
+0.4.1</a> をダウンロードして下さい)<br>
 (10.1 の場合は <a href="http://prdownloads.sourceforge.net/fink/Fink-0.4.1-installer.dmg?download">Fink
 0.4.1</a> をダウンロードして下さい)
 </p>
