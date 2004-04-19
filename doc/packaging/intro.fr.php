445d49452f23d40674b3b9e6ea216274edda44bf
Michele Garoche <michga@users.sourceforge.net>
2004-04-19 22:20:26 +0000

    partial translation packaging.fr.xml

--- doc/packaging/intro.fr.php
+++ doc/packaging/intro.fr.php
@@ -1,7 +1,7 @@
 <?
 $title = "Paquets - Intro";
 $cvs_author = 'Author: michga';
-$cvs_date = 'Date: 2004/04/19 21:34:28';
+$cvs_date = 'Date: 2004/04/19 22:20:26';
 $metatags = '<link rel="contents" href="index.php?phpLang=fr" title="Paquets Contents"><link rel="next" href="format.php?phpLang=fr" title="Descriptions de paquets"><link rel="prev" href="index.php?phpLang=fr" title="Paquets Contents">';
 
 include_once "header.inc";
--- doc/packaging/policy.fr.php
+++ doc/packaging/policy.fr.php
@@ -1,7 +1,7 @@
 <?
 $title = "Paquets - Règles";
 $cvs_author = 'Author: michga';
-$cvs_date = 'Date: 2004/04/19 21:34:28';
+$cvs_date = 'Date: 2004/04/19 22:20:26';
 $metatags = '<link rel="contents" href="index.php?phpLang=fr" title="Paquets Contents"><link rel="next" href="fslayout.php?phpLang=fr" title="Organisation des fichiers"><link rel="prev" href="format.php?phpLang=fr" title="Descriptions de paquets">';
 
 include_once "header.inc";
