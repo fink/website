28603b408a38b5172e8a55577c2ce125e8aa9679
Jeff Ye <jeff_yecn@users.sourceforge.net>
2004-04-21 23:33:32 +0000

    add Chinese l10n for mail-list page

---
+++ lists/index.zh.php
@@ -0,0 +1,56 @@
+<?
+$title = "邮件列表";
+$cvs_author = '$Author: jeff_yecn $';
+$cvs_date = '$Date: 2004/04/21 23:33:32 $';
+
+include "header.inc";
+?>
+
+
+<h1>邮件列表</h1>
+
+<p>
+本项目有几个邮件列表。
+全部都有 SourceForge 提供寄存服务，并由 Geocrawler 提供归档服务。
+</p>
+<p>
+所有列表都是公开的，并具有公开的订阅规则。
+电击下面的邮件列表名来获取详细的信息。
+</p>
+<p>
+<em>请不要在多个列表张贴相同的帖子！</em>为你的帖子选择一个合适的列表。
+如果你不确定那个列表比较适合的，可以在列表中询问你的问题是否适合在那里张贴。
+</p>
+<ul>
+<li><p><a href="fink-announce.php">fink-announce</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.announce">旧帖</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.announce">NNTP</a>)<br>
+新 Fink 发布版本的通知。(低流量)</p></li>
+<li><p><a href="fink-i18n.php">fink-i18n</a> (<a href="http://news.gmane.org/gmane.os.apple.fink.i18n">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.i18n">NNTP</a>)<br>
+关于 Fink 网站翻译的有关问题的讨论和支持。</p></li>
+<li><p><a href="fink-beginners.php">fink-beginners</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.beginners">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.beginners">NNTP</a>)<br>
+针对 Fink 新用户的讨论和支持。</p></li>
+<li><p><a href="fink-users.php">fink-users</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.general">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.general">NNTP</a>)<br>
+针对 Fink 熟练用户的讨论和支持。</p></li>
+<li><p><a href="fink-devel.php">fink-devel</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.devel">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.devel">NNTP</a>)<br>
+针对 Fink 软件包的维护者和开发者的讨论列表。</p></li>
+<li><p><a href="fink-core.php">fink-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.core">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.core">NNTP</a>)<br>
+关于 Fink 基本软件包的讨论。</p></li>
+<li><p><a href="fink-gnome-core.php">fink-gnome-core</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.gnome">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.gnome">NNTP</a>)<br>
+关于 Fink GNOME 软件包的讨论列表。</p></li>
+<li><p><a href="fink-commits.php">fink-commits</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.cvs">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.cvs">NNTP</a>)<br>
+自动生成的 cvs 提交情况报告的发布列表。</p></li>
+<li><p><a href="fink-tracker.php">fink-tracker</a>
+(<a href="http://news.gmane.org/gmane.os.apple.fink.tracker">旧贴</a>, <a href="nntp://news.gmane.org/gmane.os.apple.fink.tracker">NNTP</a>)<br>
+Bug 报告和提交软件包情况的发布列表。</p></li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
