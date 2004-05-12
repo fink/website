8dca306b9216135fc77b1972039268a5e6b5b27c
Michele Garoche <michga@users.sourceforge.net>
2004-05-12 23:39:14 +0000

    Partial translation of packaging.fr.xml

--- doc/packaging/format.fr.php
+++ doc/packaging/format.fr.php
@@ -1,7 +1,7 @@
 <?
 $title = "Paquets - Descriptions de paquets";
 $cvs_author = 'Author: michga';
-$cvs_date = 'Date: 2004/05/08 22:28:34';
+$cvs_date = 'Date: 2004/05/12 23:39:14';
 $metatags = '<link rel="contents" href="index.php?phpLang=fr" title="Paquets Contents"><link rel="next" href="policy.php?phpLang=fr" title="Règles de distribution des paquets"><link rel="prev" href="intro.php?phpLang=fr" title="Introduction">';
 
 include_once "header.inc";
