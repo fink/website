0e1e9073fdc8fb2e6efaf1b316025e6992a35e50
Jeff Ye <jeff_yecn@users.sourceforge.net>
2004-07-11 00:49:45 +0000

    update doc.zh.xml according to v1.9

--- doc/index.zh.php
+++ doc/index.zh.php
@@ -1,13 +1,13 @@
 <?
 $title = "文档";
 $cvs_author = 'Author: jeff_yecn';
-$cvs_date = 'Date: 2004/04/17 01:15:30';
+$cvs_date = 'Date: 2004/07/11 00:49:45';
 $metatags = '';
 
 
 include_once "header.inc";
 ?>
-<h1>Fink－文档</h1><!--Generated from $Fink: doc.zh.xml,v 1.7 2004/04/17 01:15:30 jeff_yecn Exp $-->
+<h1>Fink－文档</h1><!--Generated from $Fink: doc.zh.xml,v 1.8 2004/07/11 00:49:45 jeff_yecn Exp $-->
 <p>
 这里收集了为 Fink 编写的各种文档。
 有些文档可能会对使用 Mac OS X 的用户有用，或那些没有使用 Fink 但又向学习如何移植 Unix 软件的 Darwin 用户有用。
@@ -36,6 +36,7 @@ include_once "header.inc";
 <h2><a name="developerdoc">开发者文档</a></h2>
 
 <ul>
+<li><a href="security/index.php"> 网络安全策略手册</a> - 所有在 Fink 中有管理软件包或打算向里面添加软件包的人都必须阅读。</li>
 <li><a href="http://fink.sourceforge.net/doc/UsingFink.pdf">使用 Fink：开发者使用指南</a>  (2MB pdf 文件，英文版) － 一组用于 <a href="http://conferences.oreillynet.com/macosx2002/">O'Reilly Mac OS X 大会</a>上演示的幻灯片（同时也有 <a href="http://conferences.oreillynet.com/presentations/macosx02/morrison_david.ppt">PowerPoint 文件</a>版本）</li>
 <li><a href="porting/index.php">移植技巧提示</a> － 关于如何移植 Unix 软件到 Darwin 上的资料</li>
 <li><a href="packaging/index.php">软件打包手册</a> － 如何创建和维护 Fink 软件包</li>
