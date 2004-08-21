dca36c6c93f3add98ef2bb0ee95aa60da098a0bb
Daniel Macks <dmacks@users.sourceforge.net>
2004-08-21 21:03:38 +0000

    Change "|" to "or" in non-programming usage.

--- faq/usage-fink.en.php
+++ faq/usage-fink.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Fink Usage";
-$cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2004/08/20 11:52:32';
+$cvs_author = 'Author: dmacks';
+$cvs_date = 'Date: 2004/08/21 21:01:44';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="comp-general.php?phpLang=en" title="Compile Problems - General"><link rel="prev" href="upgrade-fink.php?phpLang=en" title="Upgrading Fink (version-specific troubleshooting)">';
 
 
--- faq/usage-general.en.php
+++ faq/usage-general.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Usage (1)";
-$cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2004/08/20 11:52:32';
+$cvs_author = 'Author: dmacks';
+$cvs_date = 'Date: 2004/08/21 21:01:44';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="usage-packages.php?phpLang=en" title="Package Usage Problems - Specific Packages"><link rel="prev" href="comp-packages.php?phpLang=en" title="Compile Problems - Specific Packages">';
 
 
