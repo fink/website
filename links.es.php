245a8bf7bef2cd853949cda06211c5dbeab5cb61
Michele Garoche <michga@users.sourceforge.net>
2004-09-02 23:22:39 +0000

    Adding links.es.php

---
+++ links.es.php
@@ -0,0 +1,87 @@
+<?
+$title = "Links";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/09/02 23:22:39 $';
+
+include "header.inc";
+?>
+
+
+<h1>Links</h1>
+
+<p>Aqui esta la obvia colección de vínculos... Cada vínculo abrirá una
+nueva ventana de su navegador.</p>
+
+<p>Vínculos relacionados a Fink</p>
+<ul>
+<li><a href="http://finkmirrors.net/" target="_blank">Espejos de Fink/a>
+- Instrucciones sobre como convertirse en un espejo de Fink así como 
+mensajes de estado acerca de los espejos actuales.</li>
+<li><a href="http://finkcommander.sourceforge.net/" target="_blank">FinkCommander</a> -
+una GUI para Fink</li>
+<li><a href="http://osdir.com/modules.php?op=modload&amp;name=Downloads&amp;file=index&amp;req=ratedownload&amp;lid=676">Califica Fink @ O'Reilly's OSDir.com</a> - Calificanos y ayudanos apromover Fink.
+<li><a href="http://fink.aquaflux.org/" target="_blank">FinkBot</a> -
+estadísticas y logs del canal de IRC #fink</li>
+<li><a href="http://www.funwithfink.com/" target="_blank">Diversión con Fink</a> -
+Distribución no oficial en CD de Fink y libro de consejos de Fink</li>
+<li><a href="http://www.antcrc.utas.edu.au/models/macosx/Fink_on_NFS/" target="_blank">Fink en NFS</a> -
+Un grupo de scripts para facilitar la distribución de los paquetes de fink packages a clientes de red mediante NFS</li>
+</ul>
+
+<p>Software de Codigo Abierto</p>
+<ul>
+<li><a href="http://www.gnu.org/" target="_blank">Software GNU</a> -
+también tiene información acerca de las licensias FSF y GNU.</li>
+<li><a href="http://www.opensource.org/" target="_blank">Iniciativa de Codigo Abierto</a> - información general del Códígo Abier (esp.
+ licenses)</li>
+<li><a href="http://freshmeat.net/" target="_blank">Freshmeat</a> -
+el directorio más grande de Código Abierto en la web</li>
+<li><a href="http://developer.apple.com/darwin/" target="_blank">Darwin</a>
+y otros proyectos de Código Abierto de Apple</li>
+<li><a href="http://opendarwin.org/" target="_blank">OpenDarwin.org</a> -
+Desarrollo en Darwin
+</li>
+<li><a href="http://oroborosx.sourceforge.net/" target="_blank">OroborOSX</a> -
+un muy buen manejador de ventanas X11 que trata de integrar el X11 en el
+Mac OS X.
+</ul>
+
+<p>Proyectos Similares</p>
+<ul>
+<li><a href="http://gnu-darwin.sourceforge.net/" target="_blank">La Distribución GNU de Darwin</a></li>
+<li><a href="http://elisa.utopianet.net/~rlucia/devel/darwin_ports/" target="_blank">Puertos de Darwin
+</a> - colección de puertos de FreeBSD</li>
+<li><a href="http://openpackages.org/" target="_blank">OpenPackages</a> - unificando y mejorando las colecciones de puertos de *BSD </li>
+</ul>
+
+<p>Colecciones variadas de paquetes "porteados"</p>
+<ul>
+<li><a href="http://www.caos.aamu.edu/pub/MacOS_X/"
+target="_blank">http://www.caos.aamu.edu/pub/MacOS_X/</a> - mantenido por Marius Schamschula</li>
+<li><a href="http://www.osxgnu.org/" target="_blank">El archivo GNU Mac OS X público</a></li>
+<li><a href="http://www.geeklair.net/downloads/"
+target="_blank">http://www.geeklair.net/downloads/</a> - mantenido por
+<a href="http://www.geeklair.net/~dluke/"
+target="_blank">Daniel Luke</a></li>
+<li><a href="http://macosx.forked.net/"
+target="_blank">http://macosx.forked.net/</a></li>
+<li><a href="http://www-jlc.kek.jp/~fujiik/macosx/10.1.X/"
+target="_blank">HEP en X</a> - Una colección de paquetes RPM</li>
+</ul>
+
+<p>Sitios con información relacionada</p>
+<ul>
+<li><a href="http://www.darwinfo.org/" target="_blank">Darwinfo</a> -
+información de puertos, noticias y archivo de software</li>
+<li><a href="http://www.mrcla.com/XonX/" target="_blank">X on X</a> -
+ejecutando XFree86 en Darwin y Mac OS X</li>
+<li><a href="http://www.stepwise.com/" target="_blank">StepWise</a> -
+noticias, árticulos y archivo de software</li>
+<li><a href="http://www.macgimp.org/" target="_blank">MacGimp</a> -
+ayuda ejecutando The GIMP, GTK y X en la Mac</li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
