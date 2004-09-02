e7cabb3ab59e08a03cbfdaa9131193a2502b1339
Michele Garoche <michga@users.sourceforge.net>
2004-09-02 23:26:32 +0000

    Adding organization.es.php

---
+++ organization.es.php
@@ -0,0 +1,39 @@
+<?
+$title = "Organization of Fink";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/09/02 23:26:32 $';
+
+include "header.inc";
+?>
+
+<h1>Organización de Fink</h1>
+
+<p>El proyecto Fink funciona a través de voluntarios, y una estructura de 
+organización flexible. Al presente, el Fink Core Team y los Traductores de
+Fink son los grupos más organizados dentro del proyecto Fink.</p>
+
+<a name="core"><h2>El Fink Core Team</h2></a>
+
+<p>El Fink Core Team es responsable de varios aspectos del manejo central 
+del proyecto de Fink.  Los miembros del grupo trabajan en el programa fink 
+y coordinan las contribuciones de otros al programa; funjen como 
+Maintainers de los paquetes "esenciales" en la colección de Fink,
+y se asegura de que el bootstrapping funcione con estos paquetes 
+esenciales; ellos manejan las liberaciones del programa fink y los 
+programas relacionados; manejan las creaciones y liberación de las 
+distribuciones mayores de fink, incluyendo la fuente y la binaria.
+También ayudan a coordinar a los otros grupos de voluntarios que trabajan 
+en el proyecto de Fink.</p>
+
+<p>Los miembros actuales del fink core son Max Horn, Daniel Macks, Dave
+Morrison, Peter O'Gorman, and Ben Reed.  Ellos pueden ser localizados en 
+&lt;fink-core@lists.sourceforge.net&gt;.
+</p>
+
+<h2>Los Traductores de Fink</h2>
+
+<p>Pronto habrá descripción...</p>
+
+<?
+include "footer.inc";
+?>
