df64daebbdd35a391dd0f881ec03ff197a0795bb
Michele Garoche <michga@users.sourceforge.net>
2004-09-03 00:39:52 +0000

    Synchronizing doc.es.xml with English

--- doc/index.es.php
+++ doc/index.es.php
@@ -1,16 +1,17 @@
 <?
 $title = "Documentación";
 $cvs_author = 'Author: michga';
-$cvs_date = 'Date: 2004/08/30 22:41:13';
+$cvs_date = 'Date: 2004/09/03 00:39:52';
 $metatags = '';
 
 
 include_once "header.inc";
 ?>
-<h1>Fink - Documentación</h1><!--Generated from $Fink: doc.es.xml,v 1.6 2004/08/30 22:41:13 michga Exp $-->
-  <p>Atención: la versión española de este documento no se sincroniza con la versión inglesa.</p>
+<h1>Fink - Documentación</h1><!--Generated from $Fink: doc.es.xml,v 1.7 2004/09/03 00:39:52 michga Exp $-->
     <p>
-Esta es una colección de varios documentos escritos para el Fink. Algunos de ellos tambien pueden ser útiles para la gente que usa Mac OS X o Darwin sin Fink y que deseen aprender a hacer "puertos" de software de Unix. </p>
+Esta es una colección de varios documentos escritos para el Fink. 
+Algunos de ellos tambien pueden ser útiles para la gente que usa Mac OS X
+ó Darwin sin Fink y que deseen aprender a hacer "puertos" de software de Unix. </p>
   <h2><a name="userdoc">Documentación del Usuario</a></h2>
     
     <p>
@@ -18,7 +19,8 @@ La documentación actual del Fink incluye:
 </p>
     <ul>
       <li><a href="users-guide/index.php">Guía del Usuario del Fink</a> -
-esto cubre la instalación del propio Fink, la instalación de paquetes, y actualizaciones del Fink. Contiene instrucciones tanto para la version fuente como para la binaria.
+esto cubre la instalación del propio Fink, la instalación de paquetes, y actualizaciones del Fink. 
+Contiene instrucciones tanto para la version fuente como para la binaria.
 <b>Trabajo en Proceso!</b></li>
       <li><a href="x11/index.php">Ejecutando X11 en Mac OS X y Darwin</a> -
 cubre conceptos como la instalación y ejecución (tambien dedicado a los usuarios de Darwin y Mac OS X)</li>
@@ -35,10 +37,19 @@ Un grupo de documentos que son más completos, pero ligeramente desactualizados 
   <h2><a name="developerdoc">Documentación de Desarrolladores</a></h2>
     
     <ul>
+          <li><a href="security/index.php">Manual de Políticas de Seguridad</a> - 
+	  Lectura obligatoria para aquellos que mantienen paquetes en Fink o que les gustaría añadir alguno.</li>
       <li><a href="http://fink.sourceforge.net/doc/UsingFink.pdf">Usando Fink: Un "Cómo hacer" para los desarrolladores</a> (un pdf de 2 Mbs) - transparencias y la presentación de la Conferencias<a href="http://conferences.oreillynet.com/macosx2002/">O'Reilly Mac OS X </a> (Disponible tambien como 
 <a href="http://conferences.oreillynet.com/presentations/macosx02/morrison_david.ppt">archivo de PowerPoint </a>) </li>
       <li><a href="porting/index.php">Tips para hacer "puerto"</a> - notas para hacer "puertos" de aplicaciones de Unix a Darwin</li>
+            <li><a href="quick-start-pkg/index.php">Tutorial de Empaquetamiento</a> - un complemento al Manual de Paquetes el cual se enfoca en ejemplos reales e introduce el empaquetamiento a los principiantes.</li>
       <li><a href="packaging/index.php">Manual de Paquetes</a> - como crear y mantener paquetes de Fink</li>
+    </ul>
+  <h2><a name="otherdoc">Otros Documentos</a></h2>
+    
+    <ul>
+      <li><a href="multilingual/index.php">Guía de Internacionalización</a> - Material concerniente al continuo esfuerzo de internacionalización del sitio web.</li>
+      <li><a href="netiquette/index.php">Nettiquete para la Lista de Correo</a> - Cómo emplear mejor las listas de correo del Fink.</li>
     </ul>
   
 <? include_once "../footer.inc"; ?>
