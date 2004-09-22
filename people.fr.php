fb6480c1a742a44e7af0f9fa739e904afebfeb13
Michele Garoche <michga@users.sourceforge.net>
2004-09-22 20:41:41 +0000

    Updating contributors

--- people.fr.php
+++ people.fr.php
@@ -1,7 +1,7 @@
 <?
 $title = "Contributeurs";
 $cvs_author = '$Author: michga $';
-$cvs_date = '$Date: 2004/07/02 19:44:23 $';
+$cvs_date = '$Date: 2004/09/22 20:41:41 $';
 
 include "header.inc";
 ?>
@@ -38,26 +38,49 @@ Le site web doit être mis constamment mis à jour. Les utilisateurs ont besoin 
 <h2>Contributeurs réguliers</h2>
 
 <ul>
+<li>Jorge Acereda</li> 
+<li>Aida Shinra</li>
+<li>Asari Takashi</li>
+<li>Spundun Bhatt (<i>Spundun</i>)</li>
 <li>Rob Braun (<i>bbraun</i>)</li>
 <li>Keith Conger (<i>kconger</i>)</li>
 <li>Martin Costabel</li>
 <li>Sylvain Cuaz</li>
 <li>Finlay Dobbie (<i>AngusD</i>)</li>
+<li>Koen van der Drift</li>
+<li>rayg</li>
+<li>Michèle Garoche (<i>miga</i>)</li>
+<li>James Gibbs</li>
 <li>Justin F. Hallett (<i>TheSin</i>)</li>
+<li>Corey Halpin</li>
 <li>Jeremy Higgs</li>
 <li>Ben Hines (<i>Clef</i>)</li>
+<li>Hisashi Todd Fujinaka (<i>htodd</i>)</li>
+<li>David H&#246;hn aka. Darian Lanx (<i>dmalloc</i>)</li>
+<li>Daniel Johnson</li>
 <li>Carsten Klapp</li>
+<li>Killian Koepsell</li> 
+<li>Mamoru Komashi</li>
 <li>Thomas Kotzian</li> 
-<li>Daniel Macks (<i>dmacks</i>)</li>
+<li>Rohan Lloyd</li> 
+<li>Jean-François Mertens</li>
+<li>Mathias Meyer</li>
+<li>Remi Mommsen</li>
 <li>Matthias Neeracher (<i>Murr</i>)</li>
+<li>Andrew Rohl</li> 
+<li>Lars Rosengreen</li>
 <li>Christian Schaffner (<i>chris01</i>)</li>
-<li>Masanori Sekino (<i>msek</i>)</li>
-<li>Matt Stephenson</li>
 <li>Michael G. Schwern</li>
+<li>W. G. Scott</li>
+<li>Masanori Sekino (<i>msek</i>)</li>
+<li>Dustin Sias (<i>dsias</i>)</li>
+<li>Matt Stephenson</li>
 <li>Alexander Strange (<i>Feanor</i>)</li>
 <li>Christian Swinehart</li>
+<li>Mark Treiber</li>
 <li>Dave Vasilevsky (<i>vasi</i>)</li>
 <li>Jeffrey S. Whitaker</li>
+<li>Blair Zajac</li>
 <li>Chris Zubrzycki (<i>cirdan</i>)</li>
 
 
@@ -65,24 +88,52 @@ Le site web doit être mis constamment mis à jour. Les utilisateurs ont besoin 
 
 <h2>Support, traduction et équipe de documentation </h2>
 <ul>
+<li>Asari Takashi</li>
 <li>Baba Yoshihiko (<i>baba</i>)</li> <!-- i18n and l10n -->
+<li>Léonard Bouchet</li>
+<li>Philippe Boulnois</li>
+<li>Fabio Budai</li>
+<li>Federico Corazza</li>
+<li>Juan Courcoul</li>
+<li>Nicolas Darque</li>
+<li>Lenin Dominguez</li>
 <li>Hisashi Todd Fujinaka (<i>htodd</i>)</li> <!-- doc writer -->
+<li>Claudio Garguilo</li>
+<li>Michèle Garoche</li>
 <li>Alexander Hansen  (<i>akh</i>) </li> <!-- support work on mailing lists! -->
-<li>Les <a href="./doc/multilingual/intro.php?phpLang=fr#organization"> traducteurs</a></li>
+<li>Felix Kühne</li>
+<li>Apostolos Karakoussis</li>
+<li>Marc Sierbren Kxadijk</li>
+<li>Mathieu Legrand</li>
+<li>Zoran Nesic</li>
+<li>Noda Kai</li>
+<li>Julien Palmas</li>
+<li>GianMichele Pileri</li>
+<li>José Ramón Portillo</li>
+<li>Christian Putze</li>
+<li>Germain Saval</li>
+<li>Thiemo Scherle</li>
+<li>Thøger Jull Thorsen</li>
+<li>Chris Turkel</li>
+<li>Jeff Ye</li>
+<!-- <li>The <a href="./doc/multilingual/intro.php?phpLang=en#organization"> translators</a></li> -->
 </ul>
 
 <h2>Contributeurs occasionnels</h2>
 
 <ul>
-<li>Jorge Acereda</li> 
 <li>Mike Akers</li> 
 <li>David Bacher</li> 
 <li>Michael Baudis</li> 
+<li>Stuart Bishop</li>
 <li>Chris Black</li> 
+<li>David Brown</li>
 <li>Bill Bumgarner</li> 
 <li>Yves de Champlain</li>
-<li>Avram Cherry</li> 
+<li>Avram Cherry</li>
+<li>dithi</li> 
 <li>Jon Dugan</li> 
+<li>Matthijs van Duin</li>
 <li>Jeffrey Ellis</li> 
 <li>Jeremy Erwin</li> 
 <li>Benjamin Esham</li> 
@@ -91,54 +142,65 @@ Le site web doit être mis constamment mis à jour. Les utilisateurs ont besoin 
 <li>Landon Fuller (<i>landonf</i>)</li>
 <li>Paul Gaborit</li> 
 <li>Stanton Gallegos (<i>stantron</i>)</li> 
-<li>James Gibbs</li>
+<li>Andreas Gockel (<i>gecko2</i>)</li>
+<li>Sebastian Gonzalez</li>
 <li>Joe Gorse (<i>inca34</i>)</li>
 <li>Jake Goulding</li> 
 <li>Richard Graul</li>
+<li>Stephane Gully</li>
 <li>Matthew Harris</li> 
 <li>Shawn Hsiao (<i>shawnh</i>)</li>
+<li>Morgan Hough</li>
+<li>Xavier Humbert</li>
 <li>Sam Johnston</li> 
 <li>Richard Kiss</li> 
 <li>Eric Knauel</li> 
-<li>Killian Koepsell</li> 
 <li>Eric Knauel</li>
 <li>Bernd Kuemmerlen</li> 
 <li>Christopher League</li> 
-<li>Adam Lebsack</li> 
+<li>Adam Lebsack</li>
+<li>Eric Lee</li>
 <li>William McCallum</li> 
 <li>Michael McKibben</li>
-<li>Mathias Meyer</li>
+<li>Sonic McTails</li>
 <li>Kyle Moffett</li> 
 <li>David H. Munro</li> 
 <li>Joseph K. Myers</li> 
 <li>Kaben Nanlohy</li> 
 <li>Magenta Hari Nezumi</li> 
+<li>Hanspeter Niederstrasser</li>
 <li>Eric Norum</li> 
+<li>Nil Parikh</li>
 <li>Daniel Parks</li> 
 <li>Chris Pepper</li> 
 <li>Sebastien Pierre</li> 
 <li>Damien Pollet</li> 
+<li>Andy Prock</li>
 <li>Brian Redman</li> 
 <li>Rudy Richter (<i>rudy</i>)</li> 
+<li>Andrea Riciputi</li>
+<li>Chuck Robey</li>
 <li>Stefano Rodriguez</li> 
-<li>Andrew Rohl</li> 
 <li>David Ross</li> 
 <li>Michel Salim (<i>Hayashi-M</i>)</li>
 <li>Michel Schinz</li> 
+<li>Tim Seufert</li>
 <li>Chris Shabsin</li> 
-<li>Dustin Sias (<i>dsias</i>)</li>
 <li>Leigh Smith</li> 
 <li>Daniel Sohl</li> 
 <li>Lindsey Spratt</li>
+<li>Nigel Stanger</li>
 <li>Damian Steer</li> 
 <li>Mark Stosberg</li> 
-<li>Tim Seufert</li>
+<li>Michal Suchanek</li>
 <Li>Paul Swenson</li> 
 <li>Patrick Tescher</li> 
 <li>Chris Turkel (<i>zizban</i>)</li>
 <li>Andrei Verovski</li> 
 <li>Aaron Voisine</li> 
+<li>James Watson</li>
 <li>Dominik Westner</li> 
+<li>Kyle Wheeler</li>
 <li>Ken Williams</li> 
 <li>William Dell Wisner</li>
 <li>Ashley Yakeley</li> 
