a8ee2947bf6d837d967f66bb7366b367ec62e222
Michele Garoche <michga@users.sourceforge.net>
2004-09-28 04:07:28 +0000

    Adding help/index.de.php from to Marian, correcting encoding and misspelling

---
+++ help/index.de.php
@@ -0,0 +1,77 @@
+<?
+$title = "Hilfe";
+$cvs_author = '$Author: michga $';
+$cvs_date = '$Date: 2004/09/28 04:07:28 $';
+
+include "header.inc";
+?>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr valign="top"><td width="50%">
+
+<h1>Hilfe bekommen</h1>
+
+<p>
+Benötigen Sie Hilfe bei der Nutzung von Fink? Hier sind Ihre Optionen.
+</p>
+
+<p>
+<b>Dokumentation.</b>
+Der <a href="../doc/index.php">Dokumentationsteil</a> dieser Webseite hat jede Menge nützlicher Dokumente. Natürlich sind diese Dokumente jederzeit im Aufbau und viele unvollständig, aber sie sind definitiv hilfreich.</p>
+
+<p>
+<b>Die FAQ.</b> Häufige Probleme und ihre Lösungen sind in der <a href="../faq/index.php">FAQ</a> dokumentiert.
+</p>
+
+<p>
+<b>Die Verteilerlisten.</b> Wenn Sie ein Problem nicht selbst lösen können, stellen Sie die Frage auf der <a href="../lists/fink-beginners.php">fink-beginners</a>- oder der <a href="../lists/fink-users.php">fink-users</a>-Verteilerliste. Es ist ratsam erst das <a href="http://sourceforge.net/mailarchive/forum.php?forum=fink-beginners">fink-beginners</a>- und <a href="http://sourceforge.net/mailarchive/forum.php?forum=fink-users">fink-users</a>-Archiv zu überprüfen - denn es ist ermüdend immer die gleichen Fragen zu beantworten.</p>
+<p>Wenn Sie ein Problem melden, gehen Sie sicher, dass Sie relevante Informationen einbeziehen - wir können Ihnen nicht helfen, wenn wir nicht wissen, was Ihr Problem ist. Einige Dinge, die nicht fehlen sollten: Fink-Version, Mac OS X-Version, Versionen relevanter Pakete, der Finkbefehl, der fehlgeschlagen ist, jegliche Fehlernachrichten, die sinnvoll aussehen. Geben Sie außerdem an, ob Sie Software von anderen Quellen in /usr/local installiert haben oder einen anderen Compiler verwenden (z.B. gcc 3).</p>
+
+<p>
+<b>Der IRC-Kanal.</b>
+Es gibt einen <tt>#fink</tt>-Kanal auf dem <a href="http://freenode.net">freenode</a>-IRC-Netzwerk. Hier können Sie mit anderen Finknutzern  und einigen Entwicklern chatten.</p>
+
+<p>
+<b>Andere Seiten.</b>
+Einige Links zu Diskussionsforen:
+<a href="http://sourceforge.net/forum/?group_id=18034">XonX Foren bei
+SourceForge</a> -
+<a href="http://www.xdarwin.org/forum/">xdarwin.org Foren</a> -
+<a href="http://forums.macnn.com/forumdisplay.php?forumid=54">MacNN
+Unix-Forum</a> -
+<a href="http://macosx.com/">macosx.com</a> (dort gibt es mehrere Unix-Foren)
+</p>
+<p>Einige Links zu Seiten mit mehr oder weniger nützlichen Informationen: <a href="http://mrcla.com/XonX/">XonX</a> -
+<a href="http://macgimp.org/">macgimp.org</a> -
+<a href="http://macosx.org/">macosx.org</a> -
+<a href="http://macosxhints.com/">macosxhints.com</a>
+</p>
+
+</td><td>&nbsp;&nbsp;&nbsp;</td><td width="50%">
+
+<h1>Hilfe anbieten</h1>
+
+<p>Fink basiert auf das Engagement Freiwilliger. Hier steht wie <b>Sie</b> helfen können</p>
+			<p><b>Feedback.</b> Nichts ist wertvoller als die Rückmeldungen von Nutzern. Berichte über Probleme, Erfolgsgeschichten, Vorschläge und Beiträge sind immer willkommen. Auch wenn wir nicht versprechen können, alles sofort in Ordnung bringen zu können ; - ) , es hilft sehr zu wissen, welche Teile von Fink die meiste Aufmerksamkeit benötigen.</p>
+			<p>Sie können Ihr Feedback über die <a href="../lists/index.php">Mailinglisten</a>, über verschiedene Tracker bei SourceForge (siehe Homepage für direkte Links) oder direkt bei den Paket-Maintainern zur Verfügung stellen.</p>
+			<p><b>Empfehlen Sie weiter.</b> Wenn Sie Fink mögen, empfehlen Sie es Ihren Freunden. Es hilft, weil es eine hilfreiche Gemeinde aufbaut; es hilft dem Fink-Projekt, weil die Pakete mehr Prüfungen genießen; es hilft der Unixwelt im allgemeinen, weil es die Anerkennung von Mac OS X als ein Unix OS unterstützt.</p>
+			<p>Sie können auch <a href="http://www.apple.com/macosx/feedback/">Apple</a> davon berichten, dass Sie den Pfad, den Apple mit dem BSD-Unterbau von Mac OS X eingeschlagen hat, schätzen und die weitere Verbesserung der BSD-Schicht wünschen.</p>
+			<p><b>Support.</b> Wenn Sie Erfahrung zu teilen haben, beteiligen Sie sich in der <a
+href="../lists/fink-users.php">fink-users</a>-Mailingliste und helfen Sie Probleme von anderen Nutzer zu lösen.</p>
+			<p><b>Testen Sie Pakete.</b> Nehmen Sie aktuellsten Paketbeschreibungen vom <a href="../doc/cvsaccess/index.php">CVS</a>, setzen Sie Fink auf <a href="../faq/usage-fink.php#unstable">unstable</a> und testen Sie Pakete. Die Paketdatenbank führt die <a href="../pdb/testing.php">Pakete, die noch getestet werden müssen,</a> auf einer gesonderten Seite auf. Sie können Erfolgsgeschichten und Fehlerberichte zu den Paket-Maintainern oder zu einer Mailingliste Ihrer Wahl schicken.</p>
+			<p><b>Dokumentation.</b> Dem Projekt fehlen immer Helfer, die bereit sind die Dokumentation zu schreiben. <nobr><tt>;-)</tt></nobr>
+</p>
+			<p><b>Erstellen Sie Pakete.</b> Wenn Sie ein wenig Erfahrung mit der Installation von im Quelltext vorliegende Unix-Software haben, könnten Sie bei der Erstellung neuer Pakete helfen. Um anzufangen, lesen Sie das <a href="../doc/quick-start-pkg/index.php">Packaging-Tutorial</a>. Dann nehmen Sie sich die <a href="../doc/packaging/index.php">Packaging-Anleitung</a>, lesen Sie es sorgfältig und gründlich durch, tragen Sie sich in <a href="../lists/fink-devel.php">fink-devel</a> ein und melden Sie Ihre Pakete dem <a href="http://sourceforge.net/tracker/?atid=414256&group_id=17203">Paket-Einsendungs-Tracker</a>. Beachten Sie, dass Ihr Beitrag wahrscheinlich abgelehnt oder mit niedriger Priorität behandelt wird, es sei denn, er ist konform mit der Packaging-Policy.</p>
+		</td></tr>
+	<tr valign="top">
+		<td width="50%"></td>
+		<td></td>
+		<td width="50%"></td>
+	</tr>
+</table>
+
+
+<?
+include "footer.inc";
+?>
