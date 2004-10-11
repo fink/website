e73b0e9e9ee5fdcf5527e6bea1bdbf680691a5d6
Benjamin Reed <rangerrick@users.sourceforge.net>
2004-10-11 16:43:31 +0000

    new rdf dir

---
+++ news/rdf/index.php
@@ -0,0 +1,29 @@
+<?
+$title = "Fink Package RDF Feeds";
+$cvs_author = 'Author: dmrrsn';
+$cvs_date = 'Date: 2004/09/20 15:56:45';
+$metatags = '';
+
+include_once "header.inc";
+
+?>
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+<td>
+<h1>Fink Package RDF Feeds</h1>
+
+<h2>Updated Packages By OS</h2>
+<ul>
+ <li>10.2 (<a href="fink-10.2-stable.rdf">Stable</a>) (<a href="fink-10.2-unstable.rdf">Unstable</a>)</li>
+ <li>10.2-gcc3.3 (<a href="fink-10.2-gcc3.3-stable.rdf">Stable</a>) (<a href="fink-10.2-gcc3.3-unstable.rdf">Unstable</a>)</li>
+ <li>10.3 (<a href="fink-10.3-stable.rdf">Stable</a>) (<a href="fink-10.3-unstable.rdf">Unstable</a>)</li>
+</ul>
+<h2>Updated Packages By Tree</h2>
+<ul>
+ <li><a href="fink-stable.rdf">Stable</a></li>
+ <li><a href="fink-unstable.rdf">Unstable</a></li>
+</ul>
+
+</td></tr></table>
+<? include_once "footer.inc"; ?>
