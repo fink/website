e5b517de5fbdb323292c66fc5a38930537253a84
Claudio Gargiulo <claudio87@users.sourceforge.net>
2004-11-27 09:37:34 +0000

    fixed encoding problem

--- download/srcdist.it.php
+++ download/srcdist.it.php
@@ -1,7 +1,7 @@
 <?
 $title = "Source Release Download";
-$cvs_author = '$Author: babayoshihiko $';
-$cvs_date = '$Date: 2004/11/07 14:35:26 $';
+$cvs_author = '$Author: claudio87 $';
+$cvs_date = '$Date: 2004/11/27 09:40:02 $';
 
 include "header.inc";
 ?>
@@ -11,15 +11,15 @@ include "header.inc";
 <p>
 La source release contiene il gestore pacchetti fink pi?le descrizioni
 e gli aggiornamenti.
-Scarichert枴l codice sorgente dai siti di distribuzione ufficiali
-e li compilert枹ulla vostra macchina.
+Scaricherà il codice sorgente dai siti di distribuzione ufficiali
+e li compilerà sulla vostra macchina.
 </p>
 <? 
 include "../fink_version.inc";
 ?>
 
 <p>
-Fink <? print $fink_version; ?> x?stato ufficialmente rilasciato il 
+Fink <? print $fink_version; ?> è stato ufficialmente rilasciato il 
 <? print $release_date; ?>.
 
 </p>
@@ -39,12 +39,12 @@ Le istruzioni le trovate nell'Installer Document.
 <p>
 L'installazione e le istruzioni sono all'interno del
 tarball.
-Per favore leggete di seguito - Fink non x?un programma un-click-e-fatto.
+Per favore leggete di seguito - Fink non è un programma un-click-e-fatto.
 I documenti README, INSTALL e USAGE sono scritti in plain text (per
 essere letti da linea di comando) e in HTML (per essere letti in un browser
 e per essere stampati).
 Sono anche disponibili online alla <a
-href="../doc/index.php">documentation section</a>.
+href="../doc/index.php">documentazione</a>.
 </p>
 <p>
 Per essere informato sul rilascio di nuove versioni, sottoscrivetevi alla <a
