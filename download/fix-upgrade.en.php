1c8e3e5fc27479b0a4e1546d203a92ab1064f16a
Alexander Hansen <alexkhansen@users.sourceforge.net>
2004-11-28 23:52:12 +0000

    Modify for current "state of Fink".

--- download/fix-upgrade.en.php
+++ download/fix-upgrade.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "Repairing the Upgrade Path";
-$cvs_author = '$Author: jeff_yecn $';
-$cvs_date = '$Date: 2004/03/02 03:32:02 $';
+$cvs_author = '$Author: alexkhansen $';
+$cvs_date = '$Date: 2004/11/28 23:52:12 $';
 
 include "header.inc";
 ?>
@@ -10,8 +10,8 @@ include "header.inc";
 <h1>Repairing the Upgrade Path</h1>
 
 <p>
-Certain versions of Fink have difficulties with upgrades when running
-<code>fink selfupdate</code> (without CVS).  The result is a message
+Certain versions of Fink have had difficulties with upgrades when running
+<code>fink selfupdate</code>.  The result is a message
 which states that Fink is up-to-date, even though (as the message
 reports) the installed version is older than the latest version.
 Here is how to update in this situation:
@@ -21,9 +21,22 @@ Here is how to update in this situation:
 <li><p>Install an older version of the fink package manager, by running
 the following commands in a Terminal.app window:
 </p>
-<pre>curl -O http://us.dl.sourceforge.net/fink/direct_download/dists/fink-0.5.1/main/binary-darwin-powerpc/base/fink_0.11.1-10_darwin-powerpc.deb
-sudo dpkg -i fink_0.11.1-10_darwin-powerpc.deb
-rm fink_0.11.1-10_darwin-powerpc.deb</pre>
+<ul>
+<LI>
+<p><b>OS 10.3.x:</b> (0.7.1 binary distribution)</p>
+<pre><p>sudo apt-get install fink=0.21.2-1</p></pre>
+</LI>
+<li>
+<p><b>OS 10.2.x:</b> (0.6.3 binary distribution)</p>
+<pre><p>sudo apt-get install fink=0.18.3-1</p></pre>
+</li>
+<li>
+<p><b>OS 10.1.x:</b> (0.4.1 binary distribution)</p>
+<pre><p>sudo apt-get install fink=0.10.0-1</p></pre>
+</li>
+
+</ul>
+
 <li><p>
 Now update as usual, by running <code>fink selfupdate</code>.
 </p>
