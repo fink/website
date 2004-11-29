688a4eb38f79e5af62301781ac30308aa3f7241e
Alexander Hansen <alexkhansen@users.sourceforge.net>
2004-11-29 04:03:11 +0000

    Okay, I'll admit I was too proactive here...fix the FAQ instead Reverting to original text. Added note about specific Fink version this applies to.

--- download/fix-upgrade.en.php
+++ download/fix-upgrade.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "Repairing the Upgrade Path";
 $cvs_author = '$Author: alexkhansen $';
-$cvs_date = '$Date: 2004/11/29 01:29:36 $';
+$cvs_date = '$Date: 2004/11/29 04:03:11 $';
 
 include "header.inc";
 ?>
@@ -11,45 +11,27 @@ include "header.inc";
 
 <p>
 Certain versions of Fink have had difficulties with upgrades when running
-<code>fink selfupdate</code>.  The result is a message
+<code>fink selfupdate</code> (without CVS).  The result is a message
 which states that Fink is up-to-date, even though (as the message
 reports) the installed version is older than the latest version.
 Here is how to update in this situation:
 </p>
 <ol>
-
+<b><p>Fink-0.5.0a -> Fink-0.5.1</p></b>
 <li><p>Install an older version of the fink package manager, by running
 the following commands in a Terminal.app window:
 </p>
-<ul>
-<LI>
-<p><b>OS 10.3.x:</b> (0.7.1 binary distribution)</p>
-<pre><p>sudo apt-get install fink=0.21.2-1</p></pre>
-</LI>
-<li>
-<p><b>OS 10.2.x:</b> (0.6.3 binary distribution)</p>
-<pre><p>sudo apt-get install fink=0.18.3-1</p></pre>
-</li>
-<li>
-<p><b>OS 10.1.x:</b> (0.4.1 binary distribution)</p>
-<pre><p>sudo apt-get install fink=0.10.0-1</p></pre>
-</li>
-
-</ul>
-
+<pre>curl -O http://us.dl.sourceforge.net/fink/direct_download/dists/fink-0.5.1/main/binary-darwin-powerpc/base/fink_0.11.1-10_darwin-powerpc.deb
+sudo dpkg -i fink_0.11.1-10_darwin-powerpc.deb
+rm fink_0.11.1-10_darwin-powerpc.deb</pre>
 <li><p>
 Now update as usual, by running <code>fink selfupdate</code>.
 </p>
 
 </ol>
 
-<b><p>Older instructions</p></b>
-(specifically for Fink-0.5.0.a -> 0.5.1 update)
- </p>
-<pre>curl -O http://us.dl.sourceforge.net/fink/direct_download/dists/fink-0.5.1/main/binary-darwin-powerpc/base/fink_0.11.1-10_darwin-powerpc.deb
-sudo dpkg -i fink_0.11.1-10_darwin-powerpc.deb
-rm fink_0.11.1-10_darwin-powerpc.deb</pre></p>
-<p>Now run <code>fink selfupdate</code></p>.
+
+
 <?
 include "footer.inc";
 ?>
