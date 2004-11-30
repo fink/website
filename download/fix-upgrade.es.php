63ab7832643bf90d71f5fc57e00372fce5771027
Jose Ramon Portillo Fernandez <zifra@users.sourceforge.net>
2004-11-30 17:32:56 +0000

    actualizo

---
+++ download/fix-upgrade.es.php
@@ -0,0 +1,36 @@
+<?
+$title = "Repairing the Upgrade Path";
+$cvs_author = '$Author: zifra $';
+$cvs_date = '$Date: 2004/11/30 17:32:56 $';
+
+include "header.inc";
+?>
+
+
+<h1>Reparar el <em></em>Upgrade Path</em></h1>
+
+<p>
+Algunas versiones de Fink han tenido dificultades con las actualizaciones
+al ejecutar <code>fink selfupdate</code> (sin CVS).  El resultado era un mensaje
+que afirmaba que Fink se había actualizado, incluso cuando la versión instalada
+no era la última. El procedimiento a seguir en esta situación era:
+</p>
+<ol>
+<b><p>Fink-0.5.0a -> Fink-0.5.1</p></b>
+<li><p>Instalar una versión más antigua del manipulador de paquetes de Fink
+ejecutando el siguiente comando en una ventana de Terminal.app:
+</p>
+<pre>curl -O http://us.dl.sourceforge.net/fink/direct_download/dists/fink-0.5.1/main/binary-darwin-powerpc/base/fink_0.11.1-10_darwin-powerpc.deb
+sudo dpkg -i fink_0.11.1-10_darwin-powerpc.deb
+rm fink_0.11.1-10_darwin-powerpc.deb</pre>
+<li><p>
+Y ahora, actualizar como siempre, ejecutando <code>fink selfupdate</code>.
+</p>
+
+</ol>
+
+
+
+<?
+include "footer.inc";
+?>
