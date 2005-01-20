d300d9624dd4bf07eabe2d7d4a2e32b061da4d69
Alexander Hansen <alexkhansen@users.sourceforge.net>
2005-01-20 01:35:01 +0000

    Stuff for "Advanced topics"

---
+++ xml/advanced/index.en.php
@@ -0,0 +1,20 @@
+<?
+$title = "Advanced";
+$cvs_author = 'Author: alexkhansen';
+$cvs_date = 'Date: 2005/01/20 01:33:08';
+$metatags = '<link rel="contents" href="index.php?phpLang=en" title="Advanced Contents"><link rel="next" href="bindist.php?phpLang=en" title="Running your own Binary Distribution Server">';
+
+
+include_once "header.en.inc";
+?>
+<h1>Advanced Fink Topics</h1>
+    <p>
+This document gives describes some advanced topics not found in the
+<a href="http://fink.sourceforge.net/users-guide/">User's Guide</a>.
+    </p>
+  <h2><? echo FINK_CONTENTS ; ?></h2><ul>
+	<li><a href="bindist.php?phpLang=en"><b>1 Running your own Binary Distribution Server</b></a><ul><li><a href="bindist.php?phpLang=en#intro">1.1 Introduction</a></li><li><a href="bindist.php?phpLang=en#master">1.2 On the "master" (build) server</a></li><li><a href="bindist.php?phpLang=en#client">1.3 On the client machines</a></li><li><a href="bindist.php?phpLang=en#remarks">1.4 Remarks</a></li></ul></li></ul>
+<!--Generated from $Fink: advanced.en.xml,v 1.1 2005/01/20 01:33:08 alexkhansen Exp $-->
+<? include_once "../../footer.inc"; ?>
+
+
