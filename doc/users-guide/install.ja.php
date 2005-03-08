eebfb8e6822b1bc54a0ddfef4ff223bc3e651355
BABA Yoshihiko <babayoshihiko@users.sourceforge.net>
2005-03-08 14:32:54 +0000

    uguide.en.xml 1.25

--- doc/users-guide/install.ja.php
+++ doc/users-guide/install.ja.php
@@ -1,7 +1,7 @@
 <?
 $title = "ユーザーガイド - インストール";
 $cvs_author = 'Author: babayoshihiko';
-$cvs_date = 'Date: 2005/03/08 14:32:53';
+$cvs_date = 'Date: 2004/12/10 01:52:57';
 $metatags = '<link rel="contents" href="index.php?phpLang=ja" title="ユーザーガイド Contents"><link rel="next" href="packages.php?phpLang=ja" title="パッケージのインストール"><link rel="prev" href="intro.php?phpLang=ja" title="はじめに">';
 
 
