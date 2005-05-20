f6fa9690dd17847063ecd7b7a9dcfcbb389c1fef
Benn Newman <newmanbe@users.sourceforge.net>
2005-05-20 00:17:38 +0000

    Initial import.

---
+++ map/index.en.php
@@ -0,0 +1,40 @@
+<?
+$title = "Fink Developer Map";
+$cvs_author = '$Author: newmanbe $';
+$cvs_date = '$Date: 2005/05/20 00:17:38 $';
+
+include "../header.inc";
+?>
+<h1>Fink Developer Map</h1>
+<p>Ever wonder where some of the <a href="../people.php">contributors</a> to Fink live? Even if you have not, you are in the right place to find that information.</p>
+<p>Each person on the map has a number. Look on the list, then on the map to find out where each person lives. The name name in quotes is the nick used on #fink and irc.freenode.net.</p>
+<img src="finkmap.png" height="500" width="500">
+<table summary="Listings for the Fink Developer Map">
+<tr><td>Number</td><td>Name</td></tr>
+<tr><td>1</td><td>Benn 'newmanbe' Newman</td></tr>
+<tr><td>2</td><td>Daniel 'dmacks' Macks</td></tr>
+<tr><td>3</td><td>David R. 'drm' Morrison</td></tr>
+<tr><td>4</td><td>Benjamin 'RangerRick' Reed</td></tr>
+<tr><td>5</td><td>Chris 'cirdan' Zubryzcki</td></tr>
+<tr><td>6</td><td>David 'dmalloc' Hšhn</td></tr>
+<tr><td>7</td><td>Justin 'TheSin' Hallet</td></tr>
+<tr><td>8</td><td>Peter 'pogma' O'Gorman</td></tr>
+<tr><td>9</td><td>Martin Costabel</td></tr>
+<tr><td>10</td><td>Baba 'baba' Yoshihiko</td></tr>
+<tr><td>11</td><td>Aida 'aida' Shinra</td></tr>
+<tr><td>12</td><td>Asari 'asari' Takashi</td></tr>
+<tr><td>13</td><td>Motoi 'wm3' Washida</td></tr>
+<tr><td>14</td><td>Dave 'vasi' Vasilevsky</td></tr>
+<tr><td>15</td><td>Alexander 'Feanor' Strange</td></tr>
+<tr><td>16</td><td>Alexander 'akh' Hansen</td></tr>
+<tr><td>17</td><td>Ben 'Clef' Hines</td></tr>
+</table>
+<p>Think you should be on the map? E-mail <a href="mailto:newmanbe@freshell.org?Subject=Fink Developer Map">Benn Newman</a> using the following format:</p>
+<p>
+<code>longitude latitude "First name '#fink nick' Last name"#what you do for fink(developer, maintainer)</code>
+</p>
+
+<p>To fit on the map, you may have to be combined with near-by people. Your actually coordinates will still be recorded.</p>
+<?
+include "../footer.inc";
+?>
