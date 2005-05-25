8cd16249519ef2d1cd189eb12fedd788c6d4395f
Benn Newman <newmanbe@users.sourceforge.net>
2005-05-25 18:24:13 +0000

    baba didn't like being in Tokyo

--- map/index.en.php
+++ map/index.en.php
@@ -1,16 +1,16 @@
 <?
 $title = "Fink Developer Map";
 $cvs_author = '$Author: newmanbe $';
-$cvs_date = '$Date: 2005/05/20 19:47:30 $';
+$cvs_date = '$Date: 2005/05/25 18:24:13 $';
 
 include "header.inc";
 ?>
 <h1>Fink Developer Map</h1>
 <p>Ever wonder where some of the <a href="../people.php">contributors</a> to Fink live? Even if you have not, you are in the right place to find that information.</p>
 <p>Each person on the map has a number. Look on the list, then on the map to find out where each person lives. The name name in quotes is the nick used on #fink at irc.freenode.net.</p>
-<img src="finkmap.png" height="500" width="500" alt="">
+<img src="finkmap_word.png" height="750" width="750" alt="">
 <table summary="Listings for the Fink Developer Map">
-<tr><td>Number</td><td>Name</td></tr>
+<tr><td>Number</td><td>Name</td><td>Location</td></tr>
 <tr><td>1</td><td>Benn 'newmanbe' Newman</td></tr>
 <tr><td>2</td><td>Daniel 'dmacks' Macks</td></tr>
 <tr><td>3</td><td>David R. 'drm' Morrison</td></tr>
@@ -18,12 +18,12 @@ include "header.inc";
 <tr><td>5</td><td>Chris 'cirdan' Zubryzcki</td></tr>
 <tr><td>6</td><td>David 'dmalloc' H&#246;hn</td></tr>
 <tr><td>7</td><td>Justin 'TheSin' Hallet</td></tr>
-<tr><td>8</td><td>Peter 'pogma' O'Gorman</td></tr>
+<tr><td>8</td><td>Peter 'pogma' O'Gorman</td><td>Hyogo, Japan</td></tr>
 <tr><td>9</td><td>Martin Costabel</td></tr>
-<tr><td>10</td><td>Baba 'baba' Yoshihiko</td></tr>
-<tr><td>11</td><td>Aida 'aida' Shinra</td></tr>
-<tr><td>12</td><td>Asari 'asari' Takashi</td></tr>
-<tr><td>13</td><td>Motoi 'wm3' Washida</td></tr>
+<tr><td>10</td><td>Baba 'baba' Yoshihiko</td><td>Kyoto, Japan</td></tr>
+<tr><td>11</td><td>Aida 'aida' Shinra</td><td>Tokyo, Japan</td></tr>
+<tr><td>12</td><td>Asari 'asari' Takashi</td><td>Tokyo, Japan</td></tr>
+<tr><td>13</td><td>Motoi 'wm3' Washida</td><td>Tokyo, Japan</td></tr>
 <tr><td>14</td><td>Dave 'vasi' Vasilevsky</td></tr>
 <tr><td>15</td><td>Alexander 'Feanor' Strange</td></tr>
 <tr><td>16</td><td>Alexander 'akh' Hansen</td></tr>
