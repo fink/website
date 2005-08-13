7204f07cfcad97c01d36db83d39bf9b82004abbd
Alexander Hansen <alexkhansen@users.sourceforge.net>
2005-08-13 17:20:50 +0000

    Another gettext FAQ update

--- faq/general.en.php
+++ faq/general.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - General";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2005/08/13 14:30:48';
+$cvs_date = 'Date: 2005/08/13 17:19:12';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="relations.php?phpLang=en" title="Relations with Other Projects"><link rel="prev" href="index.php?phpLang=en" title="F.A.Q. Contents">';
 
 
--- faq/usage-fink.en.php
+++ faq/usage-fink.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Fink Usage";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2005/08/13 14:30:48';
+$cvs_date = 'Date: 2005/08/13 17:19:12';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="comp-general.php?phpLang=en" title="Compile Problems - General"><link rel="prev" href="upgrade-fink.php?phpLang=en" title="Upgrading Fink (version-specific troubleshooting)">';
 
 
