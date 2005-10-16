b953b5fa3339fcbeef060b3b3ee5f452d2659d61
David R. Morrison <dmrrsn@users.sourceforge.net>
2005-10-16 22:13:11 +0000

    sancho -> sampson

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Updating CVS..."
-if [ `hostname` = "sancho.opendarwin.org" ]; then
- /usr/local/bin/cvs update
+if [ `hostname` = "sampson.opendarwin.org" ]; then
+ /usr/bin/cvs update
 else
  cvs -q update -dP
 fi
