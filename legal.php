8cb150361ea936277fea520ca33b549133c94dba
Darian Lanx <dmalloc@users.sourceforge.net>
2006-07-18 13:45:33 +0000

    Big Updated after a very long talk with my ex lawyer friends. Steven was nice enough to review the whole agreement and adjust it. Now all that is left is to add the actual data from the FDN incorporation which I will do later tonight. Thanks guys!

--- legal.php
+++ legal.php
@@ -1,7 +1,10 @@
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+
 <?
 $title = "Legal Stuff";
-$cvs_author = '$Author: dmacks $';
-$cvs_date = '$Date: 2006/01/03 19:28:03 $';
+$cvs_author = '$Author: dmalloc $';
+$cvs_date = '$Date: 2006/07/18 13:45:33 $';
 
 include "header.inc";
 ?>
@@ -27,6 +30,28 @@ Project</b>, its contributors and occasional helpers from any liability, loss,
 claim and expense (including attorneys reasonable fees) related to your use of 
 this site or violation of this agreement.</p>
 
+<h2 class="legalhead">Limited Representations and Warranties</h2>
+<p class="legal">
+(a) The Site acts as an exchange of Content between those who provide Content to the Site and 
+those who wish to use such Content. The <b>Fink Project</b> grants no rights and makes no warranties
+regarding the use of names, people, trademarks, trade dress, patented or copyrighted designs or 
+works of art or architecture or other forms of intellectual property represented in any Content.
+</p>
+<p class="legal">(b) THE CONTENT IS PROVIDED “AS IS” WITHOUT REPRESENTATION, WARRANTY OR CONDITION OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO THE IMPLIED REPRESENTATIONS, WARRANTIES OR CONDITIONS OF MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. <b>THE FINK PROJECT</b> DOES NOT 
+REPRESENT OR WARRANT THAT THE CONTENT WILL MEET YOUR REQUIREMENTS OR THAT ITS USE WILL BE UNINTERRUPTED
+OR ERROR FREE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE CONTENT IS WITH YOU. 
+SHOULD THE CONTENT PROVE DEFECTIVE, YOU (AND NOT <b>THE FINK PROJECT</b>) ASSUME THE ENTIRE RISK AND COST
+OF ALL NECESSARY CORRECTIONS. IN PARTICULAR AND WITHOUT LIMITING THE GENERALITY OF THE FOREGOING,
+IF YOU ARE DOWNLOADING CONTENT THAT IS IN A BINARY FORMAT OR FILE (WHETHER .DEB OR OTHERWISE) 
+<b>THE FINK PROJECT</b> MAKES NO REPRESENTATION OR WARRANTY RESPECTING SUCH CONTENT WHATSOEVER, 
+WHETHER AS TO OWNERSHIP, TECHNICAL OR LEGAL COMPLIANCE, OR OTHERWISE.
+</p>
+<p class="legal">
+(c) Certain jurisdictions do not allow the exclusion of implied warranties, so the above 
+exclusion may not apply to you. You have specific rights under this warranty, but you may have
+others, which vary from jurisdiction to jurisdiction. 
+</p>
+
 <h2 class="legalhead">Proprietary Rights</h2>
 
 <p class="legal">You should assume that all materials, information, data, text, or
@@ -47,7 +72,32 @@ trademark laws and may not be used without the prior express permission
 of the <b>Fink Project</b>. Any unauthorised use of the Trademarks may violate such laws. The <b>Fink Project</b> and/or any third party that owns the Trademarks shall be entitled to enforce their ownership rights in such Trademarks to the fullest extent of the law. All trademarks not held by the <b>Fink Project</b> are property of the respective owner. Furthermore, the <b>Fink Project</b> shall not be responsible for, and you hereby agree to indemnify the <b>Fink Project</b> for, any liability associated with the infringement of the trademark laws related to your use of Trademarks displayed on this site.
 </p>
 
+<h2 class="legalhead">General Provisions</h2>
+<p class="legal">
+(a) You specifically agree and acknowledge that you have, in addition to the terms of this Agreement any
+ other agreements which may be incorporated by reference therein, and to the extent of their 
+ incorporation in this Agreement you agree to be bound by them.
+</p>
+<p class="legal">
+(b) <b>The Fink Project's</b> failure to insist upon or enforce strict performance of any provision of
+ this Agreement shall not be construed as a waiver of any provision or right.
+</p>
+<p class="legal">
+(c) This Agreement is personal to you and is not assignable by you without <b>The Fink Project's</b> prior
+ written consent. <b>The Fink Project</b> may assign this Agreement without your consent to any other party so long
+ as such party agrees to be bound by its terms.
+</p>
+<p class="legalhead">
+(d) If all or part of any provision of this Agreement is wholly or partially unenforceable, the parties or, in the event
+the parties are unable to agree, a court of competent jurisdiction, shall put in place of such whole or part provision an
+enforceable provision or provisions, that as nearly as possible reflects the terms of the unenforceable whole or part
+provision.
+</p>
 
+<h2 class="legalhead">Contact</h2>
+<p class="legalhead">
+If you have concerns relating to this Agreement, please contact <b>The Fink Project</b> at fink-core-private@lists.sourceforge.net.
+</p>
 <h2 class="legalhead">Changes to the Site</h2>
 <p class="legal">The <b>Fink Project</b> may make improvements or changes in the information, services, products, and other materials on this site, or terminate this site, at any time without notice. The <b>Fink Project</b> may modify this agreement at any time, and such modifications shall be effective
 immediately upon posting of the modified agreement. Accordingly, you
@@ -58,6 +108,20 @@ agreement.
 
 <h2 class="legalhead">Intellectual Property</h2>
 <p class="legal">All contents remains the intellectual property of the <b>Fink Project</b> and cannot be duplicated or copied in its raw form to any other party,<br>
+
+<h2 class="legalhead">Acknowledgement</h2>
+<p class="legal">
+YOU ACKNOWLEDGE THAT YOU HAVE READ THIS AGREEMENT, UNDERSTAND IT, AND HAD AN OPPORTUNITY TO SEEK INDEPENDENT LEGAL ADVICE 
+PRIOR TO AGREEING TO IT. IN CONSIDERATION OF <b>THE FINK PROJECT</b> AGREEING TO PROVIDE THE CONTENT, YOU AGREE TO BE BOUND
+BY THE TERMS AND CONDITIONS OF THIS AGREEMENT. YOU FURTHER AGREE THAT IT IS THE COMPLETE AND EXCLUSIVE STATEMENT OF THE 
+AGREEMENT BETWEEN YOU AND <b>THE FINK PROJECT</b>, WHICH SUPERSEDES ANY PROPOSAL OR PRIOR AGREEMENT, ORAL OR WRITTEN,
+AND ANY OTHER COMMUNICATION BETWEEN YOU AND <b>THE FINK PROJECT</b> RELATING TO THE SUBJECT OF THIS AGREEMENT.</p>
+
+
+
+
+
+
 <br /><br />
 This site is neither approved nor endorsed by Apple Computer, Inc.
 </p> 
