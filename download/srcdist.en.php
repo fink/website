a2bbcf36aaa378fe3e5805cc7cf30f06d34c7023
Alexander Hansen <alexkhansen@users.sourceforge.net>
2007-06-20 02:32:56 +0000

    Have boostrappers use 0.27.3

--- download/srcdist.en.php
+++ download/srcdist.en.php
@@ -1,12 +1,12 @@
-<?
-$title = "Source Release Download";
-$cvs_author = '$Author: alexkhansen $';
-$cvs_date = '$Date: 2007/06/02 23:37:53 $';
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+</head>
 
-include "header.inc";
-?>
 
+<body>
 <h1>Download Fink Source Release</h1>
+
 
 <!--AKH 2007-05-31.  Fix when we have a release tarball that works with OS > 10.4.9
 <p>
@@ -16,10 +16,13 @@ It will download the source code from the original distribution sites
 and build them on your local machine.
 </p>
 -->
-<p>The source tarball contains the <em>fink</em> package manager.  After you have installed it, you will be able to get package descriptions and patches.  It will use these to download the source code from the original distribution sites or the Fink project's mirrors and build them on your local machine.</p>
-<? 
-include "../fink_version.inc";
-?>
+<p>The source tarball contains the <em>fink</em> package manager. After
+you have installed it, you will be able to get package descriptions and
+patches. It will use these to download the source code from the
+original distribution sites or the Fink project's mirrors and build
+them on your local machine.</p>
+
+
 
 <!--
 <p>
@@ -27,21 +30,27 @@ Fink <? print $fink_version; ?> was officially released on
 <? print $release_date; ?>.
 -->
 
-<p><EM>fink-0.27.1</EM> was officially released on 2007-03-19.</p>
-</p>
+<p><em>fink-0.27.3</em> was officially released on 2007-06-15.</p>
+
+<p></p>
+
 <ul>
+
 <!--<li><a
 href="http://prdownloads.sourceforge.net/fink/fink-<? print $fink_version; ?>-full.tar.gz">Fink
 <? print $release_version; ?></a> - 6786K, .tar.gz format</li>-->
-<li><a href="http://downloads.sourceforge.net/fink/fink-0.27.1.tar.gz">fink-0.27.1</a> - 1257K, .tar.gz format</li>
+  <li><a href="http://downloads.sourceforge.net/fink/fink-0.27.3.tar.gz">fink-0.27.3</a> - 1260K, .tar.gz format</li>
+
 </ul>
+
 
 <p>
 <b>Important:</b>
 Don't extract the archive with StuffIt, it will corrupt some file
 names.
 Use the command line <tt>tar</tt> utility instead.
-Instructions are in the Installation document.  You also need to install the Xcode Tools (c.f. <a href="./index.en.php" >the Quick Start page</a>).</p>  
+Instructions are in the Installation document.  You also need to install the Xcode Tools (c.f. <a href="./index.en.php">the Quick Start page</a>).</p>
+  
 <p>
 Installation and usage instructions are inside the distribution
 tarball.
@@ -49,21 +58,25 @@ Please read them - Fink is not a one-click-and-done thing.
 The documents README, INSTALL and USAGE are provided as pure text (for
 reading from the command line) and as HTML (for reading in a browser
 and for printing).
-They are also available online in the <a
-href="../doc/index.php">documentation section</a>.
+They are also available online in the <a href="../doc/index.php">documentation section</a>.
 </p>
+
 <p>After you have installed <em>fink</em> and the other base packages, the commands</p>
+
 <pre>fink selfupdate-rsync</pre>
+
 <p>or</p>
+
 <pre>fink selfupdate-cvs</pre>
+
 <p>will download the package description files and patches.</p>
 
+
 <p>
-To be informed of new releases, subscribe to the <a
-href="../lists/fink-announce.php">fink-announce mailinglist</a>.
+To be informed of new releases, subscribe to the <a href="../lists/fink-announce.php">fink-announce mailinglist</a>.
 </p>
 
 
-<?
-include "footer.inc";
-?>
+
+</body>
+</html>
