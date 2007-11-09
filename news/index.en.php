6b674b645f8c4466c76e10870fe1ff83d0a71cc9
Alexander Hansen <alexkhansen@users.sourceforge.net>
2007-11-09 02:43:30 +0000

    Format update

--- news/index.en.php
+++ news/index.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "News";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2007/11/03 21:08:58';
+$cvs_date = 'Date: 2007/11/09 02:41:45';
 $metatags = '';
 
 include_once "header.inc";
@@ -13,7 +13,7 @@ include_once "header.inc";
     <p>The 10.4-&gt;10.5 update can be accomplished by running <code>fink selfupdate</code> to get <code>fink-0.27.8</code> .  If you're doing this on 10.5, make sure that you have Xcode 3.0 installed.  After 10.5 is installed, run <code>fink reinstall fink</code>.</p>
     <p>A direct 10.3-&gt;10.5 upgrade path isn't ready yet.  If you're impatient to get going, you'll need to download the <code>fink-0.27.8</code> tarball and bootstrap a new Fink tree as above.</p>
     <p><b>Update, 2007-10-29:</b>  Users who have updated from 10.4 to 10.5 should note that using <code>fink cleanup</code> to remove .deb files will remove <b>all</b> of them.  This issue should be resolved in the next update.</p>
-    <p><b>Update, 2007-11-03:</b>  <code>fink-0.27.8 should solve the above issue as well as a couple of others that have come up.</code></p>
+    <p><b>Update, 2007-11-03:</b>  <code>fink-0.27.8</code> should solve the above issue as well as a couple of others that have come up.</p>
   <a name="2007-03-04%20Problems%20with%20tar"><span class="news-date">2007-03-04: </span><span class="news-headline">Problems with tar</span></a><?php gray_line(); ?>
 <p>Some users of fink's unstable tree are having sporadic difficulties
 at the end of building large packages, getting reports like
