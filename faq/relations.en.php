b2b5c66b590a2b7473cd6586ab118d1f08ee0e76
Alexander Hansen <alexkhansen@users.sourceforge.net>
2009-01-19 22:18:45 +0000

    Update to Installing Fink

--- faq/relations.en.php
+++ faq/relations.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Relations";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2009/01/19 21:21:25';
+$cvs_date = 'Date: 2009/01/19 22:17:27';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="mirrors.php?phpLang=en" title="Fink mirrors"><link rel="prev" href="general.php?phpLang=en" title="General Questions">';
 
 
--- faq/usage-fink.en.php
+++ faq/usage-fink.en.php
@@ -1,7 +1,7 @@
 <?
 $title = "F.A.Q. - Fink Usage";
 $cvs_author = 'Author: alexkhansen';
-$cvs_date = 'Date: 2009/01/19 21:21:25';
+$cvs_date = 'Date: 2009/01/19 22:17:27';
 $metatags = '<link rel="contents" href="index.php?phpLang=en" title="F.A.Q. Contents"><link rel="next" href="comp-general.php?phpLang=en" title="Compile Problems - General"><link rel="prev" href="upgrade-fink.php?phpLang=en" title="Upgrading Fink (version-specific troubleshooting)">';
 
 
