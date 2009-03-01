2c99ebf5a14151b78460448ef72753e553ced69e
Alexander Hansen <alexkhansen@users.sourceforge.net>
2009-03-01 02:24:30 +0000

    New translations and stuff

---
+++ doc/index.pt.php
@@ -0,0 +1,141 @@
+<?
+$title = "Documentação";
+$cvs_author = 'Author: alexkhansen';
+$cvs_date = 'Date: 2009/03/01 02:22:14';
+$metatags = '';
+
+
+include_once "header.inc";
+?>
+<h1>Fink - Documentação</h1>
+<!--Generated from $Fink: doc.pt.xml,v 1.1 2009/03/01 02:22:14 alexkhansen Exp $-->
+    <p>Este é um conjunto de vários documentos escritos para o Fink. Alguns
+    destes documentos também podem ser úteis a pessoas que usam o Mac OS X sem
+    o Fink mas que desejam aprender a portar software Unix.</p>
+  <h2><a name="userdoc">Documentação do usuário</a></h2>
+    
+
+    <p>Documentação do usuário atualizada:</p>
+
+    <ul>
+      <li>
+        <a href="users-guide/index.php">Guia do usuário do Fink</a> -
+        cobre a instalação do Fink, instalação de pacotes e atualização para
+        uma nova versão do Fink. Contém instruções para ambas as versões
+        binária e código fonte.
+      </li>
+
+      <li>
+        <a href="advanced/index.php">Guia de tópicos avançados do
+        Fink</a> - cobre conceitos mais avançados do que os cobertos no
+        Guia do Usuário.
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="x11/index.php">Executando o X11
+        no Mac OS X</a> - cobre conceitos, instalação e execução (também
+        direcionado a usuários do Mac OS X em geral).
+        <b>(em inglês)</b>
+      </li>
+    </ul>
+
+    <p>Alguns documentos que são mais completos mas estão um pouco
+    desatualizados e não são mais mantidos:</p>
+
+    <ul>
+      <li>
+        <a href="bundled/install.php">Instalação e atualização</a> - como
+        instalar o Fink ou atualizá-lo para uma versão nova
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="bundled/usage.php">Formas de
+        uso</a> - como usar o Fink e os softwares instalados
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="readme.php">LeiaMe do
+        Fink</a> - o LeiaMe da distribuição de códigos fontes
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="cvsaccess/index.php">Acesso via CVS</a> - como acessar
+        o repositório CVS do Fink para obter os fontes dos pacotes mais
+        recentes antes da liberação de uma nova versão
+        <b>(em inglês)</b>
+      </li>
+    </ul>
+  <h2><a name="developerdoc">Documentação do desenvolvedor</a></h2>
+    
+
+    <ul>
+      <li>
+        <a href="security/index.php">Manual de política de
+        segurança</a> - leitura obrigatória para todos os que mantêm
+        pacotes no Fink ou gostariam de adicionar seus próprios pacotes.
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="http://www.finkproject.org/doc/UsingFink.pdf">Using Fink: A
+        Developer's How To</a> (arquivo PDF com 2MB) - slides de uma
+        apresentação na <a href="http://conferences.oreillynet.com/macosx2002/">O'Reilly Mac OS X
+        Conference</a> (também disponível como um <a href="http://conferences.oreillynet.com/presentations/macosx02/morrison_david.ppt">arquivo
+        PowerPoint</a>)
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="porting/index.php">Dicas
+        para portar software</a> - notas para os interessados em portar
+        aplicações Unix para o Mac OS X
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="quick-start-pkg/index.php">Tutorial de
+        empacotamento</a> - um complemento ao Manual de empacotamento que
+        foca exemplos reais e faz uma introdução sobre empacotamento para
+        iniciantes.
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="packaging/index.php">Manual de empacotamento</a>
+        - como criar e manter pacotes no Fink
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="http://wiki.finkproject.org/index.php/Fink">The Fink
+        Developer Wiki</a> - material relacionado a desenvolvimento que
+        ainda está em construção.
+        <b>(em inglês)</b>
+      </li>
+    </ul>
+  <h2><a name="otherdoc">Outros documentos</a></h2>
+    
+
+    <ul>
+      <li>
+        <a href="multilingual/index.php">Guia de
+        internacionalização</a> - material sobre o esforço em andamento
+        de internacionalização do site
+        <b>(em inglês)</b>
+      </li>
+
+      <li>
+        <a href="netiquette/index.php">Netiqueta para as listas de
+        discussão</a> - como melhor usar as listas de discussão do
+        Fink.
+        <b>(em inglês)</b>
+      </li>
+    </ul>
+  
+<? include_once "../footer.inc"; ?>
+
+
