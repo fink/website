271696b6084a0d8f19fbb1199d9fce58b53459e6
Alexander Hansen <alexkhansen@users.sourceforge.net>
2010-01-10 22:12:56 +0000

    Add some 10.6 update instructions to main docs. Get rid of 10.1, 10.2, and 10.3 update instructions. (Maybe it might be worth dredging up the 10.4->10.5 ones, too)

---
+++ download/10.6-upgrade.en.php
@@ -0,0 +1,31 @@
+<?
+$title = "Upgrade Instructions for Mac OS X 10.6";
+$cvs_author = '$Author: alexkhansen $';
+$cvs_date = '$Date: 2010/01/10 22:12:56 $';
+
+include "header.inc";
+?>
+
+
+<h1>Upgrade Instructions for Mac OS X 10.6</h1>
+<h2>Important Notes:</h2>
+<p>If you've been using Xquartz 2.4 on 10.5, you will probably find it easier to do a <a href="./srcdist.php">clean install</a> from source instead.<p><br>
+<p>To upgrade your Fink installation from 10.5/32 bit to 10.6/32 bit (there is no direct upgrade path from earlier OS versions), follow the sequence below:</p>
+<ol>
+    <li>Before installing OS X 10.6, run <pre>fink selfupdate</pre> with rsync or cvs updating turned on to bring <em>fink</em> to a current version.<br>
+    Use <pre>fink -V</pre> to check your package manager version, which needs to be at least 0.29.10 before updating.</li>
+    <li>Edit the file <em>/sw/etc/fink.conf</em>, adding a line to it which reads <strong>NoAutoIndex: true</strong>.
+    (You may need to use <em>sudo</em> to obtain the correct permissions to edit this file.)</li>   
+    <li>Install OS X 10.6, as well as Xcode 3.2.</li>
+    <li>Run the command <pre>fink reinstall fink</pre> in order to tell <em>fink</em> that you are now on 10.6.
+    (If you encounter a message about package database corruption, run <pre>fink index -f</pre> and try this step again.)</li>
+    <li>Run the command <pre>fink update fink</pre> to get the latest <em>fink</em> for 10.6.</li>
+    <li>Run the command <pre>fink install perl588-core</pre> in case you have Fink packages which depend on it, as the system's Perl version was changed during the upgrade.</li>
+    <li>Remove the <strong>NoAutoIndex: true</strong> line from <em>fink.conf</em>.</li>
+    <li>Run the command <pre>find /sw/lib -name *.la -delete</pre> to remove references to files that don't exist any more on 10.6.</li>
+</ol>
+<p>After the upgrade, you may wish to run <pre>fink configure</pre> to do some cleanup.</p>
+
+<?
+include "footer.inc";
+?>
