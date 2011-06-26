8c625763bad18a8d26d2eaf02c768eae18e07cbd
Andreas Gockel <gecko2@users.sourceforge.net>
2011-06-26 23:19:10 +0000

    Fix "Undefined index: PATH_INFO"

--- pdb/section.php
+++ pdb/section.php
@@ -1,11 +1,17 @@
 <?php
 $title = "Package Database - Obsolete page";
 $cvs_author = '$Author: gecko2 $';
-$cvs_date = '$Date: 2011/06/26 23:14:08 $';
+$cvs_date = '$Date: 2011/06/26 23:19:10 $';
 
 /* check path info */
-$PATH_INFO = $HTTP_SERVER_VARS["PATH_INFO"];
-if (preg_match("/^(\/[a-zA-Z0-9_.+-]+)$/", $PATH_INFO, $r)) {
+if (isset($HTTP_SERVER_VARS["PATH_INFO"])) {
+        $PATH_INFO = $HTTP_SERVER_VARS["PATH_INFO"];
+} else {
+        $PATH_INFO = "";
+}
+if ($PATH_INFO == "") {
+	$section = "";
+} elseif (preg_match("/^(\/[a-zA-Z0-9_.+-]+)$/", $PATH_INFO, $r)) {
 	$section = $r[1];
 } elseif (preg_match("/^(\/[a-zA-Z0-9_.+-]+\/[a-zA-Z0-9_.+-]+)$/", $PATH_INFO, $r)) {
 	$section = $r[1];
