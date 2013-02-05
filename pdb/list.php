5d9e6b38678cf4561bce88031a44a32a88d33a40
Andreas Gockel <gecko2@users.sourceforge.net>
2013-02-05 23:48:30 +0000

    Some isset fixes

--- pdb/list.php
+++ pdb/list.php
@@ -1,9 +1,13 @@
 <?php
 $title = "Package Database - Obsolete page";
-$cvs_author = '$Author: rangerrick $';
-$cvs_date = '$Date: 2007/12/06 22:03:22 $';
+$cvs_author = '$Author: gecko2 $';
+$cvs_date = '$Date: 2013/02/05 23:48:30 $';
 
-$server = $_SERVER['SERVER_NAME'];
+if (isset($_SERVER['SERVER_NAME'])) {
+	$server = $_SERVER['SERVER_NAME'];
+} else {
+	$server = "localhost";
+}
 $location = "http://$server/pdb/browse.php";
 
 // This page is obsolete. We redirect to browse.php
