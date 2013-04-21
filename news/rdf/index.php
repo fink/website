0a8696b0162b365cd88a1c0adc0dce87b8d2fd25
Andreas Gockel <gecko2@users.sourceforge.net>
2013-04-21 09:12:28 +0000

    Add backup Feeds, if feeds2.feedburner.com gets no updates

--- news/rdf/index.php
+++ news/rdf/index.php
@@ -1,5 +1,5 @@
-<?
-$title = "Fink Package RDF Feeds";
+<?php
+$title = "Fink Package RDF/RSS Feeds";
 $cvs_author = 'Author: dmrrsn';
 $cvs_date = 'Date: 2004/09/20 15:56:45';
 $metatags = '';
@@ -9,8 +9,7 @@ include_once "header.inc";
 ?>
 
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
-<tr>
-<td>
+<tr><td>
 <h1>Fink Package RDF Feeds</h1>
 
 <h2>Updated Packages By OS</h2>
@@ -19,11 +18,24 @@ include_once "header.inc";
  <li>10.4-transitional (<a href="http://feeds2.feedburner.com/FinkProjectNews-104-transitional-stable">Stable</a>) (<a href="http://feeds2.feedburner.com/FinkProjectNews-104-transitional-unstable">Unstable</a>)</li>
  <li>10.4+ (<a href="http://feeds2.feedburner.com/FinkProjectNews-104-stable">Stable</a>) (<a href="http://feeds2.feedburner.com/FinkProjectNews-104-unstable">Unstable</a>)</li>
 </ul>
+
 <h2>Updated Packages By Tree</h2>
 <ul>
  <li><a href="http://feeds2.feedburner.com/FinkProjectNews-stable">Stable</a></li>
  <li><a href="http://feeds2.feedburner.com/FinkProjectNews-unstable">Unstable</a></li>
 </ul>
 
-</td></tr></table>
-<? include_once "footer.inc"; ?>
+<h2>Backup Feeds, if feeds2.feedburner.com gets no updates</h2>
+<ul>
+ <li><a href="feed://<?php print $_SERVER["SERVER_NAME"]; ?>/news/rdf/fink-stable.rss">Stable</a></li>
+ <li><a href="feed://<?php print $_SERVER["SERVER_NAME"]; ?>/news/rdf/fink-stable-no-splitoffs.rss">Stable without splitoffs</a></li>
+<!-- <li><a href="feed://<?php print $_SERVER["SERVER_NAME"]; ?>/news/rdf/fink-stable.rss">Unstable</a></li> -->
+<!-- <li><a href="feed://<?php print $_SERVER["SERVER_NAME"]; ?>/news/rdf/fink-unstable-no-splitoffs.rss">Unstable without splitoffs</a></li> -->
+ <li><a href="feed://<?php print $_SERVER["SERVER_NAME"]; ?>/news/rdf/fink-experimental.rss">Experimental</a></li>
+</ul>
+
+</td></tr>
+</table>
+<?php
+include_once "footer.inc";
+?>
