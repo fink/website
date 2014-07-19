53edaf45dd157f04baaecabcc7c131249d055f5d
K.-M. Schindler <k-m_schindler@users.sourceforge.net>
2014-07-19 17:25:01 +0000

    add localized German index file for web/map/. More of a test than really needed.

---
+++ map/index.de.php
@@ -0,0 +1,43 @@
+<?
+$title = "Fink Entwickler Karte";
+$cvs_author = '$Author: k-m_schindler $';
+$cvs_date = '$Date: 2014/07/19 17:25:01 $';
+
+include "header.inc";
+?>
+<h1>Fink Entwickler Karte</h1>
+<p>Haben sie sich schon einmal gefragt, wo <a href="../people.php">die Personen</a> leben, die Fink beitragen? Auch wenn nicht, genau hier findet man diese Information.</p>
+<p>Die Personen auf der Karte sind durch nummeriert. Schauen sie zuerst in der Liste und dann auf der Karte nach, um heraus zu finden, wo jemand lebt. Der Name in Anführungsstrichen ist der Spitzname ("nick") im irc-Kanal #fink auf irc.freenode.net.</p>
+<img src="finkmap_world.png" height="750" width="750" alt="">
+<table summary="Liste fuer die Fink Entwickler Karte">
+<tr><td>Nummer</td><td>Name</td><td>Ort</td></tr>
+<tr><td>1</td><td>Benn 'newmanbe' Newman</td></tr>
+<tr><td>2</td><td>Daniel 'dmacks' Macks</td></tr>
+<tr><td>3</td><td>David R. 'drm' Morrison</td></tr>
+<tr><td>4</td><td>Benjamin 'RangerRick' Reed</td></tr>
+<tr><td>5</td><td>Chris 'cirdan' Zubryzcki</td></tr>
+<tr><td>6</td><td>David 'dmalloc' H&#246;hn</td></tr>
+<tr><td>7</td><td>Justin 'TheSin' Hallett</td><td>Thorsby, Alberta, Canada</td></tr>
+<tr><td>8</td><td>Peter 'pogma' O'Gorman</td><td>Hyogo, Japan</td></tr>
+<tr><td>9</td><td>Martin Costabel</td></tr>
+<tr><td>10</td><td>Baba 'baba' Yoshihiko</td><td>Kyoto, Japan</td></tr>
+<tr><td>11</td><td>Aida 'aida' Shinra</td><td>Tokyo, Japan</td></tr>
+<tr><td>12</td><td>Asari 'asari' Takashi</td><td>Tokyo, Japan</td></tr>
+<tr><td>13</td><td>Motoi 'wm3' Washida</td><td>Tokyo, Japan</td></tr>
+<tr><td>14</td><td>Dave 'vasi' Vasilevsky</td><td>Montr&#233;al, Canada</td></tr>
+<tr><td>15</td><td>Alexander 'astrange' Strange</td></tr>
+<tr><td>16</td><td>Alexander 'akh' Hansen</td></tr>
+<tr><td>17</td><td>Ben 'Clef' Hines</td></tr>
+<tr><td>18</td><td>Chris 'zizban' Turkel</td><td>Cheshire, 
+Massachusetts, United States of America</td></tr>
+</table>
+<p>Soll man sie auch auf der Karte finden? Wenn ja, dann schicken sie eine E-mail in folgendem Format an <a href="mailto:newmanbe@freeshell.org?Subject=Fink Developer Map">Benn Newman</a>:</p>
+<p>
+<code>Längengrad Breitengrad "Vorname '#fink nick' Familienname" #ihre Funktion bei fink (developer, maintainer)</code>
+</p>
+
+<p>Damit alle auf die Karte passen, müssen manchmal benachbarte Leute zusammen gelegt werden. Ihre tatsächlichen Koordinaten bleiben dennoch erhalten</p>
+<?
+include "../footer.inc";
+?>
+
