6d687df906068338b44fb5d41b7157b38eef0e30
K.-M. Schindler <k-m_schindler@users.sourceforge.net>
2014-07-20 00:04:08 +0000

    more German translations

--- download/10.3-upgrade.de.php
+++ download/10.3-upgrade.de.php
@@ -1,34 +1,53 @@
 <?
-$title = "Anleitung zur Aktualisierung unter  Mac OS X 10.3";
-$cvs_author = '$Author: benh57 $';
-$cvs_date = '$Date: 2004/11/26 09:27:54 $';
+$title = "Anleitung zur Aktualisierung unter Mac OS X 10.3";
+$cvs_author = '$Author: k-m_schindler $';
+$cvs_date = '$Date: 2014/07/20 00:04:08 $';
 
 include "header.inc";
 ?>
 
 
-<h1>Anleitung zur Aktualisierung unter  Mac OS X 10.3</h1>
+<h1>Anleitung zur Aktualisierung unter Mac OS X 10.3</h1>
 
-<p>Es ist nun möglich, beim fink-Upgrade alle Vorteile des gcc 3.3-Compilers zu nutzen, oder - falls installiert - OS X 10.3 zu nutzen.
-Sowohl die Nutzer der Quelltext- als auch die der Binär-Version können Sie ihre Fink-Installationen entsprechend aktualisieren; allerdings ist die Binär-Version zur Zeit wesentlich stabiler und zuverlässiger als die Quelltext-Version.
+<p>Es ist nun möglich, beim fink-Upgrade alle Vorteile des gcc
+3.3-Compilers zu nutzen, oder - falls installiert - OS X 10.3 zu nutzen.
+Sowohl die Nutzer der Quelltext- als auch die der Binär-Version können
+ihre Fink-Installationen entsprechend aktualisieren; allerdings ist die
+Binär-Version zur Zeit wesentlich stabiler und zuverlässiger als die
+Quelltext-Version.
 </p><p>
-Sollten Sie dieses Upgrade (Quelltext-Version) durchführen wollen und sollten Sie gleichzeitig das Update der Developer Tools von Apple vom Juni 2003 durchgeführt haben, müssen Sie das Update der Developer Tools vom August 2003 installieren BEVOR Sie Fink aktualisieren können.
-Sorgen Sie unter 10.3 dafür, dass Sie XCode von der XCode-disc installieren, bevor Sie Fink aktualisieren.
+Sollten Sie dieses Upgrade (Quelltext-Version) durchführen wollen und
+sollten Sie gleichzeitig das Update der Developer Tools von Apple vom Juni
+2003 durchgeführt haben, müssen Sie das Update der Developer Tools vom
+August 2003 installieren BEVOR Sie Fink aktualisieren können.  Sorgen Sie
+unter 10.3 dafür, dass Sie XCode von der XCode-CD installieren, bevor
+Sie Fink aktualisieren.
 </p><p>
-Der Befehl "fink selfupdate" sollte die Aktualisierung für Sie leisten. Die aktuelle Version des Fink Paket-Managers erkennt automatisch welche Mac OS X- und welche gcc-Version bei Ihnen installiert ist und stellt sich darauf selbstständig ein.
+Der Befehl "fink selfupdate" sollte die Aktualisierung für Sie
+durchführen.  Die aktuelle Version des Fink Paket-Managers erkennt
+automatisch, welche Versionen von Mac OS X und gcc bei Ihnen installiert
+sind und stellt sich darauf automatisch ein.
 </p><p>
-Wenn Sie eine Fink-Neu-Installation auf einem  10.3-System durchführen wollen, empfehlen wir Ihnen eine
-<a href="http://fink.sourceforge.net/download/srcdist.php">Installation auf Basis der Quelltextversion,</a>
-mit Hilfe der Datei fink-full-0.6.1.tar.gz, die sie auf der  
-<a href="http://sourceforge.net/project/showfiles.php?group_id=17203">Sourceforge 
-Download Seite von Fink finden.</a> Hierfür benötigen Sie außerdem XCode.
+Wenn Sie eine Fink-Neu-Installation auf einem 10.3-System durchführen
+wollen, empfehlen wir Ihnen eine <a
+href="http://fink.sourceforge.net/download/srcdist.php">Installation auf
+Basis der Quelltextversion,</a> mit Hilfe der Datei fink-full-0.6.1.tar.gz,
+die sie auf der <a
+href="http://sourceforge.net/project/showfiles.php?group_id=17203">Sourceforge
+Download Seite von Fink finden.</a> Hierfür benötigen Sie außerdem
+XCode.
 </p><p>
-Beachten Sie außerdem die Tatsache, dass Sie bei einer Fink Version 0.15.0 oder höher system-xfree86 nicht mehr installieren müssen. Fink kann Ihr system-xfree86 automatisch ermitteln, wenn Sie noch keine fink x11 Pakete installiert haben. sollten Sie ein altes system-xfree86-Paket jeglicher Art installiert haben, führen Sie folgenden Befehl aus:
+Beachten Sie außerdem die Tatsache, dass Sie bei einer Fink Version 0.15.0
+oder höher system-xfree86 nicht mehr installieren müssen.  Fink kann Ihr
+system-xfree86 automatisch ermitteln, wenn Sie noch keine fink x11 Pakete
+installiert haben.  Sollten Sie irgendein altes system-xfree86-Paket
+installiert haben, führen Sie folgenden Befehl aus:
 </p>
 <pre>sudo dpkg -r --force-all system-xfree86 system-xfree86-42 \
 system-xfree86-43; fink selfupdate; fink index</pre>
 <p>
-Das fink-Team arbeitet weiterhin daran, dass die Pakete unter 10.3 funktionieren. Viele Pakete laufen bereits jetzt.
+Das fink-Team arbeitet weiterhin daran, dass die Pakete unter 10.3
+funktionieren.  Viele Pakete laufen bereits jetzt.
 </p>
 
 <?
---
+++ download/10.7-upgrade.de.php
@@ -0,0 +1,53 @@
+<?
+$title = "Anleitung zur Aktualisierung unter Mac OS X 10.7";
+$cvs_author = '$Author: k-m_schindler $';
+$cvs_date = '$Date: 2014/07/20 00:04:08 $';
+
+include "header.inc";
+?>
+
+<h1>Anleitung zur Aktualisierung unter Mac OS X 10.7</h1>
+<h2>Wichtige Anmerkungen:</h2>
+
+<p>Es gibt keinen unterstützen Weg für Fink von 10.6 (oder früher) zu
+10.7. Es gibt Berichte über eine erfolgreiche Aktualisierung von einer 
+10.6/64 bit Fink Installation zu 10.7, aber es gibt keine Garantie, dass 
+der aktualisierte Fink Baum korrekt funktioniert. Eine Aktualisierung von 
+10.6/i386 funktioniert definitiv nicht.</p>
+
+<p>Die Anleitung hier ist eine Übertragung der auf dem  
+<a href="http://finkers.wordpress.com/2011/09/26/fink-and-lion/">Fink blog</a>. 
+Die Einträge dort beschreiben die Aktualisierung mit mehr Details.</p>
+
+<p>Dieser Prozess sammelt eine Liste der Pakete, die auf 10.6 (32 oder 64 bit) 
+installiert sind und speichert sie für die spätere Verwendung bei der 
+Installation von Fink auf 10.7</p>
+
+<p>Befolgen sie die folgenden Anweisungen, um die Liste der Pakete zu
+erstellen:</p>
+
+<ol>
+	<li>Benutzen sie 
+  <pre>grep -B1 "install ok installed" /sw/var/lib/dpkg/status | grep Package | cut -d: -f2 > fink_packages.txt</pre>, 
+	um die Paketinformationen in einer Datei zu speichern.</li>
+	<li>Installieren sie OS X 10.7 und Xcode 4.5.2 oder zumindest die Command
+	Line Tools.</li>
+	<li>Löschen sie das Fink-Verzeichnis, z. B. mit 
+  <pre>sudo rm -rf /sw</pre>.</li>
+	<li><a href="./srcdist.php">Installieren sie Fink</a> auf ihrem neuen 10.7
+	System.</li>
+	<li>Führen sie das Kommando: <pre>cat fink_packages.txt | xargs fink install</pre>
+	aus, damit die neue Installation von Fink die Pakete installiert, die 
+  vorher unter 10.6. installiert waren</li>
+</ol>
+
+<p>Nicht alle Pakete, die unter 10.6 zur Verfügung standen, gibt es auch
+für 10.7, weil es einige Änderungen im System gibt.  Es wird daran
+gearbeitet, möglichst viele der Pakete wieder verfügbar zu machen.  Ist ihr
+Lieblingspaket auf 10.7 noch nicht verfügbar, kontaktieren sie den
+Maintainer des Pakets und fragen sie ihn, ob es nach 10.7 übernommen werden
+kann.</p>
+
+<?
+include "footer.inc";
+?>
