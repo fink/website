a30b35342acd5e0e340086d064cbe7249a7c0cc3
Andreas Gockel <gecko2@users.sourceforge.net>
2014-10-25 09:41:56 +0000

    Restore old pr stuff as it contains the history of ourer logo

---
+++ pr/index.php
@@ -0,0 +1,21 @@
+<?
+$title = "Public Stuff";
+$cvs_author = '$Author: gecko2 $';
+$cvs_date = '$Date: 2014/10/25 09:41:56 $';
+
+include "header.inc";
+?>
+
+
+<h1>Public Stuff</h1>
+
+<ul>
+<li><a href="thelogo.php">All about Fink's logo</a></li>
+<li><a href="openosx.php">Fink Relations with OpenOSX</a></li>
+<li><a href="forked.php">Fink Relations with forked.net</a></li>
+</ul>
+
+
+<?
+include "footer.inc";
+?>
---
+++ pr/openosx.php
@@ -0,0 +1,63 @@
+<?
+$title = "Relations with OpenOSX";
+$cvs_author = '$Author: gecko2 $';
+$cvs_date = '$Date: 2014/10/25 09:41:56 $';
+
+include "header.inc";
+?>
+
+
+<h1>Fink Relations with OpenOSX</h1>
+
+<blockquote>
+Note: This page represents the view of Fink project leader Christoph
+Pfisterer.
+Other people, including other Fink project members, may have different
+views.
+</blockquote>
+
+<p>
+Here's the story of the relations between the Fink project and
+OpenOSX.
+It is unpleasant, but I feel that is has to be made public.
+</p>
+
+<p>
+<a href="http://www.openosx.com/">OpenOSX</a> is a business that sells
+a range of CDs of Open Source applications.
+The <a href="http://www.openosx.com/gimp/index.html">GIMP CD</a> they
+sell is based to a large part of Fink 0.2.1.
+Until very recently, the OpenOSX web site made no mention at all of
+Fink, and as far as I can tell, the CDs themselves still contain no
+hints.
+My impression (and that of many others) was that OpenOSX did all the
+porting - which has now turned out to be wrong.
+</p>
+<p>
+I was never contacted by OpenOSX about their usage of Fink.
+Instead, I was notified of the situation by a third party.
+What followed was a rather unpleasant
+<a href="openosx-thread.php">e-mail conversation</a> with OpenOSX
+owner Jeshua Lacock.
+The net result?
+We almost broke into a flame war and OpenOSX has slightly modified
+their web site to cover their tracks.
+They still don't have the courage to openly say which parts are their
+work and which parts aren't.
+It also appears they don't want to cooperate with the rest of the Mac
+OS X Open Source community, and Jeshua doesn't feel like responding to
+my mails as promised.
+</p>
+<p>
+I encourage everyone to read the <a href="openosx-thread.php">e-mail
+exchange</a> that led to this and judge for themselves.
+(Be warned that it's quite lengthy, though.)
+</p>
+<p>
+-Christoph Pfisterer
+</p>
+
+
+<?
+include "footer.inc";
+?>
