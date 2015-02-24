db453f5be81eab05872fe1294635bf70587e262d
K.-M. Schindler <k-m_schindler@users.sourceforge.net>
2015-02-24 00:09:09 +0000

    update German docs: security. php/html part

---
+++ doc/security/notification.de.php
@@ -0,0 +1,103 @@
+<?php
+$title = "Sicherheitspolitik - Benachrichtigungen";
+$cvs_author = 'Author: Nachteule';
+$cvs_date = 'Date: 2009/03/31 01:41:35';
+$metatags = '<link rel="contents" href="index.php?phpLang=de" title="Sicherheitspolitik Contents"><link rel="prev" href="updating.php?phpLang=de" title="Sicherheitsaktualisierungen">';
+
+
+include_once "header.de.inc";
+?>
+<h1>Sicherheitspolitik - 5. Benachrichtigungen verschicken</h1>
+    
+    
+    
+      <p>Manche Nutzer aktualisieren ihre Software nicht sehr häufig.
+        Damit möglichst schnell alle Nutzer aktualisierte Versionen der
+        Pakete mit Sicherheitslücken installieren und benutzen, kann ein
+        Paketbetreuer darum bitten, dass eine entsprechende Ankündigung
+        über die Mailing-Liste "Fink announcement" verschickt wird.</p>
+    
+    <h2><a name="who">5.1 Wer darf verschicken?</a></h2>
+      
+      <p>Diese Ankündigung darf nur vom <b>Fink Security Team</b>
+        verschickt werden. Meistens wird das von
+        dmalloc@users.sourceforge.net erledigt und einem PGP-Schlüssel mit
+        folgender Signatur:</p>
+      <ul>
+        <li>FD77 F0B7 5C65 F546 EB08 A4EC 3CCA 1A32 7E24 291E.</li>
+        <li>zu finden bei
+          http://pgp.mit.edu:11371/pks/lookup?op=get&amp;search=0x7E24291E</li>
+      </ul>
+      <p>Das obige ist absichtlich nicht als Link formatiert.</p>
+      <p>Weitere autorisierte Mitglieder des Teams sind: (fügen sie hier
+        ihre Email-Adresse und den öffentlichen Schlüssel ein.)</p>
+      <p>peter@pogma.com signiert durch einen PGP-Schlüssel mit der Signatur:</p>
+      <ul>
+        <li>4D67 1997 DD32 AE8E D7ED  9C79 8491 2AB7 DF3B 6004.</li>
+        <li>
+          zu finden bei http://pgp.mit.edu:11371/pks/lookup?op=get&amp;search=0xDF3B6004
+        </li>
+      </ul>
+      <p>ranger@befunk.com signiert durch einen PGP-Schlüssel mit der Signatur:</p>
+      <ul>
+        <li> 6401 D02A A35F 55E9 D7DD  71C5 52EF A366 D3F6 65FE.</li>
+        <li>
+          zu finden bei http://pgp.mit.edu:11371/pks/lookup?op=get&amp;search=0xD3F665FE
+        </li>
+      </ul>
+    
+    <h2><a name="how">5.2 Wie ankündigen?</a></h2>
+      
+      <p>Damit sicher gestellt ist, dass Sicherheitsankündigungen ein
+        einheitliches Aussehen haben, <b>müssen</b> sich alle an die
+        folgende Vorlage halten.</p>
+<pre> ID: FINK-YYYY-MMDD-NN
+ Reported: YYYY-MM-DD
+ Updated:  YYYY-MM-DD
+ Package:  package-name
+ Affected: &lt;= versionid
+ Maintainer: maintainer-name
+ Tree(s): 10.3/stable, 10.3/unstable, 10.2-gcc3.3/stable,10.2-gcc3.3/unstable
+ Mac OS X version: 10.3, 10.2
+ Fix: patch|upstream
+ Updated by: maintainer|forced update (Email)
+ Description: A short description describing the issue.
+ References: KEYWORD (see above)
+ Ref-URL: URL
+</pre>
+      <p>Ein Beispielsbericht könnte so aussehen:</p>
+<pre> ID: FINK-2004-06-01
+ Reported:         2004-06-09
+ Updated:          2004-06-09
+ Package:          cvs
+ Affected:         &lt;= 1.11.16, &lt;= 1.12.8
+ Maintainer:       Sylvain Cuaz
+ Tree(s):          10.3/stable, 10.3/unstable, 10.2-gcc3.3/stable,10.2-gcc3.3/unstable
+ Mac OS X version: 10.3, 10.2
+ Fix:              upstream
+ Updated by: forced update (dmalloc@users.sourceforge.net)
+ Description: Multiple vulnerabilities in CVS found by Ematters Security.
+ References: BID
+ Ref-URL:    http://www.securityfocus.com/bid/10499
+ References: CVE
+ Ref-URL:    http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-0414
+ References: CVE
+ Ref-URL:    http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-0416
+ References: CVE
+ Ref-URL:    http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-0417
+ References: CVE
+ Ref-URL:    http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CAN-2004-0418
+ References: FULLDISCURL
+ Ref-URL:    http://lists.netsys.com/pipermail/full-disclosure/2004-June/022441.html
+ References: MISC
+ Ref-URL:    http://security.e-matters.de/advisories/092004.html
+</pre>
+      <p>Beachten sie bitte, dass sich das Schlüsselwort <b>Affected</b>
+        auf alle Versionen der Software bezieht, die Sicherheitslücken
+        aufweisen, nicht nur auf die, für die es Fink-Pakete gibt. Das
+        Beispiel zeigt dies sehr deutlich.</p>
+    
+  
+<?php include_once "../../footer.inc"; ?>
+
+
---
+++ doc/security/respo.de.php
@@ -0,0 +1,62 @@
+<?php
+$title = "Sicherheitspolitik - Verantwortung";
+$cvs_author = 'Author: Nachteule';
+$cvs_date = 'Date: 2009/03/31 01:41:35';
+$metatags = '<link rel="contents" href="index.php?phpLang=de" title="Sicherheitspolitik Contents"><link rel="next" href="severity.php?phpLang=de" title="Antwortzeiten und sofortige Aktionen"><link rel="prev" href="index.php?phpLang=de" title="Sicherheitspolitik Contents">';
+
+
+include_once "header.de.inc";
+?>
+<h1>Sicherheitspolitik - 1. Verantwortung</h1>
+    
+    
+    <h2><a name="who">1.1 Wer ist verantwortlich?</a></h2>
+      
+      <p>Jedes Fink-Paket hat einen Betreuer. Man findet ihn, indem man
+        das Kommando <code>fink info packagename</code> eingibt. Man erhält
+        eine Liste, die in etwa so aussieht: Maintainer: Fink Core Group
+        &lt;fink-core@lists.sourceforge.net&gt;. Dieser Betreuer hat
+        die volle Verantwortung für seine Pakete.</p>
+    
+    <h2><a name="contact">1.2 Wen soll ich kontaktieren?</a></h2>
+      
+      <p>Treten bei der Software eines Fink-Pakets Sicherheitslücken
+        auf, sollten sie den Paket-Betreuer und das <b>Fink Core Team</b>
+        informieren. Die Email-Adresse des Betreuers steht im Paket-Info,
+        die des <b>Fink Core Team</b> ist fink-core@lists.sourceforge.net
+        </p>
+    
+    <h2><a name="prenotifications">1.3 Im-Voraus-Benachrichtigung</a></h2>
+      
+      <p>Bei schwerwiegenden Sicherheitslücken kann es erforderlich sein,
+        den Paket-Betreuer im voraus zu benachrichtigen. Da es vorkommen
+        kann, dass der Betreuer nicht rechtzeitig erreicht wird, sollte das
+        <b>Fink Security Team</b> ebenfalls im voraus benachrichtigt
+        werden. Die Email-Adressen der Teammitglieder sind weiter unten
+        aufgeführt. Bitte beachten sie, dass
+        fink-core@lists.sourceforge.net eine öffentlich archivierte
+        Mailing-Liste ist und private Im-Voraus-Benachrichtigungen deshalb
+        <b>nie</b> an diese Adresse geschickt werden dürfen.</p>
+    
+    <h2><a name="response">1.4 Antwort</a></h2>
+      
+      <p>Eingeschickte Berichte über Sicherheitslücken werden vom
+        <b>Fink Core Team</b> beantwortet. Von jedem Betreuer wird von
+        Fink verlangt, jede einzelne Sicherheitslücke zu bestätigen. In
+        dem unwahrscheinlichen Fall, dass der Betreuer nicht erreichbar ist
+        und der Betreuer den Bericht nicht innnerhalb von 24 Stunden
+        bestätigt, soll eine Notiz an das <b>Fink Core Team</b> gehen,
+        in dem das Team informiert wird, dass der Betreuer nicht
+        antwortet.</p>
+      <p>In dem Fall, dass sie versuchten, den Paket-Betreuer zu erreichen,
+        aber das Mail-System einen Fehler bei der Zustellung meldet,
+        sollten sie sofort das <b>Fink Core Team</b> darüber
+        informieren, dass der Paket-Betreuer nicht erreicht werden konnte
+        und dass das Paket unabhängig vom Betreuer aktualisiert werden
+        muss.</p>
+    
+  <p align="right"><?php echo FINK_NEXT ; ?>:
+<a href="severity.php?phpLang=de">2. Antwortzeiten und sofortige Aktionen</a></p>
+<?php include_once "../../footer.inc"; ?>
+
+
