1705e903e380cb71ed385f8913f5194353c29403
mischi <karl-michael.schindler@web.de>
2017-03-18 00:20:23 +0100

    German translation of Download → archive
    This is not perfect, but at least it keeps the language. Other
    languages can be added more easily, now.

---
+++ download/archive.en.php
@@ -0,0 +1,24 @@
+<?php
+$title = "Archive Browser";
+$cvs_author = '$Author: thesin $';
+$cvs_date = '$Date: 2016/09/21 14:53:37 $';
+
+include "header.inc";
+?>
+
+<script type="application/javascript">
+
+window.addEventListener('message', function(e) {
+    if (e.data[0] == 'setHeight') {
+        document.getElementById( 'bindistFrame' ).height = e.data[1];
+    }
+}, false);
+
+</script>
+
+<iframe id="bindistFrame" src="http://bindist.finkproject.org/" scrolling="no" frameborder="no" marginheight="0" marginwidth="0" width="100%"></iframe>
+
+<?php
+include "footer.inc";
+?>
+<?php include_once "../phpLang.inc.php"; ?>
